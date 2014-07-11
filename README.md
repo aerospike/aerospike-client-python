6d30bb4f77c8a9abcdea70e2a6cf18f7a8871048
Chris Stivers <chris@aerospike.com>
2014-07-11 13:32:36 -0700

    Made setup.py more robust. Requires less environment setup. Updated README.md

--- README.md
+++ README.md
@@ -7,22 +7,60 @@ The Aerospike Python client depends on the Aerospike C client and Lua.
 This was tested on Python 2.6.6, but may work up through Python 3.x. You will need to install Python development packages to build this.
 
 
-### Environment Variables
+### Aerospike C Client
 
-If you are building the C client from source, rather than installing the development packages, then set the `AEROSPIKE_C_HOME` to point to the directory containing the C client output directory.
+#### Install From Package
 
-	export AEROSPIKE_C_HOME={path to c client source}/target/Linux-x86_64/
+You can download and install the Aerospike C Client development package from:
 
-Set the `LD_LIBRARY_PATH` to include the C client library directory:
+	http://aerospike.com/download/client/c
 
-	export LD_LIBRARY_PATH=$AEROSPIKE_C_HOME/lib
+When downloaded, you will want to extract the contents of the archive and install the "devel" package. The "devel" package will install header files and libraries. Read the Aerospike C Client **[Installation Guide](http://aerospike.com/docs/client/c/install/)** for additional details.
 
-Set the `CPATH` to include the C client include directory
-  
-  	export CPATH=$AEROSPIKE_C_HOME/include:$AEROSPIKE_C_HOME/include/ck
+#### Build From Source
+
+**Note** _The following ONLY pertains to users that are building the Aerospike C Client from source code. If you have installed the C client development package, then you can skip the following._
+
+If you are building the Aerospike C Client from source, then set the `AEROSPIKE_C_HOME` to point to the directory containing the C client repository. 
+
+	$ export AEROSPIKE_C_HOME={{path to c client source}}
+
+For example, to get the source code, you can `git clone` the source repository into your home directory:
+
+	$ cd ~
+	$ git clone https://github.com/aerospike/aerospike-client-c
+
+Then build the Aerospike C Client library per the instructions provided in the README.md in the repository.
+
+Then you can set the environment variable:
+
+	$ export AEROSPIKE_C_HOME=~/aerospike-client-c
 
 
-### Ubuntu 12.04
+### RedHat 6+ and CentOS 6+
+
+The following are dependencies for:
+
+- RedHat Enterprise (RHEL) 6 or newer 
+- CentOS 6 or newer 
+- and related distributions using `yum` package manager.
+
+#### Install Python Development
+
+	sudo yum install python26-devel
+
+#### Install Lua Development
+
+	sudo yum install lua-devel
+
+
+### Debian 6+ and Ubuntu 12.04+
+
+The following are dependencies for:
+
+- Debian 6 or newer 
+- Ubuntu 12.04 or newer 
+- and related distributions using `apt-get` package manager.
 
 #### Install Python Development
 
@@ -30,17 +68,15 @@ Set the `CPATH` to include the C client include directory
 
 #### Install Lua Development
 
-	sudo apt-get install lua5.1-dev
+	sudo apt-get install liblua5.1-dev
 
 
 ### Mac OS X
 
-#### Environment Variables
 
-In addition to the environment variables defined earlier, you will want to set `DYLD_LIBRARY_PATH` to equal `LD_LIBRARY_PATH`:
+#### Install Lua Development
 
-	export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH
-
+We recommend building Lua from source. To download the source code and build the library, follow the instruction provided in the _Lua_ section of the [Aerospike C Client Installation Guide](http://aerospike.com/docs/client/c/install/macosx.html#lua)
 
 ## Usage
 
@@ -58,6 +94,9 @@ To install the library:
 
 ## Examples
 
+**Note** If you did not install the library, then you will need to setup your `PYTHONPATH` environment variable. The `PYTHONPATH` should contain an entry for the directory where the Python module is stored. This is usually in `build/lib.*`.
+
+
 Examples are in the `examples` directory. The following examples are available:
 
 * `kvs.py` — Key-Value Store API Example
@@ -66,9 +105,16 @@ Examples are in the `examples` directory. The following examples are available:
 * `info.py` — Info API Example
 * `simple.lua` — Simple UDF Example
 
+Each example provides help/usage information when you specify the `--help` option. For example, for help on the `kvs.py` example, then run:
+
+	$ python examples/client/kvs.py --help
+
+
 ### Running Examples
 
 Simply call `python` with the path to the example
 
 	python examples/client/kvs.py
+
+
 
--- setup.py
+++ setup.py
@@ -1,19 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
 from distutils.core import setup, Extension
+
 import os
 import platform
+import sys
 
-ostype =  platform.platform(1)
-linux = 'Linux' in ostype
-osx = 'Darwin' in ostype
+
+AEROSPIKE_C_HOME = os.getenv('AEROSPIKE_C_HOME')
+PLATFORM =  platform.platform(1)
+LINUX = 'Linux' in PLATFORM
+DARWIN = 'Darwin' in PLATFORM
+
+library_dirs = ['/usr/local/lib','/usr/lib'] + [x for x in os.getenv('LD_LIBRARY_PATH', '').split(':') if len(x) > 0]
+
+include_dirs = ['src/include'] + [x for x in os.getenv('CPATH', '').split(':') if len(x) > 0]
+
+extra_objects = []
 
 extra_compile_args = [
-          '-std=gnu99', '-g', '-Wall', '-fPIC', '-O1',
-          '-fno-common', '-fno-strict-aliasing', '-finline-functions', 
-          '-march=nocona', 
-          '-D_FILE_OFFSET_BITS=64', '-D_REENTRANT', '-D_GNU_SOURCE'
-        ]
+    '-std=gnu99', '-g', '-Wall', '-fPIC', '-O1',
+    '-fno-common', '-fno-strict-aliasing', '-finline-functions', 
+    '-march=nocona', 
+    '-D_FILE_OFFSET_BITS=64', '-D_REENTRANT', '-D_GNU_SOURCE'
+    ]
 
-library_dirs = [x for x in os.getenv('LD_LIBRARY_PATH', '').split(':') if len(x) > 0]
+extra_link_args = []
 
 library_search = library_dirs + [
   '/usr/local/lib',
@@ -24,17 +37,38 @@ library_search = library_dirs + [
   '/lib',
   ] 
 
-libraries = [ 
-  'aerospike',
+libraries = [
   'ssl',
   'crypto',
   'pthread',
   'm'
   ]
 
+library_ext = '.so'
 
-if osx:
-    
+
+# Prefix for Aerospike C client libraries and headers
+aerospike_c_prefix = None
+
+# If True, then link libaerospike statically, otherwise it will be dynamically linked
+aerospike_c_static = True
+
+
+################################################################################
+# PLATFORM SPECIFIC SETTINGS
+################################################################################
+
+if DARWIN:
+
+    if AEROSPIKE_C_HOME:
+        aerospike_c_prefix = AEROSPIKE_C_HOME + '/target/Darwin-x86_64'
+    else:
+        aerospike_c_prefix = '/usr/local'
+
+    #---------------------------------------------------------------------------
+    # Mac Specific Compiler and Linker Settings
+    #---------------------------------------------------------------------------
+
     extra_compile_args = extra_compile_args + [
       '-D_DARWIN_UNLIMITED_SELECT',
       '-undefined','dynamic_lookup',
@@ -42,31 +76,138 @@ if osx:
       '-DMARCH_x86_64'
       ]
     
-    library_dirs = ['/usr/local/lib','/usr/lib'] + library_dirs
-    library_extname = '.dylib'
     libraries = libraries + ['lua']
 
-else:
-    
-    extra_compile_args.append('-rdynamic')
-    extra_compile_args.append('-DMARCH_x86_64')
-    
-    library_dirs = ['/usr/local/lib','/usr/lib'] + library_dirs
+    library_ext = '.dylib'
+
+elif LINUX:
+
+    if AEROSPIKE_C_HOME:
+        aerospike_c_prefix = AEROSPIKE_C_HOME + '/target/Linux-x86_64'
+    else:
+        aerospike_c_prefix = '/usr'
+
+    #---------------------------------------------------------------------------
+    # Mac Specific Compiler and Linker Settings
+    #---------------------------------------------------------------------------
+
+    extra_compile_args = extra_compile_args + [
+        '-rdynamic',
+        '-DMARCH_x86_64'
+        ]
+
     libraries = libraries + ['rt']
-    library_extname = '.so'
-    
+
+    #---------------------------------------------------------------------------
+    # The following will attempt to resolve the Lua 5.1 library dependency
+    #---------------------------------------------------------------------------
+
     lua_aliases = ['lua','lua5.1']
 
     liblua = None
-    for d in library_dirs:
-      for f in lua_aliases:
-        library = d + '/lib' + f + library_extname
+    for directory in library_search:
+      for alias in lua_aliases:
+        library = directory + '/lib' + alias + '.so'
         if os.path.isfile(library):
-          libraries = libraries + [f]
-          liblua = f
-          break
+            libraries = libraries + [alias]
+            liblua = alias
+            print("info: liblua found: ", library, file=sys.stdout)
+            break
       if liblua:
         break
+
+    if not liblua:
+        print("error: liblua was not found:\n   ", "\n    ".join(library_search), file=sys.stderr)
+        sys.exit(1)
+
+else:
+    print("error: OS not supported:", PLATFORM, file=sys.stderr)
+    sys.exit(1)
+
+
+################################################################################
+# RESOLVE AEROSPIKE C CLIENT DEPENDNECY
+################################################################################
+
+if not os.path.isdir(aerospike_c_prefix):
+    print("error: Directory not found:", aerospike_c_prefix, file=sys.stderr)
+    sys.exit(1)
+
+#-------------------------------------------------------------------------------
+# Check for aerospike.h
+#-------------------------------------------------------------------------------
+
+aerospike_h = aerospike_c_prefix + '/include/aerospike/aerospike.h'
+
+if not os.path.isfile(aerospike_h):
+    print("error: File not found:", PLATFORM, file=sys.stderr)
+    sys.exit(1)
+
+include_dirs = [
+    aerospike_c_prefix + '/include', 
+    aerospike_c_prefix + '/include/ck'
+] + include_dirs
+
+
+#-------------------------------------------------------------------------------
+# Check for libaerospike
+#-------------------------------------------------------------------------------
+
+aerospike_a = aerospike_c_prefix + '/lib/libaerospike.a'
+aerospike_a_found = False
+
+aerospike_so = aerospike_c_prefix + '/lib/libaerospike' + library_ext
+aerospike_so_found = False
+
+if os.path.isfile(aerospike_a):
+    aerospike_a_found = True
+    print("info: libaerospike found:", aerospike_a, file=sys.stdout)
+
+if os.path.isfile(aerospike_so):
+    aerospike_so_found = True
+    print("info: libaerospike found:", aerospike_so, file=sys.stdout)
+
+if aerospike_a_found and aerospike_c_static :
+
+    #---------------------------------------------------------------------------
+    # Using libaerospike.a
+    #---------------------------------------------------------------------------
+
+    print("info: Linking libaerospike statically:", aerospike_a, file=sys.stdout)
+
+    extra_objects = [aerospike_a] + extra_objects
+
+elif aerospike_so_found:
+
+    #---------------------------------------------------------------------------
+    # Using libaerospike.so
+    #---------------------------------------------------------------------------
+
+    print("info: Linking libaerospike dynamically:", aerospike_so, file=sys.stdout)
+
+    library_dirs = [
+        aerospike_c_prefix + '/lib'
+    ] + library_dirs
+
+    libraries = ['aerospike'] + libraries
+
+else:
+
+    print("error: Not able to find libaerospike: ", aerospike_so, aerospike_a, file=sys.stderr)
+    sys.exit(1)
+
+#---------------------------------------------------------------------------
+# Environment Variables
+#---------------------------------------------------------------------------
+
+os.putenv('CPATH', ':'.join(include_dirs))
+os.putenv('LD_LIBRARY_PATH', ':'.join(library_dirs))
+os.putenv('DYLD_LIBRARY_PATH', ':'.join(library_dirs))
+
+
+################################################################################
+# SETUP
+################################################################################
 
 setup(
     name        = 'aerospike-client-python', 
@@ -107,12 +248,11 @@ setup(
           'src/main/policy.c',
           'src/main/predicates.c'
         ], 
-        include_dirs = [
-          'src/include'
-        ],
+        include_dirs = include_dirs,
         library_dirs = library_dirs,
         libraries = libraries,
-        extra_objects = [],
+        extra_link_args = extra_link_args,
+        extra_objects = extra_objects,
         extra_compile_args = extra_compile_args
       )
     ]
