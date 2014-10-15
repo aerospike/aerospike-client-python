fe443bdfe929412fca7c4be341a49013947d5423
Chris Stivers <chris@aerospike.com>
2014-10-15 12:34:45 -0700

    added support for oracle linux

--- scripts/aerospike-client-c.sh
+++ scripts/aerospike-client-c.sh
@@ -51,7 +51,7 @@ detect_linux()
 
     case ${DIST_NAME} in
 
-      "centos6" | "redhatenterpriceserver6" | "fedora20")
+      "centos6" | "redhatenterpriceserver6" | "fedora20" | "oracleserver6" )
         echo "el6" "rpm"
         return 0
         ;;
@@ -79,7 +79,7 @@ detect_linux()
     dist=$(cat /etc/issue | tr '[:upper:]' '[:lower:]')
     case ${dist} in
 
-      "centos"* | "red hat enterprise linux"* | "fedora"* )
+      "centos"* | "red hat enterprise linux"* | "fedora"* | "oracleserver"* )
         echo "el6" "rpm"
         return 0
         ;;
