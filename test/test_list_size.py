3ffa1ca97bcb14430a375c134f261f73d6c5083c
PratimaMane-GSLab <pratima.mane@gslab.com>
2016-01-04 15:04:16 +0530

    Renamed unit test file from test_list_count.py to test_list_size.py

--- test/test_list_size.py
+++ test/test_list_size.py
@@ -13,7 +13,7 @@ except:
     print "Please install aerospike python client."
     sys.exit(1)
 
-class TestListCount(object):
+class TestListSize(object):
     def setup_class(cls):
         """
         Setup method.
@@ -21,18 +21,18 @@ class TestListCount(object):
         hostlist, user, password = TestBaseClass.get_hosts()
         config = {'hosts': hostlist}
         if user == None and password == None:
-            TestListCount.client = aerospike.client(config).connect()
+            TestListSize.client = aerospike.client(config).connect()
         else:
-            TestListCount.client = aerospike.client(config).connect(user, password)
+            TestListSize.client = aerospike.client(config).connect(user, password)
 
     def teardown_class(cls):
-        TestListCount.client.close()
+        TestListSize.client.close()
 
     def setup_method(self, method):
         for i in xrange(5):
             key = ('test', 'demo', i)
             rec = {'name': 'name%s' % (str(i)), 'contact_no': [i, i+1], 'city' : ['Pune', 'Dehli']}
-            TestListCount.client.put(key, rec)
+            TestListSize.client.put(key, rec)
 
     def teardown_method(self, method):
         """
@@ -41,21 +41,21 @@ class TestListCount(object):
         #time.sleep(1)
         for i in xrange(5):
             key = ('test', 'demo', i)
-            TestListCount.client.remove(key)
+            TestListSize.client.remove(key)
 
-    def test_list_count_with_correct_paramters(self):
+    def test_list_size_with_correct_paramters(self):
         """
-        Invoke list_count() with correct parameters
+        Invoke list_size() with correct parameters
         """
         key = ('test', 'demo', 1)
 
-        count = TestListCount.client.list_count(key, 'contact_no')
+        count = TestListSize.client.list_size(key, 'contact_no')
 
         assert 2 == count
 
-    def test_list_count_list_with_correct_policy(self):
+    def test_list_size_list_with_correct_policy(self):
         """
-        Invoke list_count() count list elements with correct policy
+        Invoke list_size() count list elements with correct policy
         """
         key = ('test', 'demo', 2)
         policy = {
@@ -63,36 +63,36 @@ class TestListCount(object):
             'retry': aerospike.POLICY_RETRY_ONCE,
             'commit_level': aerospike.POLICY_COMMIT_LEVEL_MASTER
         }
-        count = TestListCount.client.list_count(key, "contact_no", {}, policy)
+        count = TestListSize.client.list_size(key, "contact_no", {}, policy)
 
         assert 2 == count
 
-    def test_list_count_with_no_parameters(self):
+    def test_list_size_with_no_parameters(self):
         """
-        Invoke list_count() without any mandatory parameters.
+        Invoke list_size() without any mandatory parameters.
         """
         with pytest.raises(TypeError) as typeError:
-            TestListCount.client.list_count()
+            TestListSize.client.list_size()
         assert "Required argument 'key' (pos 1) not found" in typeError.value
 
-    def test_list_count_with_incorrect_policy(self):
+    def test_list_size_with_incorrect_policy(self):
         """
-        Invoke list_count() with incorrect policy
+        Invoke list_size() with incorrect policy
         """
         key = ('test', 'demo', 1)
         policy = {
             'timeout': 0.5
         }
         try:
-            TestListCount.client.list_count(key, "contact_no", {}, policy)
+            TestListSize.client.list_size(key, "contact_no", {}, policy)
 
         except ParamError as exception:
             assert exception.code == -2
             assert exception.msg == "timeout is invalid"
 
-    def test_list_count_with_nonexistent_key(self):
+    def test_list_size_with_nonexistent_key(self):
         """
-        Invoke list_count() with non-existent key
+        Invoke list_size() with non-existent key
         """
         charSet = 'abcdefghijklmnopqrstuvwxyz1234567890'
         minLength = 5
@@ -101,13 +101,13 @@ class TestListCount(object):
         key = ('test', 'demo', ''.join(map(lambda unused :
             random.choice(charSet), range(length)))+".com")
         try:
-            TestListCount.client.list_count(key, "contact_no")
+            TestListSize.client.list_size(key, "contact_no")
         except RecordNotFound as exception:
             assert exception.code == 2
 
-    def test_list_count_with_nonexistent_bin(self):
+    def test_list_size_with_nonexistent_bin(self):
         """
-        Invoke list_count() with non-existent bin
+        Invoke list_size() with non-existent bin
         """
         key = ('test', 'demo', 1)
         charSet = 'abcdefghijklmnopqrstuvwxyz1234567890'
@@ -117,63 +117,63 @@ class TestListCount(object):
         bin = ''.join(map(lambda unused :
             random.choice(charSet), range(length)))+".com"
         try:
-            TestListCount.client.list_count(key, bin)
+            TestListSize.client.list_size(key, bin)
         except RecordNotFound as exception:
             assert exception.code == 2
 
-    def test_list_count_with_extra_parameter(self):
+    def test_list_size_with_extra_parameter(self):
         """
-        Invoke list_count() with extra parameter.
+        Invoke list_size() with extra parameter.
         """
         key = ('test', 'demo', 1)
         policy = {'timeout': 1000}
         with pytest.raises(TypeError) as typeError:
-            TestListCount.client.list_count(key, "contact_no", {}, policy, "")
+            TestListSize.client.list_size(key, "contact_no", {}, policy, "")
 
-        assert "list_count() takes at most 4 arguments (5 given)" in typeError.value
+        assert "list_size() takes at most 4 arguments (5 given)" in typeError.value
 
-    def test_list_count_policy_is_string(self):
+    def test_list_size_policy_is_string(self):
         """
-        Invoke list_count() with policy is string
+        Invoke list_size() with policy is string
         """
         key = ('test', 'demo', 1)
         try:
-            TestListCount.client.list_count(key, "contact_no", {}, "")
+            TestListSize.client.list_size(key, "contact_no", {}, "")
 
         except ParamError as exception:
             assert exception.code == -2
             assert exception.msg == "policy must be a dict"
 
-    def test_list_count_key_is_none(self):
+    def test_list_size_key_is_none(self):
         """
-        Invoke list_count() with key is none
+        Invoke list_size() with key is none
         """
         try:
-            TestListCount.client.list_count(None, "contact_no")
+            TestListSize.client.list_size(None, "contact_no")
 
         except ParamError as exception:
             assert exception.code == -2
             assert exception.msg == "key is invalid"
 
-    def test_list_count_bin_is_none(self):
+    def test_list_size_bin_is_none(self):
         """
-        Invoke list_count() with bin is none
+        Invoke list_size() with bin is none
         """
         key = ('test', 'demo', 1)
         try:
-            TestListCount.client.list_count(key, None)
+            TestListSize.client.list_size(key, None)
 
         except ParamError as exception:
             assert exception.code == -2
             assert exception.msg == "Bin name should be of type string"
 
-    def test_list_count_meta_type_integer(self):
+    def test_list_size_meta_type_integer(self):
         """
-        Invoke list_count() with metadata input is of type integer
+        Invoke list_size() with metadata input is of type integer
         """
         key = ('test', 'demo', 1)
         try:
-            TestListCount.client.list_count(key, "contact_no", 888)
+            TestListSize.client.list_size(key, "contact_no", 888)
 
         except ParamError as exception:
             assert exception.code == -2
