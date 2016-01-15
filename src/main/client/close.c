1cd154a836629a20ccc2fdea627c416d5c534136
RaunakSabhani-GSLab <raunak.sabhani@gslab.com>
2016-01-15 16:11:39 +0530

    Fix for bug

--- src/main/client/close.c
+++ src/main/client/close.c
@@ -41,33 +41,38 @@
  */
 PyObject * AerospikeClient_Close(AerospikeClient * self, PyObject * args, PyObject * kwds)
 {
-    as_error err;
-    char *alias_to_search = NULL;
+	as_error err;
+	char *alias_to_search = NULL;
 
-    // Initialize error
-    as_error_init(&err);
+	// Initialize error
+	as_error_init(&err);
 
-    if (!self || !self->as) {
-        as_error_update(&err, AEROSPIKE_ERR_PARAM, "Invalid aerospike object");
-        goto CLEANUP;
-    }
+	if (!self || !self->as) {
+		as_error_update(&err, AEROSPIKE_ERR_PARAM, "Invalid aerospike object");
+		goto CLEANUP;
+	}
 
-    if (!self->is_conn_16) {
-        as_error_update(&err, AEROSPIKE_ERR_CLUSTER, "No connection to aerospike cluster");
-        goto CLEANUP;
-    }
+	if (!self->is_conn_16) {
+		as_error_update(&err, AEROSPIKE_ERR_CLUSTER, "No connection to aerospike cluster");
+		goto CLEANUP;
+	}
 
-    alias_to_search = return_search_string(self->as);
-    PyObject *py_persistent_item = NULL;
+	alias_to_search = return_search_string(self->as);
+	PyObject *py_persistent_item = NULL;
 
-    py_persistent_item = PyDict_GetItemString(py_global_hosts, alias_to_search); 
-    if (py_persistent_item) {
-        close_aerospike_object(self->as, &err, alias_to_search, py_persistent_item);
-        self->is_conn_16 = false;
-        self->as = NULL;
-    }
-    PyMem_Free(alias_to_search);
-    alias_to_search = NULL;
+	py_persistent_item = PyDict_GetItemString(py_global_hosts, alias_to_search); 
+	if (py_persistent_item) {
+		close_aerospike_object(self->as, &err, alias_to_search, py_persistent_item);
+	} else {
+		aerospike_close(self->as, &err);
+		Py_BEGIN_ALLOW_THREADS
+		aerospike_destroy(self->as);
+		Py_END_ALLOW_THREADS
+	}
+	self->is_conn_16 = false;
+	self->as = NULL;
+	PyMem_Free(alias_to_search);
+	alias_to_search = NULL;
 
     if ( err.code != AEROSPIKE_OK ) {
         PyObject * py_err = NULL;
