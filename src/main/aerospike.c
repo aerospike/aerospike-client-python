c335276091a6a1d30f900bd3a8d168706e61a86d
jboone100 <jboone@aerospike.com>
2016-02-12 10:51:54 -0800

    Update version to 2.0.0

--- src/main/aerospike.c
+++ src/main/aerospike.c
@@ -76,7 +76,7 @@ AerospikeConstants operator_constants[] = {
 MOD_INIT(aerospike)
 {
 
-	const char version[8] = "1.0.61";
+	const char version[8] = "2.0.0";
 	// Makes things "thread-safe"
 	PyEval_InitThreads();
 	int i = 0;
