069feb37953aad06b7d245241e944084e74537b6
Maxence Dolle <maxence@realytics.io>
2016-05-20 17:12:00 +0200

    Allow installation on Ubuntu 16.04+

--- scripts/aerospike-client-c.sh
+++ scripts/aerospike-client-c.sh
@@ -68,7 +68,7 @@ detect_linux()
         return 0
         ;;
 
-      "ubuntu12" | "ubuntu13" | "ubuntu14" | "ubuntu15" | "linuxmint17" )
+      "ubuntu12" | "ubuntu13" | "ubuntu14" | "ubuntu15" | "ubuntu16" | "linuxmint17" )
         echo "ubuntu12"  "deb"
         return 0
         ;;
@@ -146,7 +146,7 @@ detect_linux()
       "ubuntu"* )
         vers=$(lsb_release -r -s)
         case ${vers} in
-          "12."* | "13."* | "14."* | "15."* )
+          "12."* | "13."* | "14."* | "15."* | "16."* )
             echo "ubuntu12"  "deb"
             return 0
             ;;
