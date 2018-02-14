57f4f25089e57bdeaa43622c607b866cdae12038
Robert Marks <robert@aerospike.com>
2018-02-14 21:11:13 +0000

    Improve server version checking

--- test/new_tests/conftest.py
+++ test/new_tests/conftest.py
@@ -58,7 +58,7 @@ def as_connection(request):
             if value is not None:
                 versionlist = value[value.find("build") +
                                     6:value.find("\n")].split(".")
-                request.cls.server_version = [int(n) for n in versionlist]
+                request.cls.server_version = [int(n) for n in versionlist[:2]]
                 if ((int(versionlist[0]) > 3) or
                    (int(versionlist[0]) == 3 and int(versionlist[1]) >= 7)):
                     request.cls.skip_old_server = False
