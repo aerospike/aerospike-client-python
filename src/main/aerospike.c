ecb1246fe37d6a9edff75723229cf0173372ae33
dwelch-spike <53876192+dwelch-spike@users.noreply.github.com>
2020-02-20 13:18:49 -0800

    Update version

--- src/main/aerospike.c
+++ src/main/aerospike.c
@@ -95,7 +95,7 @@ AerospikeConstants operator_constants[] = {
 MOD_INIT(aerospike)
 {
 
-	const char version[8] = "3.10.0";
+	const char version[8] = "3.11.0";
 	// Makes things "thread-safe"
 	PyEval_InitThreads();
 	int i = 0;
