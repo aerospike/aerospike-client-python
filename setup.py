86a8467e9294f4f864ec942c854d6558fd24b9f0
Dylan <dwelch@aerospike.com>
2020-02-20 11:45:12 -0800

    python3.8 Travis

--- setup.py
+++ setup.py
@@ -214,9 +214,9 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Operating System :: MacOS :: MacOS X',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Database'
     ],
