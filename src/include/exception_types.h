06f64c048b3d1478adc582745f9e4922b4b5c946
dylan <dwelch@aerospike.com>
2021-01-20 16:42:04 -0800

    CLIENT-1427 add support for error code 28, LostConflict.

--- src/include/exception_types.h
+++ src/include/exception_types.h
@@ -20,7 +20,7 @@
 
 // These sizes correspond to the structs in exception.c.
 #define RECORD_EXCEPTION_COUNT 10
-#define SERVER_EXCEPTION_COUNT 12
+#define SERVER_EXCEPTION_COUNT 13
 #define INDEX_EXCEPTION_COUNT 6
 #define ADMIN_EXCEPTION_COUNT 17
 
