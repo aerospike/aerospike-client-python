51badf9578d4311709e91af41f44ab7d1a4a52f2
Ramaraj Pandian <rpandian@aerospike.com>
2021-07-13 15:50:37 -0700

    build only these supported python env PYTHONS, updated

--- scripts/manylinux2014build.sh
+++ scripts/manylinux2014build.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
-
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" wheel ./ -w /work/tempwheels
+
+echo $PYTHONS
+IFS=',' read -ra ADDR <<< "$PYTHONS"
+
+for i in "${ADDR[@]}"; do
+    echo "compiling  $i "${i}/pip" wheel ./ -w /work/tempwheels ..."
+    "${i}/pip" wheel ./ -w /work/tempwheels
 done
 
-# Bundle external shared libraries into the wheels
-for whl in /work/tempwheels/*.whl; do
-    auditwheel repair "$whl" --plat manylinux2014_x86_64 -w /work/wheels/
+for i in "${ADDR[@]}"; do
+    ${i}/pip install aerospike -f /work/wheels/
+    ${i}/python -c "import aerospike; print('Installed aerospike version{}'.format(aerospike.__version__))"
 done
 
-for PYBIN in /opt/python/*/bin/; do
-    ${PYBIN}/pip install aerospike -f /work/wheels/
-    ${PYBIN}/python -c "import aerospike; print('Installed aerospike version{}'.format(aerospike.__version__))"
-done
+echo "Building wheel $PYTHONS are done"
