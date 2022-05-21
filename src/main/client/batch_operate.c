53a03e00ca9874ddf0b4bdee41dab0801bd5b899
dwelch-spike <dwelch@aerospike.com>
2022-05-20 18:53:25 -0700

    properly decref py_batch_record

--- src/main/client/batch_operate.c
+++ src/main/client/batch_operate.c
@@ -85,9 +85,8 @@ static bool batch_operate_cb(const as_batch_result *results, uint32_t n,
 		}
 
 		PyList_Append(data->py_results, py_batch_record);
+		Py_DECREF(py_batch_record);
 	}
-
-	Py_XDECREF(py_batch_record);
 
 	PyGILState_Release(gstate);
 	return success;
