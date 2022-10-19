14adb530008869f68bac55a1cb3b67178f298c44
juliannguyen4 <109386615+juliannguyen4@users.noreply.github.com>
2022-10-19 13:05:50 -0700

    Get all C source files under src/main

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ from setuptools import setup, Extension
 from distutils.command.build import build
 from distutils.command.clean import clean
 import io
+import glob
 
 ################################################################################
 # ENVIRONMENT VARIABLES
@@ -222,6 +223,9 @@ class CClientClean(clean):
         self.execute(clean, [], 'Clean core aerospike-client-c')
 
 
+# Get all C source files in src/main
+src_files = glob.glob("src/main/**/*.c", recursive=True)
+
 setup(
     version=version.strip(),
     # Data files
@@ -231,94 +235,7 @@ setup(
             'aerospike',
 
             # Source Files
-            [
-                'src/main/aerospike.c',
-                'src/main/exception.c',
-                'src/main/log.c',
-                'src/main/client/type.c',
-                'src/main/client/apply.c',
-                'src/main/client/bit_operate.c',
-                'src/main/client/cdt_list_operate.c',
-                'src/main/client/cdt_map_operate.c',
-                'src/main/client/hll_operate.c',
-                'src/main/client/expression_operations.c',
-                'src/main/client/cdt_operation_utils.c',
-                'src/main/client/close.c',
-                'src/main/client/connect.c',
-                'src/main/client/exists.c',
-                'src/main/client/exists_many.c',
-                'src/main/client/get.c',
-                'src/main/client/get_async.c',
-                'src/main/client/put_async.c',
-                'src/main/client/get_many.c',
-                'src/main/client/batch_get_ops.c',
-                'src/main/client/select_many.c',
-                'src/main/client/info_single_node.c',
-                'src/main/client/info_random_node.c',
-                'src/main/client/info_node.c',
-                'src/main/client/info.c',
-                'src/main/client/put.c',
-                'src/main/client/operate_list.c',
-                'src/main/client/operate_map.c',
-                'src/main/client/operate.c',
-                'src/main/client/query.c',
-                'src/main/client/remove.c',
-                'src/main/client/scan.c',
-                'src/main/client/select.c',
-                'src/main/client/tls_info_host.c',
-                'src/main/client/truncate.c',
-                'src/main/client/admin.c',
-                'src/main/client/udf.c',
-                'src/main/client/sec_index.c',
-                'src/main/serializer.c',
-                'src/main/client/remove_bin.c',
-                'src/main/client/get_key_digest.c',
-                'src/main/query/type.c',
-                'src/main/query/apply.c',
-                'src/main/query/add_ops.c',
-                'src/main/query/paginate.c',
-                'src/main/query/get_parts.c',
-                'src/main/query/foreach.c',
-                'src/main/query/results.c',
-                'src/main/query/select.c',
-                'src/main/query/where.c',
-                'src/main/query/execute_background.c',
-                'src/main/scan/type.c',
-                'src/main/scan/foreach.c',
-                'src/main/scan/results.c',
-                'src/main/scan/select.c',
-                'src/main/scan/execute_background.c',
-                'src/main/scan/apply.c',
-                'src/main/scan/add_ops.c',
-                'src/main/scan/paginate.c',
-                'src/main/scan/get_parts.c',
-                'src/main/geospatial/type.c',
-                'src/main/geospatial/wrap.c',
-                'src/main/geospatial/unwrap.c',
-                'src/main/geospatial/loads.c',
-                'src/main/geospatial/dumps.c',
-                'src/main/policy.c',
-                'src/main/conversions.c',
-                'src/main/convert_expressions.c',
-                'src/main/policy_config.c',
-                'src/main/calc_digest.c',
-                'src/main/predicates.c',
-                'src/main/tls_config.c',
-                'src/main/global_hosts/type.c',
-                'src/main/nullobject/type.c',
-                'src/main/cdt_types/type.c',
-                'src/main/key_ordered_dict/type.c',
-                'src/main/client/set_xdr_filter.c',
-                'src/main/client/get_expression_base64.c',
-                'src/main/client/get_cdtctx_base64.c',
-                'src/main/client/get_nodes.c',
-                'src/main/convert_partition_filter.c',
-                'src/main/client/get_key_partition_id.c',
-                'src/main/client/batch_write.c',
-                'src/main/client/batch_operate.c',
-                'src/main/client/batch_remove.c',
-                'src/main/client/batch_apply.c'
-            ],
+            src_files,
 
             # Compile
             include_dirs=include_dirs,
