a27a62c5445dac54b8dc3cf62120890fccef5f5d
juliannguyen4 <109386615+juliannguyen4@users.noreply.github.com>
2022-10-27 22:27:21 -0700

    Also set default ttl

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,5 @@ cd aerospike-server-community-6.1.0.1-ubuntu20.04/
 sudo ./asinstall
 sudo systemctl start aerospike
 # Configure aerospike server to run tests
-asinfo -v "set-config:context=namespace;id=test;allow-ttl-without-nsup=true"
+asinfo -v "set-config:context=namespace;id=test;allow-ttl-without-nsup=true" -l
+asinfo -v "set-config:context=namespace;id=test;default-ttl=3d" -l
