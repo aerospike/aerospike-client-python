ca2abf0b79bd7916e711f95acf39e1272d226bd2
rpandian-spike <rpandian@aerospike.com>
2022-11-04 10:07:17 -0700

    check for valid meta

--- src/main/client/batch_write.c
+++ src/main/client/batch_write.c
@@ -272,7 +272,9 @@ static PyObject *AerospikeClient_BatchWriteInvoke(AerospikeClient *self,
 
         as_operations *ops = NULL;
         if (batch_type == AS_BATCH_READ ||
-            batch_type == AS_BATCH_WRITE && (py_ops_size || py_meta)) {
+            batch_type == AS_BATCH_WRITE && (py_ops_size || 
+            (py_meta != NULL && py_meta != Py_None))) {
+                
             ops = as_operations_new(py_ops_size);
             garb->ops_to_free = ops;
 
@@ -455,6 +457,10 @@ static PyObject *AerospikeClient_BatchWriteInvoke(AerospikeClient *self,
 
     // populate results
     as_vector *res_list = &batch_records.list;
+    // if (py_batch_records_size == 0) {
+    //     printf("aerospike_batch_write failed with no result");
+    // }
+
     for (Py_ssize_t i = 0; i < py_batch_records_size; i++) {
         PyObject *py_batch_record = PyList_GetItem(py_batch_records, i);
 
@@ -498,7 +504,10 @@ static PyObject *AerospikeClient_BatchWriteInvoke(AerospikeClient *self,
                 PyObject_SetAttrString(py_batch_record, FIELD_NAME_BATCH_RECORD,
                                        Py_None);
             }
-        }
+        } 
+        // else {
+        //     printf("aerospike_batch_write record:%d failed:%d", i, *result_code );
+        // }
     }
 
     goto CLEANUP3;
