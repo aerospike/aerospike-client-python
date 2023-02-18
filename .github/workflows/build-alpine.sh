ecab7c54f39f4dfe00ce8b2ddc4d57a87baabba7
juliannguyen4 <109386615+juliannguyen4@users.noreply.github.com>
2023-02-17 16:00:32 -0800

    Use script again

---
+++ .github/workflows/build-alpine.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+apk add git py3-pip python3-dev zlib-dev automake make musl-dev gcc openssl-dev lua-dev libuv-dev doxygen graphviz
+git clone --branch $1 --recurse-submodules https://github.com/aerospike/aerospike-client-python
+cd aerospike-client-python/
+
+python3 -m pip install build
+python3 -m build
+python3 -m pip install dist/*.whl
+
+cd test/
+python3 -m pip install pytest
+python3 -m pytest new_tests/
