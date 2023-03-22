ae12454578b98ec90648cbea4feadcef31058d44
juliannguyen4 <109386615+juliannguyen4@users.noreply.github.com>
2023-03-22 09:50:55 -0700

    add scripts

---
+++ .github/workflows/build-rhel9.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+set -x
+
+test "a" == "b"
+
+# Update packages to get latest git version
+yum update -y
+yum install -y git openssl-devel glibc-devel autoconf automake libtool zlib-devel openssl-devel python-devel
+
+git clone --branch $1 --recurse-submodules https://github.com/aerospike/aerospike-client-python
+cd aerospike-client-python/
+
+python3 -m pip install build
+python3 -m build
+python3 -m pip install dist/*.whl
+
+cd test
+python3 -m pip install -r requirements.txt
+python3 -m pytest new_tests/
+
+# Wheel install test
+pip uninstall -y aerospike
+python3 -m pip install aerospike
