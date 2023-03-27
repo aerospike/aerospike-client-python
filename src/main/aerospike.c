b4461824484d1f32abe1dddc5eb8c2d24384f866
juliannguyen4 <109386615+juliannguyen4@users.noreply.github.com>
2023-03-27 12:11:05 -0700

    Bump version to 11.0.0

--- src/main/aerospike.c
+++ src/main/aerospike.c
@@ -172,7 +172,7 @@ static int Aerospike_Clear(PyObject *aerospike)
 MOD_INIT(aerospike)
 {
 
-    const char version[8] = "10.0.1";
+    const char version[8] = "11.0.0";
     // Makes things "thread-safe"
     PyEval_InitThreads();
     int i = 0;
