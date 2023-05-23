5f2dff693659fbe1dee8f9de83da3eb53160fc82
juliannguyen4 <109386615+juliannguyen4@users.noreply.github.com>
2023-05-23 09:35:35 -0700

    Add cat 1 and 2 test for amz linux 2023

--- .github/workflows/build-rpm-based.sh
+++ .github/workflows/build-rpm-based.sh
@@ -5,6 +5,8 @@ set -x
 
 # Executed inside the aerospike-client-python directory
 yum install -y openssl-devel glibc-devel autoconf automake libtool zlib-devel openssl-devel python-devel
+# Amazon Linux 2023's default python doesn't come with pip
+python3 -m ensurepip
 python3 -m pip install build
 python3 -m build
 python3 -m pip install dist/*.whl
