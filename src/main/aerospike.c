a560fea5e059509781e08bcbc144ddb180c1b61d
juliannguyen4 <juliannguyen4@users.noreply.github.com>
2023-07-17 20:43:57 +0000

    Bump version to 12.0.0-rc.11

--- src/main/aerospike.c
+++ src/main/aerospike.c
@@ -153,7 +153,7 @@ static int Aerospike_Clear(PyObject *aerospike)
 PyMODINIT_FUNC PyInit_aerospike(void)
 {
 
-    const char version[] = "12.0.0-rc.10";
+    const char version[] = "12.0.0-rc.11";
     // Makes things "thread-safe"
     Py_Initialize();
     int i = 0;
