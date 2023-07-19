ec96549c11e5eb02f4aa87c5d7cb711edd4c6325
juliannguyen4 <109386615+juliannguyen4@users.noreply.github.com>
2023-07-19 12:05:29 -0700

    [CLIENT-2392] Add Debian 12 support (#469)
    * Fix macOS x86 manylinux tests failing by downgrading Ubuntu VM to 20.04

--- src/main/aerospike.c
+++ src/main/aerospike.c
@@ -138,7 +138,7 @@ static int Aerospike_Clear(PyObject *aerospike)
 PyMODINIT_FUNC PyInit_aerospike(void)
 {
 
-    const char version[] = "12.0.0-rc.12";
+    const char version[] = "12.0.0-rc.13";
     // Makes things "thread-safe"
     Py_Initialize();
     int i = 0;
