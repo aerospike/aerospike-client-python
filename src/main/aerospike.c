1e2b0d2f24f7092b5dd62eea404b88ea293d66e1
juliannguyen4 <109386615+juliannguyen4@users.noreply.github.com>
2023-08-16 16:09:04 -0700

    Manually update version to 7.2.0

--- src/main/aerospike.c
+++ src/main/aerospike.c
@@ -153,7 +153,7 @@ static int Aerospike_Clear(PyObject *aerospike)
 MOD_INIT(aerospike)
 {
 
-	const char version[8] = "7.1.1";
+	const char version[8] = "7.2.0";
 	// Makes things "thread-safe"
 	PyEval_InitThreads();
 	int i = 0;
