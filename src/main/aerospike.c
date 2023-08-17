92ea86b2ac471a368e2c3c6b5c22e860822940c3
juliannguyen4 <109386615+juliannguyen4@users.noreply.github.com>
2023-08-17 14:19:35 -0700

    Manually bump version to 6.2.0

--- src/main/aerospike.c
+++ src/main/aerospike.c
@@ -155,7 +155,7 @@ static int Aerospike_Clear(PyObject *aerospike)
 MOD_INIT(aerospike)
 {
 
-	const char version[8] = "6.1.2";
+	const char version[8] = "6.2.0";
 	// Makes things "thread-safe"
 	PyEval_InitThreads();
 	int i = 0;
