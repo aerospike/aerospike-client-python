ef98d7db67ce47a0ad9ae33cd35cc9908510d5a0
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2023-09-26 13:56:01 -0700

    Refactor build wheels workflow

--- setup.py
+++ setup.py
@@ -165,10 +165,6 @@ os.environ['CPATH'] = ':'.join(include_dirs)
 with io.open(os.path.join(CWD, 'README.rst'), "r", encoding='utf-8') as f:
     long_description = f.read()
 
-# Get the version from the relevant file
-with io.open(os.path.join(CWD, 'VERSION'), "r", encoding='utf-8') as f:
-    version = f.read()
-
 BASEPATH = os.path.dirname(os.path.abspath(__file__))
 CCLIENT_PATH = os.path.join(BASEPATH, 'aerospike-client-c')
 
@@ -222,7 +218,6 @@ class CClientClean(clean):
 
 
 setup(
-    version=version.strip(),
     # Data files
     ext_modules=[
         Extension(
