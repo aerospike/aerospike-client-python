88f7f363ad1820fe65c0205f84debacd9d7a0b04
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2023-10-02 08:06:48 -0700

    fix

--- benchmarks/rust-based-client-benchmarks.py
+++ benchmarks/rust-based-client-benchmarks.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-import asyncio
 import pyperf
-import random
 
 import aerospike
 
 client: aerospike.Client = None
 key = ("test", "test", 0)
 
-async def setup():
+def setup():
     global client, key
-    client = await aerospike.client({"hosts": [("127.0.0.1", 3000)]})
+    client = aerospike.client({"hosts": [("127.0.0.1", 3000)]})
     client.put(key, {
         "brand": "Ford",
         "model": "Mustang",
@@ -19,7 +17,7 @@ async def setup():
     })
 
 
-async def put():
+def put():
     global client, key
     client.put(key, {
         "id": 0,
@@ -29,32 +27,30 @@ async def put():
         "fa/ir": "بر آن مردم دیده روشنایی سلامی چو بوی خوش آشنایی",
     })
 
-async def get():
+def get():
     global client, key
     client.get(key)
 
-async def touch():
+def touch():
     global client, key
-    await client.touch(key)
+    client.touch(key)
 
-async def append():
+def append():
     global client, key
-    await client.append(key, {"brand": "+"})
+    client.append(key, {"brand": "+"})
 
-async def prepend():
+def prepend():
     global client, key
-    await client.prepend(key, {"brand": "-"})
+    client.prepend(key, {"brand": "-"})
 
-async def exists():
+def exists():
     global client, key
-    await client.exists(key)
-
-asyncio.run(setup())
+    client.exists(key)
 
 runner = pyperf.Runner()
-runner.bench_async_func('put', put)
-runner.bench_async_func('get', get)
-runner.bench_async_func('touch', touch)
-runner.bench_async_func('append', append)
-runner.bench_async_func('prepend', prepend)
-runner.bench_async_func('exists', exists)
+runner.bench_func('put', put)
+runner.bench_func('get', get)
+runner.bench_func('touch', touch)
+runner.bench_func('append', append)
+runner.bench_func('prepend', prepend)
+runner.bench_func('exists', exists)
