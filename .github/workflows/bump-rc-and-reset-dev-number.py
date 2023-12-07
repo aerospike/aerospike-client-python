f75f8f546890021c28973cde391ac112dcd74af2
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2023-12-07 12:38:31 -0800

    fix

--- .github/workflows/bump-rc-and-reset-dev-number.py
+++ .github/workflows/bump-rc-and-reset-dev-number.py
@@ -1,8 +1,8 @@
 from parver import Version
 import sys
 
-# Take in an old dev version as input
-# e.g 14.0.0rc1.dev1 -> 14.0.0rc2.dev1 -> 14.0.0rc2.dev0
+# Take in the current RC version as input
+# e.g 14.0.0rc1 -> 14.0.0rc2.dev0
 version_string = sys.argv[1]
 version = Version.parse(version_string, strict=True)
 new_version = version.bump_pre("rc", by=1)
