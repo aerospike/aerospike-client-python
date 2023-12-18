d51cde4f61bcc0b194bb095545742f79be32c60a
Dominic Pelini <111786059+DomPeliniAerospike@users.noreply.github.com>
2023-12-18 10:00:01 -0700

    Added an expanding block size and pointer block
    CLIENT-681
    Added an expanding block size and pointer block to avoid several realloc calls

--- src/include/dynamic_pool.h
+++ src/include/dynamic_pool.h
@@ -5,16 +5,20 @@
  * as_bytes containing AS_DYNAMIC_POOL_BLOCK_SIZE as_bytes.
  *******************************************************************************************************
  */
-#define AS_DYNAMIC_POOL_BLOCK_SIZE 1024
+#define AS_DYNAMIC_POOL_BLOCK_SIZE 128
+#define AS_DYNAMIC_POOL_BLOCK_SIZE_MAX 32768
+#define AS_DYNAMIC_POOL_POINTER_BLOCK_SIZE 4
+
 
 typedef struct bytes_dynamic_pool {
     as_bytes** pools;
-    uint32_t current_bytes_id;
-    uint32_t pool_id;
+    uint16_t current_bytes_id;
+    uint16_t pool_id;
+    uint16_t current_block_size;
 } as_dynamic_pool;
 
 #define DYNAMIC_POOL_INIT(dynamic_pool, err)                                                                                                            \
-    BYTES_POOLS(dynamic_pool) = (as_bytes **) malloc(sizeof(as_bytes *));                                                                               \
+    BYTES_POOLS(dynamic_pool) = (as_bytes **) malloc(AS_DYNAMIC_POOL_POINTER_BLOCK_SIZE * sizeof(as_bytes *));                                          \
     if(BYTES_POOLS(dynamic_pool) == NULL){                                                                                                              \
         as_error_update(err, AEROSPIKE_ERR, "Failed to allocated memory for dynamic_pool");                                                             \
     }                                                                                                                                                   \
@@ -23,16 +27,22 @@ typedef struct bytes_dynamic_pool {
         as_error_update(err, AEROSPIKE_ERR, "Failed to allocated memory for dynamic_pool");                                                             \
     }                                                                                                                                                   \
     BYTES_POOL_CNT(dynamic_pool) = 0;                                                                                                                   \
+    BYTES_BLOCK_SIZE(dynamic_pool) = AS_DYNAMIC_POOL_BLOCK_SIZE;                                                                                        \
     BYTES_CNT(dynamic_pool) = 0;
 
 #define DYNAMIC_POOL_RESIZE(dynamic_pool, err)                                                                                                          \
     BYTES_CNT(dynamic_pool) = 0;                                                                                                                        \
     BYTES_POOL_CNT(dynamic_pool)++;                                                                                                                     \
-    BYTES_POOLS(dynamic_pool) = (as_bytes **) realloc(BYTES_POOLS(dynamic_pool), (BYTES_POOL_CNT(dynamic_pool) + 1) * sizeof(as_bytes *));              \
+    if(BYTES_BLOCK_SIZE(dynamic_pool) != AS_DYNAMIC_POOL_BLOCK_SIZE_MAX){                                                                               \
+        BYTES_BLOCK_SIZE(dynamic_pool) = BYTES_BLOCK_SIZE(dynamic_pool) << 1;                                                                           \
+    }                                                                                                                                                   \
+    if(BYTES_POOL_CNT(dynamic_pool) % AS_DYNAMIC_POOL_POINTER_BLOCK_SIZE == 0){                                                                         \
+        BYTES_POOLS(dynamic_pool) = (as_bytes **) realloc(BYTES_POOLS(dynamic_pool), (BYTES_POOL_CNT(dynamic_pool) + AS_DYNAMIC_POOL_POINTER_BLOCK_SIZE) * sizeof(as_bytes *));              \
+    }                                                                                                                                                   \
     if(BYTES_POOLS(dynamic_pool) == NULL){                                                                                                              \
         as_error_update(err, AEROSPIKE_ERR, "Failed to allocated memory for dynamic_pool");                                                             \
     }                                                                                                                                                   \
-    BYTES_POOLS(dynamic_pool)[BYTES_POOL_CNT(dynamic_pool)] = (as_bytes *)malloc(sizeof(as_bytes) * AS_DYNAMIC_POOL_BLOCK_SIZE);                        \
+    BYTES_POOLS(dynamic_pool)[BYTES_POOL_CNT(dynamic_pool)] = (as_bytes *)malloc(sizeof(as_bytes) * BYTES_BLOCK_SIZE(dynamic_pool));                    \
     if(BYTES_POOLS(dynamic_pool)[BYTES_POOL_CNT(dynamic_pool)] == NULL){                                                                                \
         as_error_update(err, AEROSPIKE_ERR, "Failed to allocated memory for dynamic_pool");                                                             \
     }
@@ -49,11 +59,14 @@ typedef struct bytes_dynamic_pool {
 #define BYTES_POOL(dynamic_pool)                                                                                                                        \
     ((as_dynamic_pool *)dynamic_pool)->pools[BYTES_POOL_CNT(dynamic_pool)]
 
+#define BYTES_BLOCK_SIZE(dynamic_pool)                                                                                                                  \
+    (((as_dynamic_pool *)dynamic_pool)->current_block_size)
+
 #define GET_BYTES_POOL(map_bytes, dynamic_pool, err)                                                                                                    \
     if(BYTES_POOLS(dynamic_pool) == NULL){                                                                                                              \
         DYNAMIC_POOL_INIT(dynamic_pool, err)                                                                                                            \
     }                                                                                                                                                   \
-    if (AS_DYNAMIC_POOL_BLOCK_SIZE > BYTES_CNT(dynamic_pool)) {                                                                                         \
+    if (BYTES_BLOCK_SIZE(dynamic_pool) > BYTES_CNT(dynamic_pool)) {                                                                                     \
         map_bytes = &(BYTES_POOL(dynamic_pool)[BYTES_CNT(dynamic_pool)++]);                                                                             \
     }                                                                                                                                                   \
     else {                                                                                                                                              \
@@ -62,13 +75,17 @@ typedef struct bytes_dynamic_pool {
     }
 
 #define POOL_DESTROY(dynamic_pool, freeBytes)                                                                                                           \
+    uint32_t delete_pool_size = AS_DYNAMIC_POOL_BLOCK_SIZE;                                                                                             \
     for (uint32_t pool_num = 0; pool_num < BYTES_POOL_CNT(dynamic_pool); pool_num++){                                                                   \
         if(freeBytes){                                                                                                                                  \
-            for(uint32_t i = 0; i < AS_DYNAMIC_POOL_BLOCK_SIZE; i++ ){                                                                                  \
+            for(uint32_t i = 0; i < delete_pool_size; i++ ){                                                                                            \
                 as_bytes_destroy(&BYTES_POOL(dynamic_pool)[i]);                                                                                         \
             }                                                                                                                                           \
         }                                                                                                                                               \
         cf_free(((as_dynamic_pool *)dynamic_pool)->pools[pool_num]);                                                                                    \
+        if(delete_pool_size != AS_DYNAMIC_POOL_BLOCK_SIZE_MAX){                                                                                         \
+            delete_pool_size = delete_pool_size << 1;                                                                                                   \
+        }                                                                                                                                               \
     }                                                                                                                                                   \
     if(freeBytes){                                                                                                                                      \
         for(uint32_t i = 0; i < BYTES_CNT(dynamic_pool); i++ ){                                                                                         \
