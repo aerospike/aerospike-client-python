46694a689b887929fe1e05853c97b437159b0d8e
Dominic Pelini <111786059+DomPeliniAerospike@users.noreply.github.com>
2024-01-10 12:02:04 -0700

    Update test/new_tests/test_query.py
    Co-authored-by: juliannguyen4 <109386615+juliannguyen4@users.noreply.github.com>

--- test/new_tests/test_query.py
+++ test/new_tests/test_query.py
@@ -809,7 +809,7 @@ class TestQuery(TestBaseClass):
 
     def test_query_with_results_fail_with_operations(self):
         """
-        Invoke query() with multple results() call on same query object
+        Invoke query.results() after adding operations
         """
         ops = [
             operations.increment("testBinName", 1)
