983c1c289945d9c581b708116c3b21ee587d6202
Dominic Pelini <111786059+DomPeliniAerospike@users.noreply.github.com>
2024-01-10 12:03:24 -0700

    Update test/new_tests/test_scan.py
    Co-authored-by: juliannguyen4 <109386615+juliannguyen4@users.noreply.github.com>

--- test/new_tests/test_scan.py
+++ test/new_tests/test_scan.py
@@ -469,7 +469,7 @@ class TestScan(TestBaseClass):
 
     def test_scan_with_results_fail_with_operations(self):
         """
-        Invoke query() with multple results() call on same query object
+        Invoke scan.results() after adding operations
         """
         ops = [
             operations.increment("testBinName", 1)
