bbde64674932222371c01286133ab3094354eef7
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-02-08 10:32:07 -0800

    Add test cleanup code

--- test/new_tests/test_get_put_keyordereddict.py
+++ test/new_tests/test_get_put_keyordereddict.py
@@ -1,19 +1,24 @@
 from aerospike import KeyOrderedDict
 import pytest
+from aerospike import exception as e
 
 
 class TestGetPutOrderedDict:
     @pytest.fixture(autouse=True)
     def setup(self, request, as_connection):
-        pass
+        yield
+        try:
+            self.as_connection.remove(self.key)
+        except e.AerospikeError:
+            pass
 
     def test_get_put_keyordereddict(self):
         bins = {
             "dict": KeyOrderedDict({"f": 6, "e": 5, "d": 4})
         }
-        key = ("test", "demo", 1)
-        self.as_connection.put(key, bins)
+        self.key = ("test", "demo", 1)
+        self.as_connection.put(self.key, bins)
 
-        _, _, res = self.as_connection.get(key)
+        _, _, res = self.as_connection.get(self.key)
         assert res["dict"] == KeyOrderedDict({"f": 6, "e": 5, "d": 4})
         assert type(res["dict"]) == KeyOrderedDict
