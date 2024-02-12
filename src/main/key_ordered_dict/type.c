2c0303ae246ddc91f67ada6b498a9c51da1247d4
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-02-12 14:40:24 -0800

    Remove unnecessary cast to PyObject*

--- src/main/key_ordered_dict/type.c
+++ src/main/key_ordered_dict/type.c
@@ -32,7 +32,7 @@ static PyMethodDef AerospikeKeyOrderedDict_Type_Methods[] = {{NULL}};
 static int AerospikeKeyOrderedDict_Type_Init(PyObject *self, PyObject *args,
                                              PyObject *kwds)
 {
-    return PyDict_Type.tp_init((PyObject *)self, args, kwds);
+    return PyDict_Type.tp_init(self, args, kwds);
 }
 
 /*******************************************************************************
