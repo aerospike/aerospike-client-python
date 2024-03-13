a379775129069c9a3b661deb9b9ca3c571c4b3f4
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-03-13 14:23:30 -0700

    Add more tests

--- test/new_tests/test_metrics.py
+++ test/new_tests/test_metrics.py
@@ -1,6 +1,14 @@
 from aerospike import Client
+from aerospike_helpers.metrics import MetricsPolicy
 
 
 class TestMetrics:
+    def test_enable_metrics(self, as_connection: Client):
+        as_connection.enable_metrics()
+
+    def test_enable_metrics_with_default_metrics_policy(self, as_connection: Client):
+        policy = MetricsPolicy()
+        as_connection.enable_metrics(policy=policy)
+
     def test_disable_metrics(self, as_connection: Client):
         as_connection.disable_metrics()
