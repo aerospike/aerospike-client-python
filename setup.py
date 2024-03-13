b399dc3ca893ea06809c425fec81fa2ce9d492d2
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-03-13 14:10:03 -0700

    add metrics source code

--- setup.py
+++ setup.py
@@ -343,7 +343,8 @@ setup(
                 'src/main/client/batch_operate.c',
                 'src/main/client/batch_remove.c',
                 'src/main/client/batch_apply.c',
-                'src/main/client/batch_read.c'
+                'src/main/client/batch_read.c',
+                'src/main/client/metrics.c'
             ],
 
             # Compile
