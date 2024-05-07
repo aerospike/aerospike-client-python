c5438a5e47af9def764ea97e918ebe9b14635e4a
juliannguyen4 <109386615+juliannguyen4@users.noreply.github.com>
2024-05-07 14:39:09 -0700

    [CLIENT-2856] Docs: Clarify how list order works and add link to explain negative indices (#602)
    * Add test cases that use a negative index value for all list operations that take in an index parameter
    * Docs: Add missing return values for list_insert_items(), list_remove(), list_remove_range(), and list_set() operations

--- aerospike_helpers/operations/list_operations.py
+++ aerospike_helpers/operations/list_operations.py
@@ -22,6 +22,9 @@ This module provides helper functions to produce dictionaries to be used with:
 List operations support nested CDTs through an optional ctx context argument.
 The ctx argument is a list of cdt_ctx context operation objects. See :class:`aerospike_helpers.cdt_ctx`.
 
+For list operations that take in an index parameter, you can use negative values for indexes.
+See this `page <https://aerospike.com/docs/server/guide/data-types/cdt-list#list-terminology>`_.
+
 .. note:: Nested CDT (ctx) requires server version >= 4.6.0
 
 """
@@ -170,7 +173,7 @@ def list_insert_items(bin_name: str, index, values, policy: Optional[dict] = Non
     Creates a list insert items operation.
 
     The list insert items operation inserts items at index: `index` into the list contained
-    in the specified bin.
+    in the specified bin. Server returns list size.
 
     Args:
         bin_name (str): The name of the bin to be operated on.
@@ -277,7 +280,8 @@ def list_remove(bin_name: str, index, ctx: Optional[list] = None):
     """
     Creates a list remove operation.
 
-    The list remove operation removes an item located at `index` in the list specified by `bin_name`
+    The list remove operation removes an item located at `index` in the list specified by `bin_name`.
+    Server returns number of items removed.
 
     Args:
         bin_name (str): The name of the bin containing the item to be removed.
@@ -301,7 +305,7 @@ def list_remove_range(bin_name: str, index, count, ctx: Optional[list] = None):
     Creates a list remove range operation.
 
     The list remove range operation removes `count` items starting at `index`
-    in the list specified by `bin_name`
+    in the list specified by `bin_name`. Server returns number of items removed.
 
     Args:
         bin_name (str): The name of the bin containing the items to be removed.
@@ -346,7 +350,8 @@ def list_clear(bin_name: str, ctx: Optional[list] = None):
 def list_set(bin_name: str, index, value, policy: Optional[dict] = None, ctx: Optional[list] = None):
     """Create a list set operation.
 
-    The list set operations sets the value of the item at `index` to `value`
+    The list set operations sets the value of the item at `index` to `value`.
+    Server does not return a result by default.
 
     Args:
         bin_name (str): The name of the bin containing the list to be operated on.
