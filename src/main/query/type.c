b40d8439edcea37fde56c84aa88785e12f311ecf
Dominic Pelini <111786059+DomPeliniAerospike@users.noreply.github.com>
2024-05-24 06:23:51 -0600

    Update type.c

--- src/main/query/type.c
+++ src/main/query/type.c
@@ -239,7 +239,7 @@ static void AerospikeQuery_Type_Dealloc(AerospikeQuery *self)
 
     if (self->dynamic_pool != NULL) {
         if (self->dynamic_pool->pool != NULL) {
-            DESTROY_DYNAMIC_POOL(self->dynamic_pool, true);
+            DESTROY_DYNAMIC_POOL(self->dynamic_pool, false);
         }
         cf_free(self->dynamic_pool);
     }
