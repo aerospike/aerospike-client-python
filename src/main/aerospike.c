2b455df02f3ad75cea504f611d31668f496a664e
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-05-28 14:57:57 -0700

    oop

--- src/main/aerospike.c
+++ src/main/aerospike.c
@@ -137,8 +137,6 @@ static int Aerospike_Clear(PyObject *aerospike)
 
 PyMODINIT_FUNC PyInit_aerospike(void)
 {
-
-    const char version[] = "15.0.1rc3.dev3";
     // Makes things "thread-safe"
     Py_Initialize();
     int i = 0;
