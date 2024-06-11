744e64f1e911b143ab8fd6518f36eb1ea83d9f2e
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-06-11 14:07:15 -0700

    wip

--- .github/workflows/upload-jfrog-artifacts-to-github.js
+++ .github/workflows/upload-jfrog-artifacts-to-github.js
@@ -7,18 +7,11 @@ exports.upload_jfrog_artifacts_to_github = async (dev_tag_to_test) => {
     const artifacts_path = `./aerospike/${dev_tag_to_test}/artifacts`;
     var files = fs.readdirSync(artifacts_path);
 
-    const python_tag_and_os_regex = /([a-z0-9]+)-[a-z0-9]+-(manylinux|macosx|win)/;
-    const arch_regex = /_([a-z0-9_])\.whl$/;
     for (var i = 0; i < files.length; i++){
       console.log(`Processing ${files[i]}`)
-      // Construct artifact name (i.e build identifier)
-      let python_tag, os, arch;
-      [python_tag, os] = python_tag_and_os_regex.exec(files[i]);
-      [arch] = arch_regex.exec(files[i]);
-      const artifact_name = `{python_tag}-{os}-{arch}.build`;
+      const artifact_name = `${cibw_build_identifier}.build`;
 
       const {id, size} = await artifact.uploadArtifact(
-        // name of the artifact
         artifact_name,
         // files to include (supports absolute and relative paths)
         [`${artifacts_path}/${files[i]}`],
