b18e752fd3079038ab5c28a11163af82a417efa1
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-07-01 11:29:27 -0700

    disable quiet mode to make easier to debug

--- .github/workflows/wait-for-as-server-to-start.bash
+++ .github/workflows/wait-for-as-server-to-start.bash
@@ -5,7 +5,7 @@ set -x
 container_name=$1
 
 while true; do
-    docker exec $container_name asinfo -v status | grep -qE "^ok"
+    docker exec $container_name asinfo -v status | grep -E "^ok"
     docker_return_code=${PIPESTATUS[0]}
     if [[ $docker_return_code -ne 0 ]]; then
         exit 1
