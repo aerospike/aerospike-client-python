59d3ca1d5311506d5e20c6771e92f9dd813cb8ad
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-07-01 16:42:36 -0700

    Revert "Print errors from first command"
    This reverts commit 9c3300a4a53a139ddd9f237ee6d7551ca4a94913.

--- .github/workflows/wait-for-as-server-to-start.bash
+++ .github/workflows/wait-for-as-server-to-start.bash
@@ -12,8 +12,7 @@ while true; do
         user_credentials="--user=admin --password=admin"
     fi
 
-    # We use asinfo since the Aerospike server Docker image doesn't support healthcheck
-    docker exec $container_name asinfo $user_credentials -v status 2>&1 | tee --append /dev/tty | grep -qE "^ok"
+    docker exec $container_name asinfo $user_credentials -v status | grep -qE "^ok"
     return_code=$?
     if [[ $return_code -eq 0 ]]; then
         # Server is ready when asinfo returns ok
