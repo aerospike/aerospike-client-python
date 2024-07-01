2a0226ec53a7adf55470eb6d4f5e022d1c07aa95
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-07-01 14:57:45 -0700

    try

--- .github/workflows/wait-for-as-server-to-start.bash
+++ .github/workflows/wait-for-as-server-to-start.bash
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 set -x
+set -o pipefail
 
 container_name=$1
 
 while true; do
-    # Print first command in case it fails and we need to figure out why
-    # Stderr doesn't get printed for some reason, so redirect it to stdout
-    docker exec $container_name asinfo --user=superuser --password=superuser -v status
-    docker exec $container_name asinfo --user=superuser --password=superuser -v status 2>&1 | tee >(echo) | grep -qE "^ok"
-    docker_return_code=${PIPESTATUS[0]}
-    if [[ $docker_return_code -ne 0 ]]; then
-        exit 1
-    fi
-    grep_return_code=${PIPESTATUS[1]}
+    docker exec $container_name asinfo --user=superuser --password=superuser -v status | grep -qE "^ok"
+    grep_return_code=$1
     if [[ $grep_return_code -eq 0 ]]; then
         # Server is ready when asinfo returns ok
         break
