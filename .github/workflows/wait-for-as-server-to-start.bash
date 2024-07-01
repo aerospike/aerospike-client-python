d7d4cbc1789087222a5414b453db767d4ee7f68c
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-07-01 15:35:45 -0700

    only enable security when necessary

--- .github/workflows/wait-for-as-server-to-start.bash
+++ .github/workflows/wait-for-as-server-to-start.bash
@@ -4,10 +4,14 @@ set -x
 set -o pipefail
 
 container_name=$1
+is_security_enabled=$2
 
 while true; do
-    docker exec $container_name asinfo --user=admin --password=admin -v status
-    docker exec $container_name asinfo --user=admin --password=admin -v status | grep -qE "^ok"
+    if [[ $is_security_enabled == true ]]; then
+        $instance_argument = "--instance security"
+    fi
+
+    docker exec $container_name asinfo $instance_argument -v status | grep -qE "^ok"
     grep_return_code=$?
     if [[ $grep_return_code -eq 0 ]]; then
         # Server is ready when asinfo returns ok
