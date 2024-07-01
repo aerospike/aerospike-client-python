5c25369be001e67dff796a677810da9106da4774
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-07-01 14:54:04 -0700

    Debug

--- .github/workflows/wait-for-as-server-to-start.bash
+++ .github/workflows/wait-for-as-server-to-start.bash
@@ -7,6 +7,7 @@ container_name=$1
 while true; do
     # Print first command in case it fails and we need to figure out why
     # Stderr doesn't get printed for some reason, so redirect it to stdout
+    docker exec $container_name asinfo --user=superuser --password=superuser -v status
     docker exec $container_name asinfo --user=superuser --password=superuser -v status 2>&1 | tee >(echo) | grep -qE "^ok"
     docker_return_code=${PIPESTATUS[0]}
     if [[ $docker_return_code -ne 0 ]]; then
