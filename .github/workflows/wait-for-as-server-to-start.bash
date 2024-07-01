932621ec9e32e49f8d820da79d5bf0708a4ef3f8
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-07-01 11:42:34 -0700

    Revert

--- .github/workflows/wait-for-as-server-to-start.bash
+++ .github/workflows/wait-for-as-server-to-start.bash
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 set -x
-# We only care about the last command's result (i.e grep)
-set -o pipefail
 
 container_name=$1
 
 while true; do
     # Print first command in case it fails and we need to figure out why
     docker exec $container_name asinfo -v status | tee >(echo) | grep -qE "^ok"
-    grep_return_code=$?
+    docker_return_code=${PIPESTATUS[0]}
+    if [[ $docker_return_code -ne 0 ]]; then
+        exit 1
+    fi
+    grep_return_code=${PIPESTATUS[1]}
     if [[ $grep_return_code -eq 0 ]]; then
         # Server is ready when asinfo returns ok
         break
