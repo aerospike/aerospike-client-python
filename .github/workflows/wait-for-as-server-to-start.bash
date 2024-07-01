891ca09faad3be3904d933ce99ab2ea78bcd6a60
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-07-01 15:07:50 -0700

    fix

--- .github/workflows/wait-for-as-server-to-start.bash
+++ .github/workflows/wait-for-as-server-to-start.bash
@@ -6,7 +6,7 @@ set -o pipefail
 container_name=$1
 
 while true; do
-    docker exec $container_name asinfo --user=superuser --password=superuser -v status | grep -qE "^ok"
+    docker exec $container_name asinfo --user=admin --password=admin -v status | grep -qE "^ok"
     grep_return_code=$?
     if [[ $grep_return_code -eq 0 ]]; then
         # Server is ready when asinfo returns ok
