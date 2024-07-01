f326713c5d6262cea76b76dd278c6772777f04b4
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-07-01 07:52:31 -0700

    finish

--- .github/workflows/wait-for-as-server-to-start.bash
+++ .github/workflows/wait-for-as-server-to-start.bash
@@ -1,15 +1,9 @@
-#!/bin/sh
-
-set -x
-
 container_name=$1
 
-while [ true ];
-do
+while true; do
     docker exec $container_name asinfo -v status | grep -qE "^ok"
     return_code=$?
-    if [ $return_code -eq 0 ]
-    then
+    if [[ $return_code -eq 0 ]]; then
         # Server is ready when asinfo returns ok
         break
     fi
