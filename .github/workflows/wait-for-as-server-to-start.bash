810fe884b6300259e5d73b149bd8a73396881934
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-07-01 15:37:06 -0700

    fix

--- .github/workflows/wait-for-as-server-to-start.bash
+++ .github/workflows/wait-for-as-server-to-start.bash
@@ -8,7 +8,7 @@ is_security_enabled=$2
 
 while true; do
     if [[ $is_security_enabled == true ]]; then
-        $instance_argument = "--instance security"
+        instance_argument="--instance security"
     fi
 
     docker exec $container_name asinfo $instance_argument -v status | grep -qE "^ok"
