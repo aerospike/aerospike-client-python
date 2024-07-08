07cd6bb955c2864f1143701557cc154981be558a
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-07-08 14:34:25 -0700

    [CLIENT-3017] CI/CD: Wait for Aerospike server Docker container to be ready using asinfo instead of a time delay (#642)
    - Fix dev-workflow-p1.yml not skipping server release tests when running manually and setting input to false

---
+++ .github/workflows/wait-for-as-server-to-start.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -x
+# Makes sure that if the "docker exec" command fails, it is not ignored
+set -o pipefail
+
+container_name=$1
+is_security_enabled=$2
+
+while true; do
+    if [[ $is_security_enabled == true ]]; then
+        # We need to pass credentials to asinfo if server requires it
+        # TODO: passing in hardcoded credentials since I can't figure out how to use --instance with global astools.conf
+        user_credentials="--user=admin --password=admin"
+    fi
+
+    # An unset variable will have a default empty value
+    # Intermediate step is to print docker exec command's output in case it fails
+    # Sometimes, errors only appear in stdout and not stderr, like if asinfo throws an error because of no credentials
+    # (This is a bug in asinfo since all error messages should be sent to stderr)
+    # But piping and passing stdin to grep will hide the first command's stdout.
+    # grep doesn't have a way to print all lines passed as input.
+    # ack does have an option but it doesn't come installed by default
+    # shellcheck disable=SC2086 # The flags in user credentials should be separate anyways. Not one string
+    if docker exec "$container_name" asinfo $user_credentials -v status | tee >(cat) | grep -qE "^ok"; then
+        # Server is ready when asinfo returns ok
+        break
+    fi
+done
