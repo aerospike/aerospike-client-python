bc7be56f325a1adf373e165c6e5ba1f94a9fee7b
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-07-12 06:41:08 -0700

    Check cluster-stable state

--- .github/workflows/wait-for-as-server-to-start.bash
+++ .github/workflows/wait-for-as-server-to-start.bash
@@ -25,6 +25,7 @@ while true; do
     if docker exec "$container_name" asinfo $user_credentials -v status | tee >(cat) | grep -qE "^ok"; then
         # Server is ready when asinfo returns ok
         echo "Server is ready now."
+        docker exec "$container_name" asinfo $user_credentials -v "cluster-stable"
         docker container inspect "$container_name"
         break
     fi
