0613a60b17e824cd1f98da25b28a661387c710d6
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-08-06 13:28:14 -0700

    oop

---
+++ src/main/pyobject_setattrstringsafe.c
@@ -0,0 +1,5 @@
+#include "macros.h"
+
+extern inline void PyObject_SetAttrStringSafe(PyObject *obj,
+                                              const char *attr_name,
+                                              PyObject *value);
