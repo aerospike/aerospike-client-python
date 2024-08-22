a34aec2e5f720f1daf5990f93d775c187f1753e6
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-08-22 09:01:07 -0700

    fix

--- aerospike/__init__.py
+++ aerospike/__init__.py
@@ -1,3 +1,4 @@
 # aerospike extension module will be stored in this folder (i.e package)
 # Same with the exception and predicates extension modules
 from .aerospike import *
+from . import exception, predicates
