d03bcf9a225ce726fecacd3cd6c79631f4bc7dd1
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-09-17 11:57:31 -0700

    implement transaction class

--- src/main/transaction/type.c
+++ src/main/transaction/type.c
@@ -4,15 +4,46 @@
 
 typedef struct {
     PyObject_HEAD
-    /* Type-specific fields go here. */
-    as_txn *txn;
+        /* Type-specific fields go here. */
+        as_txn *txn;
 } AerospikeTransaction;
 
+static PyObject *AerospikeTransaction_name(AerospikeTransaction *self)
+{
+    uint64_t id = self->txn->id;
+    PyObject *py_id = PyLong_FromUnsignedLong(id);
+    return py_id;
+}
+
+static PyMethodDef AerospikeTransaction_methods[] = {
+    {"id", (PyCFunction)AerospikeTransaction_name, METH_NOARGS,
+     "Return multi-record transaction ID"},
+    {NULL} /* Sentinel */
+};
+
 static PyTypeObject CustomType = {
-    .ob_base = PyVarObject_HEAD_INIT(NULL, 0)
-    .tp_name = "aerospike.Transaction",
+    .ob_base = PyVarObject_HEAD_INIT(NULL, 0).tp_name = "aerospike.Transaction",
     .tp_basicsize = sizeof(AerospikeTransaction),
     .tp_itemsize = 0,
     .tp_flags = Py_TPFLAGS_DEFAULT,
-    .tp_new = PyType_GenericNew,
-};
+    .tp_new = AerospikeTransaction_new,
+    .tp_dealloc = AerospikeTransaction_dealloc};
+
+static void AerospikeTransaction_dealloc(AerospikeTransaction *self)
+{
+    as_txn_destroy(self->txn);
+    Py_TYPE(self)->tp_free((PyObject *)self);
+}
+
+static PyObject *AerospikeTransaction_new(PyTypeObject *type, PyObject *args,
+                                          PyObject *kwds)
+{
+    AerospikeTransaction *self =
+        (AerospikeTransaction *)type->tp_alloc(type, 0);
+    if (self != NULL) {
+        // TODO: how to check if this fails?
+        self->txn = as_txn_create();
+    }
+
+    return (PyObject *)self;
+}
