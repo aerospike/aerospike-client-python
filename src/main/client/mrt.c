667f0bcc63dd4858aaaa6f465adb73c50d0ea821
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-09-18 09:00:06 -0700

    combine two methods into a single helper meth

--- src/main/client/mrt.c
+++ src/main/client/mrt.c
@@ -5,8 +5,9 @@
 #include "types.h"
 #include "client.h"
 
-PyObject *AerospikeClient_Commit(AerospikeClient *self, PyObject *args,
-                                 PyObject *kwds)
+static PyObject *call_c_client_mrt_method(
+    AerospikeClient *self, PyObject *args, PyObject *kwds,
+    as_status (*c_client_api_call)(aerospike *as, as_error *err, as_txn *txn))
 {
     AerospikeTransaction *py_transaction = NULL;
 
@@ -22,7 +23,7 @@ PyObject *AerospikeClient_Commit(AerospikeClient *self, PyObject *args,
     as_error_init(&err);
 
     Py_BEGIN_ALLOW_THREADS
-    aerospike_commit(self->as, &err, py_transaction->txn);
+    c_client_api_call(self->as, &err, py_transaction->txn);
     Py_END_ALLOW_THREADS
 
     if (err.code != AEROSPIKE_OK) {
@@ -33,30 +34,14 @@ PyObject *AerospikeClient_Commit(AerospikeClient *self, PyObject *args,
     Py_RETURN_NONE;
 }
 
+PyObject *AerospikeClient_Commit(AerospikeClient *self, PyObject *args,
+                                 PyObject *kwds)
+{
+    return call_c_client_mrt_method(self, args, kwds, aerospike_commit);
+}
+
 PyObject *AerospikeClient_Abort(AerospikeClient *self, PyObject *args,
                                 PyObject *kwds)
 {
-    AerospikeTransaction *py_transaction = NULL;
-
-    static char *kwlist[] = {"transaction", NULL};
-
-    if (PyArg_ParseTupleAndKeywords(args, kwds, "O!:abort", kwlist,
-                                    AerospikeTransaction_Type,
-                                    (PyObject **)(&py_transaction)) == false) {
-        return NULL;
-    }
-
-    as_error err;
-    as_error_init(&err);
-
-    Py_BEGIN_ALLOW_THREADS
-    aerospike_abort(self->as, &err, py_transaction->txn);
-    Py_END_ALLOW_THREADS
-
-    if (err.code != AEROSPIKE_OK) {
-        raise_exception(&err);
-        return NULL;
-    }
-
-    Py_RETURN_NONE;
+    return call_c_client_mrt_method(self, args, kwds, aerospike_abort);
 }
--- test/new_tests/test_mrt.py
+++ test/new_tests/test_mrt.py
@@ -13,18 +13,12 @@ class TestMRT:
 
     @pytest.mark.parametrize(
         "args",
-        [
-            [],
-            ["invalid_type"],
-        ],
-        # "api_method",
-        # [
-        #     commit,
-        #     abort
-        # ]
+        [],
+        ["string"]
     )
-    def test_mrt_invalid_arg(self, args):
-        self.as_connection.commit()
+    def test_mrt_invalid_args(self, args: list):
+        with pytest.raises(TypeError):
+            self.as_connection.commit(*args)
 
     def test_commit(self):
         mrt = aerospike.Transaction()
@@ -43,4 +37,5 @@ class TestMRT:
     def test_abort_fail(self):
         mrt = aerospike.Transaction()
         self.as_connection.abort(mrt)
-        self.as_connection.abort(mrt)
+        with pytest.raises(e.AerospikeError):
+            self.as_connection.abort(mrt)
