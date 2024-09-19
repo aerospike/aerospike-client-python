e78aa7626c71e0ec02363515ac48da160154285c
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-09-19 09:33:03 -0700

    WIP will break

--- src/main/policy.c
+++ src/main/policy.c
@@ -759,19 +759,12 @@ static struct policy_field read_policy_fields[] = {
     POLICY_FIELD_DEF(as_policy_read, as_policy_read_mode_sc, read_mode_sc),
     {0}};
 
-int set_as_policy_fields_using_pyobject(as_error *err, void *policy_ref,
-                                        PyObject *py_policy,
-                                        struct policy_field *policy_fields)
+// TODO: Client object needed to create C client expressions object
+int set_as_policy_fields_using_pyobject(AerospikeClient *self, as_error *err,
+                                        void *policy_ref, PyObject *py_policy,
+                                        struct policy_field *policy_fields,
+                                        as_exp *exp_list)
 {
-    if (!py_policy || py_policy == Py_None) {
-        // Use config-level policy
-        return 0;
-    }
-    else if (!PyDict_Check(py_policy)) {
-        as_error_update(err, AEROSPIKE_ERR_PARAM, "policy must be a dict");
-        goto error;
-    }
-
     struct policy_field *curr_field = policy_fields;
     while (curr_field != NULL) {
         const char *python_client_field_name =
@@ -818,6 +811,13 @@ int set_as_policy_fields_using_pyobject(as_error *err, void *policy_ref,
             }
             *((bool *)as_policy_field_ref) = (bool)retval;
         }
+        else if (strcmp(curr_field->type, "as_exp*")) {
+            if (convert_exp_list(self, py_field_value, &exp_list, err) ==
+                AEROSPIKE_OK) {
+                *((bool *)as_policy_field_ref) = (bool)retval;
+                *exp_list_p = exp_list;
+            }
+        }
         else {
             as_error_update(err, AEROSPIKE_ERR_PARAM,
                             "Unable to parse %s of type %s",
@@ -837,6 +837,12 @@ error:
     return -1;
 }
 
+static int validate_py_policy(PyObject *py_policy)
+{
+    if (!py_policy || py_policy != Py_None) {
+    }
+}
+
 /**
  * Converts a PyObject into an as_policy_read object.
  * Returns AEROSPIKE_OK on success. On error, the err argument is populated.
@@ -850,6 +856,7 @@ as_status pyobject_to_policy_read(AerospikeClient *self, as_error *err,
                                   as_policy_read *config_read_policy,
                                   as_exp *exp_list, as_exp **exp_list_p)
 {
+
     if (py_policy && py_policy != Py_None) {
         // Initialize Policy
         POLICY_INIT(as_policy_read);
@@ -862,7 +869,7 @@ as_status pyobject_to_policy_read(AerospikeClient *self, as_error *err,
     if (py_policy && py_policy != Py_None) {
         // Set policy fields
         int retval = set_as_policy_fields_using_pyobject(
-            err, &policy->base, py_policy, base_policy_fields);
+            err, &policy->base, py_policy, base_policy_fields, exp_list);
         if (retval == -1) {
             return -1;
         }
