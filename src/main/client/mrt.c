d384e7d6d09a7480cb3e06e802ef37ed4aba73ab
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-10-02 08:46:15 -0700

    add support for new arg

--- src/main/client/mrt.c
+++ src/main/client/mrt.c
@@ -5,32 +5,34 @@
 #include "types.h"
 #include "client.h"
 
-static PyObject *call_c_client_mrt_method(
-    AerospikeClient *self, PyObject *args, PyObject *kwds,
-    as_status (*c_client_api_call)(aerospike *as, as_error *err, as_txn *txn))
+PyObject *AerospikeClient_Commit(AerospikeClient *self, PyObject *args,
+                                 PyObject *kwds)
 {
     AerospikeTransaction *py_transaction = NULL;
+    int get_status = 0;
 
-    static char *kwlist[] = {"transaction", NULL};
+    static char *kwlist[] = {"transaction", "get_commit_status", NULL};
 
-    const char *format;
-    if (c_client_api_call == aerospike_commit) {
-        format = "O!:commit";
-    }
-    else {
-        format = "O!:abort";
-    }
-    if (PyArg_ParseTupleAndKeywords(args, kwds, format, kwlist,
-                                    &AerospikeTransaction_Type,
-                                    (PyObject **)(&py_transaction)) == false) {
+    if (PyArg_ParseTupleAndKeywords(
+            args, kwds, "O!|p:commit", kwlist, &AerospikeTransaction_Type,
+            (PyObject **)(&py_transaction), &get_status) == false) {
         return NULL;
     }
 
     as_error err;
     as_error_init(&err);
 
+    as_commit_status status;
+    as_commit_status *status_ref;
+    if (get_status) {
+        status_ref = &status;
+    }
+    else {
+        status_ref = NULL;
+    }
+
     Py_BEGIN_ALLOW_THREADS
-    c_client_api_call(self->as, &err, py_transaction->txn);
+    aerospike_commit(self->as, &err, py_transaction->txn, status_ref);
     Py_END_ALLOW_THREADS
 
     if (err.code != AEROSPIKE_OK) {
@@ -38,17 +40,61 @@ static PyObject *call_c_client_mrt_method(
         return NULL;
     }
 
-    Py_RETURN_NONE;
-}
-
-PyObject *AerospikeClient_Commit(AerospikeClient *self, PyObject *args,
-                                 PyObject *kwds)
-{
-    return call_c_client_mrt_method(self, args, kwds, aerospike_commit);
+    if (get_status) {
+        PyObject *py_status = PyLong_FromUnsignedLong((unsigned long)status);
+        if (py_status == NULL) {
+            return NULL;
+        }
+        return py_status;
+    }
+    else {
+        Py_RETURN_NONE;
+    }
 }
 
 PyObject *AerospikeClient_Abort(AerospikeClient *self, PyObject *args,
                                 PyObject *kwds)
 {
-    return call_c_client_mrt_method(self, args, kwds, aerospike_abort);
+    AerospikeTransaction *py_transaction = NULL;
+
+    static char *kwlist[] = {"transaction", "get_abort_status", NULL};
+    int get_status = 0;
+
+    if (PyArg_ParseTupleAndKeywords(
+            args, kwds, "O!:abort", kwlist, &AerospikeTransaction_Type,
+            (PyObject **)(&py_transaction), &get_status) == false) {
+        return NULL;
+    }
+
+    as_error err;
+    as_error_init(&err);
+
+    as_abort_status status;
+    as_abort_status *status_ref;
+    if (get_status) {
+        status_ref = &status;
+    }
+    else {
+        status_ref = NULL;
+    }
+
+    Py_BEGIN_ALLOW_THREADS
+    aerospike_abort(self->as, &err, py_transaction->txn, status_ref);
+    Py_END_ALLOW_THREADS
+
+    if (err.code != AEROSPIKE_OK) {
+        raise_exception(&err);
+        return NULL;
+    }
+
+    if (get_status) {
+        PyObject *py_status = PyLong_FromUnsignedLong((unsigned long)status);
+        if (py_status == NULL) {
+            return NULL;
+        }
+        return py_status;
+    }
+    else {
+        Py_RETURN_NONE;
+    }
 }
