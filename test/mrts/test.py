2be8448f28e78a6109a0868701b003b0d4c7e63c
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-10-03 11:28:40 -0700

    Cleanup

--- test/mrts/test.py
+++ test/mrts/test.py
@@ -4,7 +4,7 @@ import aerospike
 
 
 # We don't include this in the new_tests/ suite because this requires strong consistency
-# We can't detect if the server has strong consistency or not
+# The logic for checking the server is properly enabled for MRTs is complicated
 class TestMRTBasicFunctionality:
     @classmethod
     def setup_class(cls):
@@ -18,58 +18,58 @@ class TestMRTBasicFunctionality:
     @pytest.fixture(autouse=True)
     def insert_or_update_records(self):
         config = {"hosts": [("127.0.0.1", 3000)]}
-        self.as_connection = aerospike.client(config)
+        self.client = aerospike.client(config)
         for i, key in enumerate(self.keys):
-            self.as_connection.put(key, {self.bin_name: i})
+            self.client.put(key, {self.bin_name: i})
 
     # Test case 1: Execute a simple MRT with multiple SRTs(Read and Write) in any sequence (P3)
     # Validate that all operations complete successfully.
-    @pytest.mark.parametrize("get_status", [False, True])
-    def test_commit_api_and_functionality(self, get_status: bool):
+    @pytest.mark.parametrize("get_commit_status", [False, True])
+    def test_commit_api_and_functionality(self, get_commit_status: bool):
         mrt = aerospike.Transaction()
         policy = {
             "txn": mrt
         }
-        self.as_connection.put(self.keys[0], {self.bin_name: 1}, policy=policy)
+        self.client.put(self.keys[0], {self.bin_name: 1}, policy=policy)
         # Reads in an MRT should read the intermediate values of the MRT
-        _, _, bins = self.as_connection.get(self.keys[0], policy)
+        _, _, bins = self.client.get(self.keys[0], policy)
         # Check that original value was overwritten
         assert bins == {self.bin_name: 1}
-        self.as_connection.put(self.keys[1], {self.bin_name: 2}, policy=policy)
+        self.client.put(self.keys[1], {self.bin_name: 2}, policy=policy)
 
-        retval = self.as_connection.commit(transaction=mrt, get_commit_status=get_status)
-        if get_status:
+        retval = self.client.commit(transaction=mrt, get_commit_status=get_commit_status)
+        if get_commit_status:
             assert type(retval) is int
         else:
             assert retval is None
 
         # Were the writes committed?
         for i in range(len(self.keys)):
-            _, _, bins = self.as_connection.get(self.keys[i])
+            _, _, bins = self.client.get(self.keys[i])
             assert bins == {self.bin_name: i + 1}
 
     # Test case 57: "Execute the MRT. Before issuing commit, give abort request using abort API" (P1)
-    @pytest.mark.parametrize("get_status", [False, True])
-    def test_abort_api_and_functionality(self, get_status: bool):
+    @pytest.mark.parametrize("get_abort_status", [False, True])
+    def test_abort_api_and_functionality(self, get_abort_status: bool):
         mrt = aerospike.Transaction()
         policy = {
             "txn": mrt
         }
-        self.as_connection.put(self.keys[0], {self.bin_name: 1}, policy=policy)
+        self.client.put(self.keys[0], {self.bin_name: 1}, policy=policy)
         # Should return intermediate overwritten value from MRT
-        _, _, bins = self.as_connection.get(self.keys[0], policy)
+        _, _, bins = self.client.get(self.keys[0], policy)
         assert bins == {self.bin_name: 1}
-        self.as_connection.put(self.keys[1], {self.bin_name: 2}, policy=policy)
+        self.client.put(self.keys[1], {self.bin_name: 2}, policy=policy)
 
-        retval = self.as_connection.abort(transaction=mrt, get_abort_status=get_status)
-        if get_status:
+        retval = self.client.abort(transaction=mrt, get_abort_status=get_abort_status)
+        if get_abort_status:
             assert type(retval) is int
         else:
             assert retval is None
 
         # Test that MRT didn't go through
         for i in range(len(self.keys)):
-            _, _, bins = self.as_connection.get(self.keys[i])
+            _, _, bins = self.client.get(self.keys[i])
             # Write transaction was rolled back
             assert bins == {self.bin_name: i}
 
@@ -81,7 +81,7 @@ class TestMRTBasicFunctionality:
         policy = {
             "txn": mrt
         }
-        self.as_connection.put(self.keys[0], {self.bin_name: 1}, policy=policy)
-        self.as_connection.commit(mrt)
+        self.client.put(self.keys[0], {self.bin_name: 1}, policy=policy)
+        self.client.commit(mrt)
         with pytest.raises(e.RollAlreadyAttempted):
-            self.as_connection.abort(mrt)
+            self.client.abort(mrt)
