a41d711e0d15080abb6b4e1a33bdb21eadaafc10
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-10-23 15:00:46 -0700

    see what happens

--- test/new_tests/test_free_threading.py
+++ test/new_tests/test_free_threading.py
@@ -1,8 +1,35 @@
 import threading
-from concurrent.futures import ThreadPoolExecutor
 
 import aerospike
 
-def test_unsafe():
-    n_threads = 10
-    barrier = threading.Barrier(n_threads)
+
+def test_unsafe(as_connection):
+    key = ("test", "demo", 1)
+    BIN_VALUE = 1
+    as_connection.put(key, policy={"a": BIN_VALUE})
+
+    bin_value_sum = 0
+
+    THREAD_COUNT = 10
+    barrier = threading.Barrier(parties=THREAD_COUNT)
+    config = {"hosts": [("127.0.0.1", 3000)]}
+
+    def read_bin():
+        barrier.wait()
+        client = aerospike.client(config)
+        _, _, bins = client.get(key)
+        nonlocal bin_value_sum
+        bin_value_sum += bins["a"]
+
+    workers = []
+    for _ in range(THREAD_COUNT):
+        workers.append(threading.Thread(target=read_bin))
+
+    for worker in workers:
+        worker.start()
+
+    for worker in workers:
+        worker.join()
+
+    # Do something about the results
+    assert bin_value_sum == THREAD_COUNT * BIN_VALUE
