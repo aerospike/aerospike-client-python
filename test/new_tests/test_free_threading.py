622b67a250303549c601c4b9988126682de5c91c
juliannguyen4 <109386615+juliannguyen4@users.noreply.github.com>
2024-10-23 22:11:19 +0000

    fix

--- test/new_tests/test_free_threading.py
+++ test/new_tests/test_free_threading.py
@@ -1,35 +1,37 @@
 import threading
-
+import pytest
 import aerospike
 
 
-def test_unsafe(as_connection):
-    key = ("test", "demo", 1)
-    BIN_VALUE = 1
-    as_connection.put(key, policy={"a": BIN_VALUE})
+@pytest.mark.usefixtures("as_connection")
+class TestFreeThreading:
+    def test_unsafe(self):
+        key = ("test", "demo", 1)
+        BIN_VALUE = 1
+        self.as_connection.put(key, bins={"a": BIN_VALUE})
 
-    bin_value_sum = 0
+        bin_value_sum = 0
 
-    THREAD_COUNT = 10
-    barrier = threading.Barrier(parties=THREAD_COUNT)
-    config = {"hosts": [("127.0.0.1", 3000)]}
+        THREAD_COUNT = 10
+        barrier = threading.Barrier(parties=THREAD_COUNT)
+        config = {"hosts": [("127.0.0.1", 3000)]}
 
-    def read_bin():
-        nonlocal barrier
-        barrier.wait()
-        client = aerospike.client(config)
-        _, _, bins = client.get(key)
-        nonlocal bin_value_sum
-        bin_value_sum += bins["a"]
+        def read_bin():
+            nonlocal barrier
+            barrier.wait()
+            client = aerospike.client(config)
+            _, _, bins = client.get(key)
+            nonlocal bin_value_sum
+            bin_value_sum += bins["a"]
 
-    workers = []
-    for _ in range(THREAD_COUNT):
-        workers.append(threading.Thread(target=read_bin))
+        workers = []
+        for _ in range(THREAD_COUNT):
+            workers.append(threading.Thread(target=read_bin))
 
-    for worker in workers:
-        worker.start()
+        for worker in workers:
+            worker.start()
 
-    for worker in workers:
-        worker.join()
+        for worker in workers:
+            worker.join()
 
-    assert bin_value_sum == THREAD_COUNT * BIN_VALUE
+        assert bin_value_sum == THREAD_COUNT * BIN_VALUE
