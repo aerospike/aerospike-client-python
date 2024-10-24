1fdb0d8eb0422d8357c4cbd36f56678b0572fc0d
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-10-24 11:24:28 -0700

    rewrite

--- test/new_tests/test_free_threading.py
+++ test/new_tests/test_free_threading.py
@@ -1,35 +1,38 @@
 import threading
 import pytest
+
 import aerospike
+from aerospike_helpers.operations import operations
 
 
 @pytest.mark.usefixtures("as_connection")
 class TestFreeThreading:
     def test_unsafe(self):
         key = ("test", "demo", 1)
-        BIN_VALUE = 1
-        self.as_connection.put(key, bins={"a": BIN_VALUE})
-
-        bin_value_sum = 0
+        INIT_BIN_VALUE = 0
+        BIN_NAME = "a"
+        BIN_VALUE_AMOUNT_TO_ADD_IN_EACH_THREAD = 1
+        self.as_connection.put(key, bins={BIN_NAME: INIT_BIN_VALUE})
 
         THREAD_COUNT = 10
         barrier = threading.Barrier(parties=THREAD_COUNT)
         config = {"hosts": [("127.0.0.1", 3000)]}
-        lock = threading.Lock()
 
-        def read_bin():
-            nonlocal barrier, config, lock
+        ops = [
+            operations.increment(bin_name=BIN_NAME, amount=BIN_VALUE_AMOUNT_TO_ADD_IN_EACH_THREAD)
+        ]
+
+        def increment_bin():
+            nonlocal barrier
             barrier.wait()
+            nonlocal config
             client = aerospike.client(config)
-            _, _, bins = client.get(key)
-            nonlocal bin_value_sum
-            lock.acquire()
-            bin_value_sum += bins["a"]
-            lock.release()
+            nonlocal key, ops
+            client.operate(key, ops)
 
         workers = []
         for _ in range(THREAD_COUNT):
-            workers.append(threading.Thread(target=read_bin))
+            workers.append(threading.Thread(target=increment_bin))
 
         for worker in workers:
             worker.start()
@@ -37,4 +40,5 @@ class TestFreeThreading:
         for worker in workers:
             worker.join()
 
-        assert bin_value_sum == THREAD_COUNT * BIN_VALUE
+        _, _, bins = self.as_connection.get(key)
+        assert bins[BIN_NAME] == INIT_BIN_VALUE + BIN_VALUE_AMOUNT_TO_ADD_IN_EACH_THREAD * THREAD_COUNT
