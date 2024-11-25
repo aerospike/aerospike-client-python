3864f774d95904577bef346f5f237ddbccd162a5
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-11-25 13:57:17 -0800

    forgot

---
+++ src/include/exception_attrs.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+extern const char *const aerospike_err_attrs[6];
---
+++ src/main/exception_attrs.c
@@ -0,0 +1,4 @@
+#include "exception_attrs.h"
+
+const char *const aerospike_err_attrs[6] = {"code", "msg",      "file",
+                                            "line", "in_doubt", NULL};
