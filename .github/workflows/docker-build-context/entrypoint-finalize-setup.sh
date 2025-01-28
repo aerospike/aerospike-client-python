a9f887f859f7eac0f1f95ebe649596c5b6ad2fa6
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-01-28 09:42:08 -0800

    Dont bother using orig entrypoint

--- .github/workflows/docker-build-context/entrypoint-finalize-setup.sh
+++ .github/workflows/docker-build-context/entrypoint-finalize-setup.sh
@@ -1,6 +1,8 @@
-/entrypoint.sh $1
+asd --fgdaemon &
 
 SUPERUSER_NAME_AND_PASSWORD=superuser
 ASADM_AUTH_FLAGS="--user=$SUPERUSER_NAME_AND_PASSWORD --password=$SUPERUSER_NAME_AND_PASSWORD"
 asadm $ASADM_AUTH_FLAGS --enable --execute "manage revive ns test"
 asadm $ASADM_AUTH_FLAGS --enable --execute "manage recluster"
+
+fg
