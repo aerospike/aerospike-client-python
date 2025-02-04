f7ae18c7fc92786e8218b9160cc54e991ceeb5ab
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-02-04 08:53:22 -0800

    use astools.conf

--- .github/workflows/docker-build-context/entrypoint-finalize-setup.sh
+++ .github/workflows/docker-build-context/entrypoint-finalize-setup.sh
@@ -7,10 +7,6 @@ set -m
 
 asd --fgdaemon --config-file /etc/aerospike/aerospike-dev.conf &
 
-# We need to pass credentials to asinfo if server requires it
-# TODO: passing in credentials via command line flags since I can't figure out how to use --instance with global astools.conf
-user_credentials="--user=admin --password=admin"
-
 while true; do
     # An unset variable will have a default empty value
     # Intermediate step is to print docker exec command's output in case it fails
@@ -21,13 +17,13 @@ while true; do
     # ack does have an option but it doesn't come installed by default
     # shellcheck disable=SC2086 # The flags in user credentials should be separate anyways. Not one string
     echo "Checking if we can reach the server via the service port..."
-    if asinfo $user_credentials -v status | tee >(cat) | grep -qE "^ok"; then
+    if asinfo -v status | tee >(cat) | grep -qE "^ok"; then
         # Server is ready when asinfo returns ok
         echo "Can reach server now."
         # docker container inspect "$container_name"
         break
     fi
-    asinfo $user_credentials -v status
+    asinfo -v status
     echo "Server didn't return ok via the service port. Polling again..."
 done
 
@@ -41,7 +37,7 @@ while true; do
     # When we reuse this roster, the server assumes all of its partitions are dead because it's running on a new
     # storage device.
     cluster_stable_info_cmd="$cluster_stable_info_cmd:ignore-migrations=true"
-    if asinfo $user_credentials -v $cluster_stable_info_cmd 2>&1 | (! grep -qE "^ERROR"); then
+    if asinfo -v $cluster_stable_info_cmd 2>&1 | (! grep -qE "^ERROR"); then
         echo "Server is in a stable state."
         break
     fi
@@ -49,10 +45,8 @@ while true; do
     echo "Server did not return a cluster key. Polling again..."
 done
 
-SUPERUSER_NAME_AND_PASSWORD=superuser
-ASADM_AUTH_FLAGS="--user=$SUPERUSER_NAME_AND_PASSWORD --password=$SUPERUSER_NAME_AND_PASSWORD"
-asadm $ASADM_AUTH_FLAGS --enable --execute "manage revive ns test"
-asadm $ASADM_AUTH_FLAGS --enable --execute "manage recluster"
+asadm --enable --execute "manage revive ns test"
+asadm --enable --execute "manage recluster"
 
 # Allows HEALTHCHECK to report this container as healthy, now
 touch /finalized
