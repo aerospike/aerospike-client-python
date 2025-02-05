51f4fe1d0f3367dddd6e67d84134272678f6811b
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-02-05 07:31:35 -0800

    Share variables btwn Dockerfile and entrypoint using ENV

--- .github/workflows/docker-build-context/entrypoint-finalize-setup.sh
+++ .github/workflows/docker-build-context/entrypoint-finalize-setup.sh
@@ -3,11 +3,12 @@
 set -x
 set -m
 
-asd --fgdaemon --config-file /etc/aerospike/aerospike-dev.conf &
+asd --fgdaemon --config-file $AEROSPIKE_CONF_PATH &
 
 # timeout uses sh shell by default, so we need to be specific
-timeout 30s bash /wait-for-as-server-to-start.bash
+timeout 30s bash $WAIT_SCRIPT_FILE_PATH
 
+# Finish setting up strong consistency
 asadm --enable --execute "manage revive ns test"
 asadm --enable --execute "manage recluster"
 
