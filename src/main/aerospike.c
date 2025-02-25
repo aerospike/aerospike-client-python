ca59565f22ccb3b1a3d3b3e242acd014f3a888f3
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-02-25 08:47:01 -0800

    fix

--- src/main/aerospike.c
+++ src/main/aerospike.c
@@ -633,7 +633,7 @@ PyMODINIT_FUNC PyInit_aerospike(void)
     }
     PyObject *py_sys_modules = PyObject_GetAttrString(py_sys, "modules");
     if (py_sys_modules == NULL) {
-        goto SYS_MODULE_CLEANUP_ON_ERROR;
+        goto SYS_CLEANUP_ON_ERROR;
     }
 
     for (i = 0; i < sizeof(py_submodules) / sizeof(py_submodules[0]); i++) {
@@ -641,7 +641,7 @@ PyMODINIT_FUNC PyInit_aerospike(void)
             py_submodules[i].pyobject_creation_method;
         PyObject *py_submodule = create_py_submodule();
         if (py_submodule == NULL) {
-            goto SYS_MODULE_CLEANUP_ON_ERROR;
+            goto SYS_MODULES_CLEANUP_ON_ERROR;
         }
 
         int retval = PyDict_SetItemString(py_sys_modules,
@@ -658,14 +658,16 @@ PyMODINIT_FUNC PyInit_aerospike(void)
         }
         continue;
 
-        SUBMODULE_CLEANUP_ON_ERROR:
-            Py_DECREF(py_submodule);
-            goto SYS_MODULE_CLEANUP_ON_ERROR;
+    SUBMODULE_CLEANUP_ON_ERROR:
+        Py_DECREF(py_submodule);
+        goto SYS_MODULES_CLEANUP_ON_ERROR;
     }
 
     return py_aerospike_module;
 
-SYS_MODULE_CLEANUP_ON_ERROR:
+SYS_MODULES_CLEANUP_ON_ERROR:
+    Py_DECREF(py_sys_modules);
+SYS_CLEANUP_ON_ERROR:
     // TODO: Clean up any submodules that were manually added to sys.modules
     // This isn't a big deal though, so just leave off for now
     Py_DECREF(py_sys);
