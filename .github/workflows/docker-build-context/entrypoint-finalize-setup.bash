bda388b1ed036f4dd6d070fceed15ffc3be25816
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-03-12 15:17:09 -0700

    hardcode today. fix this tmrw

--- .github/workflows/docker-build-context/entrypoint-finalize-setup.bash
+++ .github/workflows/docker-build-context/entrypoint-finalize-setup.bash
@@ -18,7 +18,7 @@ if [[ -n "$NO_SC" ]]; then
     rm roster.smd
 fi
 
-asd --fgdaemon --config-file $AEROSPIKE_CONF_PATH &
+asd --fgdaemon --config-file /etc/aerospike/aerospike.conf &
 
 # We don't need to timeout here.
 # If the wait script runs forever, users running the container manually will know that
