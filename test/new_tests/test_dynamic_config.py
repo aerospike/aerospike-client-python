f66e74c0bb10ee3c46afe9a5daab93b2d2d21b0e
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-04-02 11:33:33 -0700

    Add reinit test

--- test/new_tests/test_dynamic_config.py
+++ test/new_tests/test_dynamic_config.py
@@ -6,22 +6,25 @@ import pytest
 
 class TestDynamicConfig:
     def test_config_provider_class(self):
-        # Should be immutable
         provider = aerospike.ConfigProvider(path="path", interval=20)
 
         assert provider.path == "path"
         assert provider.interval == 20
+        # Fields should be read only
         with pytest.raises(AttributeError):
             provider.path = "invalid"
         with pytest.raises(AttributeError):
             provider.interval = 10
+
+        # We should be able to reinitialize with no issues
+        provider.__init__()
 
     def test_basic_functionality(self):
         config = TestBaseClass.get_connection_config()
         provider = aerospike.ConfigProvider("./dyn_config.yml")
         config["config_provider"] = provider
         # We want to check that the config file we pass in is valid
-        # TODO: does this affect the rest of the tests?
+        # The C client prints logs showing that it detects changes to the dynamic config file
         aerospike.set_log_level(aerospike.LOG_LEVEL_TRACE)
         client = aerospike.client(config)
 
