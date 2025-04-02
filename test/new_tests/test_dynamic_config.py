57e7f711d609f77b9fadd9249e8fab91b524dc64
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-04-02 08:24:56 -0700

    Fix test

--- test/new_tests/test_dynamic_config.py
+++ test/new_tests/test_dynamic_config.py
@@ -34,7 +34,10 @@ class TestDynamicConfig:
         query = client.query("test", "demo")
         recs = query.results()
         assert len(recs) == 1
-        assert recs[0][2] is not None
+        # Check that record key tuple does not have a primary key
+        first_record = recs[0]
+        first_record_key = first_record[0]
+        assert first_record_key[2] is not None
 
         # Cleanup
         client.remove(key)
