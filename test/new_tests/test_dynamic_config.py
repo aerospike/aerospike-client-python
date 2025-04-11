79afd88124b77fe3beabb530fa6a4ccd84ae12cb
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-04-11 08:21:25 -0700

    bad comment

--- test/new_tests/test_dynamic_config.py
+++ test/new_tests/test_dynamic_config.py
@@ -53,7 +53,7 @@ class TestDynamicConfig:
         query = client.query("test", "demo")
         recs = query.results()
         assert len(recs) == 1
-        # Check that record key tuple does not have a primary key
+        # Check that record key tuple has a primary key
         first_record = recs[0]
         first_record_key = first_record[0]
         assert first_record_key[2] is not None
