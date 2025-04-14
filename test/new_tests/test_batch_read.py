f1f0f69eb8e33705f3d89f51d751feea9f8eb7cf
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-04-14 10:08:40 -0700

    improve test

--- test/new_tests/test_batch_read.py
+++ test/new_tests/test_batch_read.py
@@ -50,10 +50,17 @@ class TestBatchRead(TestBaseClass):
 
         request.addfinalizer(teardown)
 
-    def test_batch_read_with_policy(self):
+    # Ensures that filtering out records behaves the same, regardless of which parameter we pass into bins
+    @pytest.mark.parametrize("bins", [
+        None,
+        [],
+        ["count"]
+    ])
+    def test_batch_read_with_policy(self, bins):
         # No record will satisfy this expression condition
         expr = exp.Eq(exp.IntBin("count"), 99).compile()
-        res: BatchRecords = self.as_connection.batch_read(self.keys, policy={"expressions": expr})
+        # TODO: type stub isn't correct
+        res: BatchRecords = self.as_connection.batch_read(self.keys, bins=bins, policy={"expressions": expr})
         assert res.result == 0
         for i, batch_rec in enumerate(res.batch_records):
             assert batch_rec.key[:3] == self.keys[i]  # checking key
