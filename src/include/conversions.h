93c90d3f9756c1a362dd9c0fe63bc4b361748c6b
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-04-18 16:23:38 -0700

    fix

--- src/include/conversions.h
+++ src/include/conversions.h
@@ -234,7 +234,7 @@ PyObject *create_py_node_from_as_node(as_error *error_p,
 
 // Checks if pyobject is an instance of a class type defined in aerospike_helpers or one of its submodules
 // If expected_submodule_name is NULL, the type is expected to be defined directly in the aerospike_helpers package
-// If is_subtype is true, we expect the instance's type to directly inherit from aerospike_helpers.<expected_submodule_name>.<expected_type_name>
+// If is_subclass_instance is true, we expect the instance's type to directly inherit from aerospike_helpers.<expected_submodule_name>.<expected_type_name>
 bool is_pyobj_correct_as_helpers_type(PyObject *obj,
                                       const char *expected_submodule_name,
                                       const char *expected_type_name,
