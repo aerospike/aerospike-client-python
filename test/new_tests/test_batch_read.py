469bd106e4a8de31a665e9acc6ba1699e84f31fe
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-04-18 11:03:14 -0700

    [CLIENT-2756] Remove API calls that were deprecated in Python client 12.0.0 (#747)
    Extra changes:
    
    - Add regression test for batch_operate() that checks if only read operations can be used
    - Improve and fix type stubs
    - Docs: batch_read() parameter "policy_batch" should be "policy"
    - Docs: move aerospike.Scan.execute_background() example to a separate file so it is easier to test
    - Tests: many expression tests relied on get_many() to verify the results after applying filter expressions. When moving from get_many() to batch_read(), we had to change the number of records to retrieve with batch_read() because we only wanted to get records that existed. batch_read() will return a different error code for non-existent records vs. filtered out records.

--- test/new_tests/test_batch_read.py
+++ test/new_tests/test_batch_read.py
@@ -50,10 +50,17 @@ class TestBatchRead(TestBaseClass):
 
         request.addfinalizer(teardown)
 
-    def test_batch_read_with_policy(self):
+    # Ensures that filtering out records behaves the same, regardless of which parameter we pass into bins
+    # Setting bins=None does not work, so we have to do this for now
+    @pytest.mark.parametrize("args", [
+        [],
+        [[]],
+        [["count"]]
+    ])
+    def test_batch_read_with_policy(self, args):
         # No record will satisfy this expression condition
         expr = exp.Eq(exp.IntBin("count"), 99).compile()
-        res: BatchRecords = self.as_connection.batch_read(self.keys, policy={"expressions": expr})
+        res: BatchRecords = self.as_connection.batch_read(self.keys, policy={"expressions": expr}, *args)
         assert res.result == 0
         for i, batch_rec in enumerate(res.batch_records):
             assert batch_rec.key[:3] == self.keys[i]  # checking key
