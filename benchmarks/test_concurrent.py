c71e4617df9fc88d10307fb69133cac77cf79864
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-04-28 13:25:36 -0700

    missing files

---
+++ benchmarks/test_concurrent.py
@@ -0,0 +1,20 @@
+import pyperf
+import aerospike
+
+# TODO: must connect to a network attached server. not Docker container
+config = {"hosts": [("127.0.0.1", 3000)]}
+client = aerospike.client(config)
+
+keys = []
+try:
+    for i in range(1000):
+        key = ("test", "demo", i)
+        client.put(key, {"a": 1})
+        keys.append(key)
+
+    policy = {"concurrent": True}
+    runner = pyperf.Runner()
+    bench = runner.bench_func('batch_read', client.batch_read, keys, ["a"], policy)
+    bench.dump(file="./benchmark.json")
+finally:
+    client.close()
