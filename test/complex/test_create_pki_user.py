d1236423cea06e0621eb08dad875ea3edea8c61c
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-06-30 14:27:03 -0700

    fix test

--- test/complex/test_create_pki_user.py
+++ test/complex/test_create_pki_user.py
@@ -1,4 +1,8 @@
+import unittest
+
 import aerospike
+from aerospike import exception as e
+from contextlib import nullcontext
 
 # Creating a PKI user with the superuser doesn't work
 # I believe it's because the user creating a PKI user must only have the user-admin role
@@ -18,18 +22,30 @@ config = {
         "certfile": "../../.github/workflows/docker-build-context/client.cer"
     }
 }
-print("Connecting to server...")
-as_client = aerospike.client(config)
 
-try:
-    pki_user = "pki2"
-    roles = ["read-write"]
-    admin_policy = {}
-    as_client.admin_create_pki_user(user=pki_user, roles=roles, policy=admin_policy)
 
-    # Check that the PKI user was created.
-    userDict = as_client.admin_query_user_info(pki_user)
-    assert userDict["roles"] == ["read-write"]
-finally:
-    # Cleanup
-    as_client.close()
+class TestCreatePKIUser(unittest.TestCase):
+    def test_create_pki_user(self):
+        print("Connecting to server...")
+        as_client = aerospike.client(config)
+
+        try:
+            pki_user = "pki2"
+            roles = ["read-write"]
+            admin_policy = {}
+            server_version = as_client.info_random_node("build")
+            if server_version.startswith('8.1'):
+                context = nullcontext()
+            else:
+                context = self.assertRaises(e.AerospikeError)
+
+            with context:
+                as_client.admin_create_pki_user(user=pki_user, roles=roles, policy=admin_policy)
+
+            if type(context) == nullcontext:
+                # Check that the PKI user was created.
+                userDict = as_client.admin_query_user_info(pki_user)
+                assert userDict["roles"] == ["read-write"]
+        finally:
+            # Cleanup
+            as_client.close()
