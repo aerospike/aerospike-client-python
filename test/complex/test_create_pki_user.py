de7f5a9eb0a8d83ded5d991d9370ba408211d6e9
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-07-01 08:06:56 -0700

    try rerunning test without pki auth mode

--- test/complex/test_create_pki_user.py
+++ test/complex/test_create_pki_user.py
@@ -9,9 +9,11 @@ config = {
         ("127.0.0.1", 4333, "docker")
     ],
     # "user": "pki1",
-    "policies": {
-        "auth_mode": aerospike.AUTH_PKI
-    },
+    "user": "superuser",
+    "password": "superuser",
+    # "policies": {
+    #     "auth_mode": aerospike.AUTH_PKI
+    # },
     "tls": {
         "enable": True,
         "cafile": "../../.github/workflows/docker-build-context/ca.cer",
@@ -27,19 +29,20 @@ class TestCreatePKIUser(unittest.TestCase):
         as_client = aerospike.client(config)
 
         try:
-            pki_user = "pki2"
-            roles = ["read-write"]
-            admin_policy = {}
             info_response = as_client.info_random_node("build")
             print(info_response)
             # Example response format: build <version>
             tokens = info_response.split()
             server_version = tokens[1]
+
             if server_version.startswith('8.1'):
                 context = nullcontext()
             else:
                 context = self.assertRaises(e.AerospikeError)
 
+            pki_user = "pki2"
+            roles = ["read-write"]
+            admin_policy = {}
             with context:
                 as_client.admin_create_pki_user(user=pki_user, roles=roles, policy=admin_policy)
 
