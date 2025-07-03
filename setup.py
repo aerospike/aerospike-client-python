5c36d9d74c44601032b4334052e98763dd63ba3b
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-07-03 14:07:30 -0700

    pthread isnt being linked against when compiling the python client

--- setup.py
+++ setup.py
@@ -164,6 +164,7 @@ elif LINUX:
     AEROSPIKE_C_TARGET = AEROSPIKE_C_HOME + '/target/Linux-' + machine
 elif WINDOWS:
     libraries.clear()
+    libraries.append("pthread")
     extra_compile_args.append("-DAS_SHARED_IMPORT")
     include_dirs.append(f"{AEROSPIKE_C_TARGET}/vs/packages/aerospike-client-c-dependencies.{c_client_dependencies_version}/build/native/include")
 else:
