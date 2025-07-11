017346199674cd20874dfbb328599b217785d7ce
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-07-11 10:10:47 -0700

    Update src/main/client/type.c
    Co-authored-by: dwelch-spike <53876192+dwelch-spike@users.noreply.github.com>

--- src/main/client/type.c
+++ src/main/client/type.c
@@ -334,7 +334,7 @@ static PyMethodDef AerospikeClient_Type_Methods[] = {
 
     {"admin_create_pki_user",
      (PyCFunction)AerospikeClient_Admin_Create_PKI_User,
-     METH_VARARGS | METH_KEYWORDS, "Create a new user."},
+     METH_VARARGS | METH_KEYWORDS, "Create a new pki user."},
     {"admin_create_user", (PyCFunction)AerospikeClient_Admin_Create_User,
      METH_VARARGS | METH_KEYWORDS, "Create a new user."},
     {"admin_drop_user", (PyCFunction)AerospikeClient_Admin_Drop_User,
