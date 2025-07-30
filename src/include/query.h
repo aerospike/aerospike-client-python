b5b9268bcc05c6ea34a143b4468e618763606e1b
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-07-30 14:37:56 -0700

    fix

--- src/include/query.h
+++ src/include/query.h
@@ -148,5 +148,5 @@ PyObject *StoreUnicodePyObject(AerospikeQuery *self, PyObject *obj);
 
 int64_t pyobject_to_int64(PyObject *py_obj);
 
-PyObject *AerospikeScan_Type_New(PyTypeObject *type, PyObject *args,
-                                 PyObject *kwds);
+PyObject *AerospikeQuery_Type_New(PyTypeObject *type, PyObject *args,
+                                  PyObject *kwds);
