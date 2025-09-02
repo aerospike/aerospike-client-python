a02c8b7439f0030e8a1bf81639f6fdccc521b7a4
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-09-02 14:29:49 -0700

    check that client connection was not destroyed

--- test/standalone/test_timeout_delay.py
+++ test/standalone/test_timeout_delay.py
@@ -11,6 +11,7 @@ from aerospike import exception as e
 CONTAINER_NAME = "aerospike"
 PORT = 3000
 
+
 class TestTimeoutDelay(unittest.TestCase):
     def setUp(self):
         self.docker_client = docker.from_env()
@@ -47,23 +48,34 @@ class TestTimeoutDelay(unittest.TestCase):
             CONTAINER_NAME,
             "--docker",
             "--delay",
-            "100ms"
+            # e2e latency should be less than the client's timeout delay window
+            # We want to test that the server does return a response after the timeout delay
+            "1000ms"
         ]
         env = os.environ.copy()
         subprocess.run(args=inject_latency_command, check=True, env=env)
 
         key = ("test", "demo", 1)
+        TIMEOUT_DELAY_MS = 2000
         policy = {
             # Should cause first command to timeout
             "total_timeout": 1,
             # All subsequent commands should timeout because of timeout_delay
-            "timeout_delay": 2000
+            "timeout_delay": TIMEOUT_DELAY_MS
         }
         with self.assertRaises(e.TimeoutError):
             client.get(key=key, policy=policy)
 
         with self.assertRaises(e.TimeoutError):
             client.get(key=key, policy=policy)
+
+        time.sleep(TIMEOUT_DELAY_MS)
+
+        # By now, we have passed the timeout delay window
+        # The server should've returned a response to the client connection
+        # and the client connection should not have been destroyed
+        # TODO: how to verify this?
+        client.get(key=key)
 
 
 if __name__ == "__main__":
