91e75a7d04ce8503607ed9dd5eacf58b79dfda7a
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-09-05 09:41:47 -0700

    Tend interval uses connect timeout to refresh nodes. set this to be really high so failing to refresh doesn't drop the node

--- test/standalone/test_timeout_delay.py
+++ test/standalone/test_timeout_delay.py
@@ -38,14 +38,19 @@ class TestTimeoutDelay(unittest.TestCase):
         cls.docker_client.close()
 
     # latency is this high because timeout_delay must be >= 3000ms
-    E2E_LATENCY_MS = 6000
+    INJECTED_LATENCY_MS = 6000
 
     def setUp(self):
         config = {
             "hosts": [
+                # TODO: get ip addr from docker client
                 ("127.0.0.1", PORT)
             ],
-            "tend_interval": self.E2E_LATENCY_MS // 2
+            # The default 1000ms tend interval runs frequently enough for this test
+            #
+            # We want connect_timeout > injected latency so the node refresh doesn't fail
+            # Otherwise the node will be dropped after 5 tend iterations
+            "connect_timeout": self.INJECTED_LATENCY_MS * 2
         }
         self.client = aerospike.client(config)
 
@@ -53,14 +58,14 @@ class TestTimeoutDelay(unittest.TestCase):
         self.client.put(self.key, bins={"a": 1})
 
     def tearDown(self):
-        del_latency_command = [
+        DELETE_LATENCY_COMMAND = [
             "sudo",
             "tcdel",
             CONTAINER_NAME,
             "--docker",
             "--all"
         ]
-        subprocess.run(args=del_latency_command, check=True)
+        subprocess.run(args=DELETE_LATENCY_COMMAND, check=True)
 
         self.client.close()
 
@@ -82,15 +87,17 @@ class TestTimeoutDelay(unittest.TestCase):
 
     def test_case(self):
         test_cases = [
+            # latency window < timeout delay window
             # The connection will receive a response before the timeout delay window ends
             # So the connection will be returned to the pool.
-            TestCase(timeout_delay_ms=self.E2E_LATENCY_MS * 2, expected_aborted_count=0, expected_recovered_count=1),
+            TestCase(timeout_delay_ms=self.INJECTED_LATENCY_MS * 2, expected_aborted_count=0, expected_recovered_count=1),
+            # latency window > timeout delay window
             # The connection will not receive a response during the timeout delay window
             # So the connection will be destroyed.
-            TestCase(timeout_delay_ms=self.E2E_LATENCY_MS // 2, expected_aborted_count=1, expected_recovered_count=0),
+            TestCase(timeout_delay_ms=self.INJECTED_LATENCY_MS // 2, expected_aborted_count=1, expected_recovered_count=0),
         ]
 
-        self.inject_e2e_latency(self.E2E_LATENCY_MS)
+        self.inject_e2e_latency(self.INJECTED_LATENCY_MS)
 
         for timeout_delay_ms, expected_abort_count, expected_recovered_count in test_cases:
             with self.subTest(
