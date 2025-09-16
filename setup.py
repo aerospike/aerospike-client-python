1bbf905f20921e1d795c8e0b5afce8a8a569fd16
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-09-16 12:46:17 -0700

    dead code. long description already provided by README in pyproject.toml

--- setup.py
+++ setup.py
@@ -207,10 +207,6 @@ os.environ['CPATH'] = ':'.join(include_dirs)
 # SETUP
 ################################################################################
 
-# Get the long description from the relevant file
-with io.open(os.path.join(CWD, 'README.rst'), "r", encoding='utf-8') as f:
-    long_description = f.read()
-
 # Get the version from the relevant file
 with io.open(os.path.join(CWD, 'VERSION'), "r", encoding='utf-8') as f:
     version = f.read()
