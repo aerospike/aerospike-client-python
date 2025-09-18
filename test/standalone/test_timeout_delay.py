7faf570e887344ce85f2fef85a963924e11647ec
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-09-18 15:43:26 -0700

    Use pytest to use a new client for each parametrized test case. Unittest does not make this easy with subtests

--- test/standalone/test_timeout_delay.py
+++ test/standalone/test_timeout_delay.py
@@ -1,5 +1,5 @@
 import aerospike
-import unittest
+import pytest
 import subprocess
 import time
 from collections import namedtuple
@@ -8,10 +8,6 @@ import os
 import docker
 from aerospike import exception as e
 
-TestCase = namedtuple(
-    "TestCase",
-    ["timeout_delay_ms", "expected_aborted_count", "expected_recovered_count"],
-)
 CONTAINER_NAME = "aerospike"
 PORT = 3000
 
@@ -20,10 +16,10 @@ PORT = 3000
 aerospike.set_log_level(aerospike.LOG_LEVEL_DEBUG)
 
 
-# Using unittest to check that exception was raised
-class TestTimeoutDelay(unittest.TestCase):
+class TestTimeoutDelay:
+    # Ensure that the server doesn't reap client connections
     @classmethod
-    def setUpClass(cls):
+    def setup_class(cls):
         cls.docker_client = docker.from_env()
         CUSTOM_AEROSPIKE_CONF_FOLDER = "/opt/aerospike/etc/"
         print("Running server container...")
@@ -50,7 +46,7 @@ class TestTimeoutDelay(unittest.TestCase):
         time.sleep(5)
 
     @classmethod
-    def tearDownClass(cls):
+    def teardown_class(cls):
         subprocess.run(args=["docker", "logs", CONTAINER_NAME])
         cls.container.stop()
         cls.docker_client.close()
@@ -58,7 +54,8 @@ class TestTimeoutDelay(unittest.TestCase):
     # latency is this high because timeout_delay must be >= 3000ms
     INJECTED_LATENCY_MS = 6000
 
-    def setUp(self):
+    @pytest.fixture
+    def as_client(self):
         config = {
             "hosts": [
                 # TODO: get ip addr from docker client
@@ -76,94 +73,81 @@ class TestTimeoutDelay(unittest.TestCase):
         self.key = ("test", "demo", 1)
         self.client.put(self.key, bins={"a": 1})
 
-    def tearDown(self):
-        DELETE_LATENCY_COMMAND = ["sudo", "tcdel", CONTAINER_NAME, "--docker", "--all"]
-        subprocess.run(args=DELETE_LATENCY_COMMAND, check=True)
-
-        self.client.close()
-
-    @staticmethod
-    def inject_e2e_latency(latency_ms: int):
         inject_latency_command = [
             "sudo",
             "tcset",
             CONTAINER_NAME,
             "--docker",
             "--delay",
-            f"{latency_ms}ms",
+            f"{self.INJECTED_LATENCY_MS}ms",
         ]
-        print(f"Injecting latency of {latency_ms} ms for outgoing packets from client to server")
+        print(f"Injecting latency of {self.INJECTED_LATENCY_MS} ms for outgoing packets from client to server")
         subprocess.run(args=inject_latency_command, check=True)
 
-    def test_case(self):
-        test_cases = [
-            # latency window < timeout delay window
-            # The connection will receive a response before the timeout delay window ends
-            # So the connection will be returned to the pool.
-            TestCase(
-                timeout_delay_ms=self.INJECTED_LATENCY_MS * 2,
-                expected_aborted_count=0,
-                expected_recovered_count=1,
-            ),
-            # latency window > timeout delay window
-            # The connection will not receive a response during the timeout delay window
-            # So the connection will be destroyed.
-            TestCase(
-                timeout_delay_ms=self.INJECTED_LATENCY_MS // 2,
-                expected_aborted_count=1,
-                expected_recovered_count=0,
-            ),
-        ]
+        yield
 
-        self.inject_e2e_latency(self.INJECTED_LATENCY_MS)
+        DELETE_LATENCY_COMMAND = ["sudo", "tcdel", CONTAINER_NAME, "--docker", "--all"]
+        subprocess.run(args=DELETE_LATENCY_COMMAND, check=True)
 
-        for timeout_delay_ms, expected_abort_count, expected_recovered_count in test_cases:
-            with self.subTest(
-                timeout_delay_ms=timeout_delay_ms,
-                expected_abort_count=expected_abort_count,
-                expected_recovered_count=expected_recovered_count,
-            ):
-                policy = {
-                    # socket_timeout should cause get() to timeout and trigger timeout delay window
-                    "socket_timeout": 1,
-                    "max_retries": 0,
-                    "total_timeout": 99999,
-                    "timeout_delay": timeout_delay_ms,
-                }
-                print(f"Trigger socket timeout and start timeout delay window of {timeout_delay_ms} ms...")
-                with self.assertRaises(e.TimeoutError):
-                    self.client.get(key=self.key, policy=policy)
+        self.client.close()
 
-                # When a connection is aborted, it will not be updated precisely when the timeout delay window
-                # ends. We need to wait for the tend thread to update the cluster's stats properly.
-                # So we wait a few seconds after the timeout delay window.
-                #
-                # This is also ok for the other test case (where the connection gets recovered)
-                time.sleep(timeout_delay_ms / 1000 + 2)
+    TestCase = namedtuple(
+        "TestCase",
+        ["timeout_delay_ms", "expected_aborted_count", "expected_recovered_count"],
+    )
 
-                print("Timeout delay window has ended.")
+    @pytest.mark.parametrize("timeout_delay_ms, expected_aborted_count, expected_recovered_count", [
+        # latency window < timeout delay window
+        # The connection will receive a response before the timeout delay window ends
+        # So the connection will be returned to the pool.
+        TestCase(
+            timeout_delay_ms=INJECTED_LATENCY_MS * 2,
+            expected_aborted_count=0,
+            expected_recovered_count=1,
+        ),
+        # latency window > timeout delay window
+        # The connection will not receive a response during the timeout delay window
+        # So the connection will be destroyed.
+        TestCase(
+            timeout_delay_ms=INJECTED_LATENCY_MS // 2,
+            expected_aborted_count=1,
+            expected_recovered_count=0,
+        ),
+    ])
+    def test_timeout_delay(self, timeout_delay_ms, expected_aborted_count, expected_recovered_count):
+        policy = {
+            # This socket_timeout and max_retries should cause get() to timeout immediately
+            # and trigger the timeout delay window
+            "socket_timeout": 1,
+            "max_retries": 0,
+            # total_timeout will override timeout_delay, so make sure it doesn't interfere with it.
+            "total_timeout": 99999,
+            "timeout_delay": timeout_delay_ms,
+        }
+        print(f"Trigger socket timeout and start timeout delay window of {timeout_delay_ms} ms...")
+        with pytest.raises(e.TimeoutError):
+            self.client.get(key=self.key, policy=policy)
 
-                # By now, we have passed the timeout delay window
-                # And we assume the tend thread has attempted to drain the connection
-                cluster_stats = self.client.get_stats()
-                print("Using standard metrics to get synchronous connection statistics...")
-                # If the first assert fails, we won't know the number of recovered connections
-                print("Num of aborted connections:", cluster_stats.nodes[0].conns.aborted)
-                print("Num of recovered connections:", cluster_stats.nodes[0].conns.recovered)
+        # When a connection is aborted, it will not be updated precisely when the timeout delay window
+        # ends. We need to wait for the tend thread to update the cluster's stats properly.
+        # So we wait a few seconds after the timeout delay window.
+        #
+        # This is also ok for the other test case (where the connection gets recovered)
+        time.sleep(timeout_delay_ms / 1000 + 2)
 
-                # DEBUG: check if server reaped a client connection
-                _, stdout = self.container.exec_run(cmd='sh -c "asinfo -v \'statistics\' -l | grep reaped_fds"')
-                print("Number of client connections reaped by server:", stdout)
+        print("Timeout delay window has ended.")
 
-                self.assertEqual(
-                    cluster_stats.nodes[0].conns.aborted, expected_abort_count,
-                    msg=f"Actual abort count = {cluster_stats.nodes[0].conns.aborted} doesn't match expected abort count = {expected_abort_count}"
-                )
-                self.assertEqual(
-                    cluster_stats.nodes[0].conns.recovered, expected_recovered_count,
-                    msg=f"Actual recovered count = {cluster_stats.nodes[0].conns.recovered} doesn't match expected recovered count = {expected_recovered_count}"
-                )
+        # By now, we have passed the timeout delay window
+        # And we assume the tend thread has attempted to drain the connection
+        cluster_stats = self.client.get_stats()
+        print("Using standard metrics to get synchronous connection statistics...")
+        # If the first assert fails, we won't know the number of recovered connections
+        print("Num of aborted connections:", cluster_stats.nodes[0].conns.aborted)
+        print("Num of recovered connections:", cluster_stats.nodes[0].conns.recovered)
 
+        # DEBUG: check if server reaped a client connection
+        _, stdout = self.container.exec_run(cmd='sh -c "asinfo -v \'statistics\' -l | grep reaped_fds"')
+        print("Number of client connections reaped by server:", stdout)
 
-if __name__ == "__main__":
-    unittest.main()
+        assert cluster_stats.nodes[0].conns.aborted == expected_aborted_count
+        assert cluster_stats.nodes[0].conns.recovered == expected_recovered_count
