2c2692f6916dad38b0d5a86a39ed2e68cbc84c9e
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-10-06 16:53:51 -0700

    fix

--- src/main/scan/execute_background.c
+++ src/main/scan/execute_background.c
@@ -61,10 +61,10 @@ PyObject *AerospikeScan_ExecuteBackground(AerospikeScan *self, PyObject *args,
     }
 
     if (py_policy) {
-        if (pyobject_to_policy_scan(self->client, &err, py_policy, &scan_policy,
-                                    &scan_policy_p,
-                                    &self->client->as->config.policies.scan,
-                                    &exp_list, &exp_list_p) != AEROSPIKE_OK) {
+        if (pyobject_to_policy_scan(
+                self->client, &err, py_policy, &scan_policy, &scan_policy_p,
+                &self->client->as->config.policies.scan, &exp_list, &exp_list_p,
+                false) != AEROSPIKE_OK) {
             goto CLEANUP;
         }
     }
