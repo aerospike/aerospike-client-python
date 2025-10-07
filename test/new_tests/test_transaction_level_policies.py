4417a03673a42cdbb63b066a381bdcde31a3cddd
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-10-07 12:27:08 -0700

    Add tests to cover command level policies taking in an invalid key

--- test/new_tests/test_transaction_level_policies.py
+++ test/new_tests/test_transaction_level_policies.py
@@ -2,7 +2,7 @@ import pytest
 from aerospike import exception as e
 import time
 
-from aerospike_helpers.operations import operations
+from aerospike_helpers.operations import operations, bitwise_operations, map_operations, list_operations, hll_operations
 from aerospike_helpers.batch import records as br
 from .test_base_class import TestBaseClass
 
@@ -100,7 +100,119 @@ class TestReadTouchTTLPercent:
         time.sleep(self.delay)
         self.as_connection.get(self.key)
 
+    # Test all policy code paths for invalid policy keys
+    # This codepath is only for command (e.g transaction)-level policies. Config level policies
+    # have a separate codepath.
+    # Read and write command policies with invalid keys already covered by other test cases, so
+    # we don't include test cases for them here.
     def test_invalid_policy_keys(self):
+        EXPECTED_ERROR_MESSAGE = '\"a\" is an invalid policy dictionary key'
+
+        # Operate policy
+        ops = [
+            operations.read(bin_name="a")
+        ]
         with pytest.raises(e.ParamError) as excinfo:
-            self.as_connection.get(self.key, policy={"a": "key"})
-        assert excinfo.value.msg == '\"a\" is an invalid policy dictionary key'
+            self.as_connection.operate(self.key, ops=ops, policy={"a": "key"})
+        assert excinfo.value.msg == EXPECTED_ERROR_MESSAGE
+
+        with pytest.raises(e.ParamError) as excinfo:
+            self.as_connection.admin_query_user_info(user="asdf", policy={"a": "key"})
+        assert excinfo.value.msg == EXPECTED_ERROR_MESSAGE
+
+        # Info policy
+        with pytest.raises(e.ParamError) as excinfo:
+            self.as_connection.truncate("test", "demo", 0, policy={"a": "key"})
+        assert excinfo.value.msg == EXPECTED_ERROR_MESSAGE
+
+        # Apply policy
+        with pytest.raises(e.ParamError) as excinfo:
+            self.as_connection.apply(self.key, "module", "function", args=[], policy={"a": "key"})
+        assert excinfo.value.msg == EXPECTED_ERROR_MESSAGE
+
+        # Scan policy
+        scan = self.as_connection.scan("test", "demo")
+        with pytest.raises(e.ParamError) as excinfo:
+            scan.results(policy={"a": "key"})
+        assert excinfo.value.msg == EXPECTED_ERROR_MESSAGE
+
+        # Query policy
+        query = self.as_connection.query("test", "demo")
+        with pytest.raises(e.ParamError) as excinfo:
+            query.results(policy={"a": "key"})
+        assert excinfo.value.msg == EXPECTED_ERROR_MESSAGE
+
+        # Remove policy
+        with pytest.raises(e.ParamError) as excinfo:
+            self.as_connection.remove(self.key, policy={"a": "key"})
+        assert excinfo.value.msg == EXPECTED_ERROR_MESSAGE
+
+        # Batch policy
+        with pytest.raises(e.ParamError) as excinfo:
+            self.as_connection.batch_read(keys=[self.key], policy={"a": "key"})
+        assert excinfo.value.msg == EXPECTED_ERROR_MESSAGE
+
+        # Batch write policy
+        with pytest.raises(e.ParamError) as excinfo:
+            self.as_connection.batch_operate(keys=[self.key], ops=ops, policy_batch_write={"a": "key"})
+        assert excinfo.value.msg == EXPECTED_ERROR_MESSAGE
+
+        # Batch read policy
+        batch_records = br.BatchRecords(
+            [
+                br.Read(
+                    key=("test", "demo", 1),
+                    ops=[
+                        operations.read("count"),
+                    ],
+                    policy={
+                        "a": "key"
+                    }
+                )
+            ]
+        )
+        with pytest.raises(e.ParamError) as excinfo:
+            self.as_connection.batch_write(batch_records)
+        assert excinfo.value.msg == EXPECTED_ERROR_MESSAGE
+
+        # Batch apply policy
+        with pytest.raises(e.ParamError) as excinfo:
+            self.as_connection.batch_apply(keys=[self.key], module="module", function="func", args=[], policy_batch_apply={"a": "key"})
+        assert excinfo.value.msg == EXPECTED_ERROR_MESSAGE
+
+        # Batch remove policy
+        with pytest.raises(e.ParamError) as excinfo:
+            self.as_connection.batch_remove(keys=[self.key], policy_batch_remove={"a": "key"})
+        assert excinfo.value.msg == EXPECTED_ERROR_MESSAGE
+
+        # Bit policy
+        ops = [
+            bitwise_operations.bit_not("bin", bit_offset=0, bit_size=0, bit_size=0, policy={"a": "key"})
+        ]
+        with pytest.raises(e.ParamError) as excinfo:
+            self.as_connection.operate(self.key, ops=ops)
+        assert excinfo.value.msg == EXPECTED_ERROR_MESSAGE
+
+        # Map policy
+        ops = [
+            map_operations.map_put("bin", "map_key", "map_value", map_policy={"a": "key"})
+        ]
+        with pytest.raises(e.ParamError) as excinfo:
+            self.as_connection.operate(self.key, ops=ops)
+        assert excinfo.value.msg == EXPECTED_ERROR_MESSAGE
+
+        # List policy
+        ops = [
+            list_operations.list_append("bin_name", "list_item_value", policy={"a": "key"})
+        ]
+        with pytest.raises(e.ParamError) as excinfo:
+            self.as_connection.operate(self.key, ops=ops)
+        assert excinfo.value.msg == EXPECTED_ERROR_MESSAGE
+
+        # HLL policy
+        ops = [
+            hll_operations.hll_add("bin_name", values=[1, 2, 3], policy={"a": "key"})
+        ]
+        with pytest.raises(e.ParamError) as excinfo:
+            self.as_connection.operate(self.key, ops=ops)
+        assert excinfo.value.msg == EXPECTED_ERROR_MESSAGE
