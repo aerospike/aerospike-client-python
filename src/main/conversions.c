9ced0c4c3787919f3dfae76974f22f0b7ff9e3fc
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-10-14 14:57:08 -0700

    Calling PyDict_* API on a non dictionary obj probably causing this seg fault

--- src/main/conversions.c
+++ src/main/conversions.c
@@ -2480,24 +2480,24 @@ as_status check_and_set_meta(PyObject *py_meta, uint32_t *ttl_ref,
                              uint16_t *gen_ref, as_error *err,
                              bool validate_keys)
 {
-    if (validate_keys) {
-        // TODO: assuming pyobject is a py_dict
-        // TODO: function signature needs to take in string instead of bool for last param
-        as_status retval = does_py_dict_contain_valid_keys(
-            err, py_meta, py_record_metadata_valid_keys, false);
-        if (retval == -1) {
-            // This shouldn't happen, but if it did...
-            // TODO: wrong error message
-            return as_error_update(err, AEROSPIKE_ERR,
-                                   ERR_MSG_FAILED_TO_VALIDATE_POLICY_KEYS);
-        }
-        else if (retval == 0) {
-            return err->code;
-        }
-    }
-
     as_error_reset(err);
     if (py_meta && PyDict_Check(py_meta)) {
+        if (validate_keys) {
+            // TODO: assuming pyobject is a py_dict
+            // TODO: function signature needs to take in string instead of bool for last param
+            as_status retval = does_py_dict_contain_valid_keys(
+                err, py_meta, py_record_metadata_valid_keys, false);
+            if (retval == -1) {
+                // This shouldn't happen, but if it did...
+                // TODO: wrong error message
+                return as_error_update(err, AEROSPIKE_ERR,
+                                       ERR_MSG_FAILED_TO_VALIDATE_POLICY_KEYS);
+            }
+            else if (retval == 0) {
+                return err->code;
+            }
+        }
+
         PyObject *py_gen = PyDict_GetItemString(py_meta, "gen");
         PyObject *py_ttl = PyDict_GetItemString(py_meta, "ttl");
         uint32_t ttl = 0;
