a56d8d796691655936006cf9a5da51ccdb438344
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-10-14 19:16:21 -0700

    Reduce nested if statements

--- src/main/conversions.c
+++ src/main/conversions.c
@@ -1449,10 +1449,10 @@ as_status as_record_init_from_pyobject(AerospikeClient *self, as_error *err,
     check_and_set_meta(py_meta, &rec->ttl, &rec->gen, err, self->validate_keys);
 
 CLEANUP:
+
     if (err->code != AEROSPIKE_OK) {
         as_record_destroy(rec);
     }
-
     return err->code;
 }
 
