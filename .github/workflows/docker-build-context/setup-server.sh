5175bca2bdb44d4cd955194c912f3825ce281480
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-10-28 13:44:52 -0700

    WIP...

--- .github/workflows/docker-build-context/setup-server.sh
+++ .github/workflows/docker-build-context/setup-server.sh
@@ -1,2 +1,3 @@
-asadm -h aerospike --enable --execute "manage revive ns test"
-asadm -h aerospike --enable --execute "manage recluster"
+asadm -h aerospike -U admin -P admin --enable --execute "manage acl grant role user-admin priv service-ctrl"
+asadm -h aerospike -U admin -P admin --enable --execute "manage revive ns test"
+asadm -h aerospike -U admin -P admin --enable --execute "manage recluster"
