f68c32747d02bd54f9bb1427050709a84f5c270f
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-10-30 13:16:28 -0700

    fix

--- test/new_tests/test_query_get_partitions_status.py
+++ test/new_tests/test_query_get_partitions_status.py
@@ -4,26 +4,52 @@ import pytest
 from .test_base_class import TestBaseClass
 
 
-class TestQueryGetPartitionsStatus(TestBaseClass):
-    @pytest.fixture(autouse=True, scope="class")
-    def setup(request, as_connection):
-        if request.cls.server_version < [6, 0]:
-            pytest.mark.xfail(reason="Servers older than 6.0 do not support partition queries.")
-            pytest.xfail()
+@pytest.fixture(autouse=True, scope="class")
+def setup(request, as_connection):
+    if request.cls.server_version < [6, 0]:
+        pytest.mark.xfail(reason="Servers older than 6.0 do not support partition queries.")
+        pytest.xfail()
 
-        request.cls.test_ns = "test"
-        request.cls.test_set = "demo"
+    request.cls.test_ns = "test"
+    request.cls.test_set = "demo"
 
-        request.cls.partition_1000_count = 0
-        request.cls.partition_1001_count = 0
-        request.cls.partition_1002_count = 0
-        request.cls.partition_1003_count = 0
+    request.cls.partition_1000_count = 0
+    request.cls.partition_1001_count = 0
+    request.cls.partition_1002_count = 0
+    request.cls.partition_1003_count = 0
 
-        as_connection.truncate(request.cls.test_ns, None, 0)
+    as_connection.truncate(request.cls.test_ns, None, 0)
 
+    for i in range(1, 100000):
+        put = 0
+        key = (request.cls.test_ns, request.cls.test_set, str(i))
+        rec_partition = as_connection.get_key_partition_id(request.cls.test_ns, request.cls.test_set, str(i))
+
+        if rec_partition == 1000:
+            request.cls.partition_1000_count += 1
+            put = 1
+        if rec_partition == 1001:
+            request.cls.partition_1001_count += 1
+            put = 1
+        if rec_partition == 1002:
+            request.cls.partition_1002_count += 1
+            put = 1
+        if rec_partition == 1003:
+            request.cls.partition_1003_count += 1
+            put = 1
+        if put:
+            rec = {
+                "i": i,
+                "s": "xyz",
+                "l": [2, 4, 8, 16, 32, None, 128, 256],
+                "m": {"partition": rec_partition, "b": 4, "c": 8, "d": 16},
+            }
+            as_connection.put(key, rec)
+
+    def teardown():
         for i in range(1, 100000):
             put = 0
-            key = (request.cls.test_ns, request.cls.test_set, str(i))
+            key = ("test", "demo", str(i))
             rec_partition = as_connection.get_key_partition_id(request.cls.test_ns, request.cls.test_set, str(i))
 
             if rec_partition == 1000:
@@ -39,37 +65,12 @@ class TestQueryGetPartitionsStatus(TestBaseClass):
                 request.cls.partition_1003_count += 1
                 put = 1
             if put:
-                rec = {
-                    "i": i,
-                    "s": "xyz",
-                    "l": [2, 4, 8, 16, 32, None, 128, 256],
-                    "m": {"partition": rec_partition, "b": 4, "c": 8, "d": 16},
-                }
-                as_connection.put(key, rec)
+                as_connection.remove(key)
 
-        def teardown():
-            for i in range(1, 100000):
-                put = 0
-                key = ("test", "demo", str(i))
-                rec_partition = as_connection.get_key_partition_id(request.cls.test_ns, request.cls.test_set, str(i))
+    request.addfinalizer(teardown)
 
-                if rec_partition == 1000:
-                    request.cls.partition_1000_count += 1
-                    put = 1
-                if rec_partition == 1001:
-                    request.cls.partition_1001_count += 1
-                    put = 1
-                if rec_partition == 1002:
-                    request.cls.partition_1002_count += 1
-                    put = 1
-                if rec_partition == 1003:
-                    request.cls.partition_1003_count += 1
-                    put = 1
-                if put:
-                    as_connection.remove(key)
 
-        request.addfinalizer(teardown)
-
+class TestQueryGetPartitionsStatus(TestBaseClass):
     def test_query_get_partitions_status_no_tracking(self):
         query_obj = self.as_connection.query(self.test_ns, self.test_set)
 
