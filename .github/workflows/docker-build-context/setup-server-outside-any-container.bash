e5b222f9171ca08ac66217a554dcba006fcbefb7
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-10-31 13:43:53 -0700

    Now works. But needs cleanup

--- .github/workflows/docker-build-context/setup-server-outside-any-container.bash
+++ .github/workflows/docker-build-context/setup-server-outside-any-container.bash
@@ -10,18 +10,19 @@ set -x
 AEROSPIKE_YAML_PATH=/workdir/aerospike-dev.yaml
 
 CALL_FROM_YQ_CONTAINER() {
-    docker run --rm -v ./:/workdir mikefarah/yq '$1' -i $AEROSPIKE_YAML_PATH
+    docker run --rm -v ./:/workdir mikefarah/yq "$1" -i $AEROSPIKE_YAML_PATH
+    cat aerospike-dev.yaml
 }
 
 if [[ "$MUTUAL_TLS" == "1" ]]; then
-    CALL_FROM_YQ_CONTAINER ".network.service.tls-authenticate-client = any"
-    CALL_FROM_YQ_CONTAINER ".network.service.tls-name = docker"
-    CALL_FROM_YQ_CONTAINER ".network.service.tls-port = 4333"
-    # TODO: needs volume for these certs
-    CALL_FROM_YQ_CONTAINER ".network.service.tls.ca-file = /etc/ssl/certs/ca.cer"
-    CALL_FROM_YQ_CONTAINER ".network.service.tls.cert-file = /etc/ssl/certs/server.cer"
-    CALL_FROM_YQ_CONTAINER ".network.service.tls.key-file = /etc/ssl/private/server.pem"
-    CALL_FROM_YQ_CONTAINER ".network.service.tls.name = docker"
+    CALL_FROM_YQ_CONTAINER ".network.service.tls-authenticate-client = \"any\""
+    CALL_FROM_YQ_CONTAINER ".network.service.tls-name = \"docker\""
+    CALL_FROM_YQ_CONTAINER ".network.service.tls-port = \"4333\""
+    CALL_FROM_YQ_CONTAINER ".network.service.tls = []"
+    CALL_FROM_YQ_CONTAINER ".network.tls[0].ca-file = \"/etc/ssl/certs/ca.cer\""
+    CALL_FROM_YQ_CONTAINER ".network.tls[0].cert-file = \"/etc/ssl/certs/server.cer\""
+    CALL_FROM_YQ_CONTAINER ".network.tls[0].key-file = \"/etc/ssl/private/server.pem\""
+    CALL_FROM_YQ_CONTAINER ".network.tls[0].name = \"docker\""
 else
     # TODO: Make sure these are idempotent
     CALL_FROM_YQ_CONTAINER "del(.network.service.tls-authenticate-client)"
@@ -31,23 +32,21 @@ else
 fi
 
 if [[ "$SECURITY" == "1" ]]; then
-    CALL_FROM_YQ_CONTAINER ".security.enable-quotas = false"
-    CALL_FROM_YQ_CONTAINER ".security.log.report-violation = true"
+    CALL_FROM_YQ_CONTAINER ".security.enable-quotas = \"false\""
+    CALL_FROM_YQ_CONTAINER ".security.log.report-violation = \"true\""
 else
     CALL_FROM_YQ_CONTAINER "del(.security)"
 fi
 
 if [[ "$STRONG_CONSISTENCY" == "1" ]]; then
-    CALL_FROM_YQ_CONTAINER ".namespaces[0].strong-consistency = true"
-    CALL_FROM_YQ_CONTAINER ".namespaces[0].strong-consistency-allow-expunge = true"
+    CALL_FROM_YQ_CONTAINER ".namespaces[0].strong-consistency = \"true\""
+    CALL_FROM_YQ_CONTAINER ".namespaces[0].strong-consistency-allow-expunge = \"true\""
 else
-    CALL_FROM_YQ_CONTAINER ".namespaces[0].strong-consistency = false"
-    CALL_FROM_YQ_CONTAINER ".namespaces[0].strong-consistency-allow-expunge = false"
+    CALL_FROM_YQ_CONTAINER ".namespaces[0].strong-consistency = \"false\""
+    CALL_FROM_YQ_CONTAINER ".namespaces[0].strong-consistency-allow-expunge = \"false\""
 fi
 
 CALL_FROM_TOOLS_CONTAINER="docker run --rm -v ./:/workdir --network host aerospike/aerospike-tools"
-
-cat $AEROSPIKE_YAML_PATH
 
 $CALL_FROM_TOOLS_CONTAINER asconfig convert -f $AEROSPIKE_YAML_PATH -o /workdir/aerospike.conf
 cat aerospike.conf
@@ -60,7 +59,7 @@ openssl x509 -req -in server.csr -CA ca.cer -CAkey ca.pem -out server.cer
 docker run -d --rm --name aerospike -p 4333:4333 -p 3000:3000 \
     -v ./ca.cer:/etc/ssl/certs/ca.cer \
     -v ./server.cer:/etc/ssl/certs/server.cer \
-    -v ./server.pem:/etc/ssl/certs/server.pem \
+    -v ./server.pem:/etc/ssl/private/server.pem \
     -v ./aerospike.conf:/custom-dir/aerospike.conf \
     aerospike/aerospike-server-enterprise --config-file /custom-dir/aerospike.conf
 
@@ -68,7 +67,7 @@ if [[ "$SECURITY" == "1" ]]; then
     export SECURITY_FLAGS="-U admin -P admin"
 fi
 
-docker logs -f aerospike
+# docker logs -f aerospike
 
 # TODO: make sure server container is alive or waiting is pointless
 while true; do
