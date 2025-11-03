dc674cc35e0a54ac664e28e176fbd27341807f12
Dominic Pelini <dpelini@aerospike.com>
2025-11-03 12:19:45 -0700

    Update conversions.c

--- src/main/conversions.c
+++ src/main/conversions.c
@@ -1452,7 +1452,7 @@ as_status as_record_init_from_pyobject(AerospikeClient *self, as_error *err,
         }
 
         as_val *val = NULL;
-        as_val_new_from_pyobject(self, err, py_bin_value, &val, static_pool,
+        as_val_new_from_pyobject(self, err, py_bin_value, &val, dynamic_pool,
                                  serializer_type);
         if (err->code != AEROSPIKE_OK) {
             goto CLEANUP;
