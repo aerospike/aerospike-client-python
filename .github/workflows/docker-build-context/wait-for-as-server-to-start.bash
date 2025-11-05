f012b62dbf4acbc05e097925212b0ad002ac2bbe
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-11-05 10:13:14 -0800

    Waiting for CE and EE container needs to be done without asinfo embedded in server container

--- .github/workflows/docker-build-context/wait-for-as-server-to-start.bash
+++ .github/workflows/docker-build-context/wait-for-as-server-to-start.bash
@@ -5,6 +5,8 @@ set -o pipefail
 
 # We use bash because we need the not (!) operator
 
+CALL_FROM_TOOLS_CONTAINER="docker run --rm -v ./:/workdir --network host aerospike/aerospike-tools"
+
 while true; do
     # Intermediate step is to send docker exec command's output to stdout in case it fails
     # Sometimes, errors only appear in stdout and not stderr, like if asinfo throws an error because of no credentials
@@ -13,7 +15,7 @@ while true; do
     # grep doesn't have a way to print all lines passed as input.
     # ack does have an option but it doesn't come installed by default
     echo "Checking if we can reach the server via the service port..."
-    if asinfo $SECURITY_FLAGS -v status | tee >(cat) | grep -qE "^ok"; then
+    if $CALL_FROM_TOOLS_CONTAINER asinfo $SECURITY_FLAGS -v status | tee >(cat) | grep -qE "^ok"; then
         # Server is ready when asinfo returns ok
         echo "Can reach server now."
         break
@@ -29,7 +31,7 @@ while true; do
     # The Dockerfile uses a roster from a previously running Aerospike server in a Docker container
     # When we reuse this roster, the server assumes all of its partitions are dead because it's running on a new
     # storage device. That is why we ignore-migrations here
-    if asinfo $SECURITY_FLAGS -v "cluster-stable:ignore-migrations=true" 2>&1 | (! grep -qE "^ERROR"); then
+    if $CALL_FROM_TOOLS_CONTAINER asinfo $SECURITY_FLAGS -v "cluster-stable:ignore-migrations=true" 2>&1 | (! grep -qE "^ERROR"); then
         echo "Server is in a stable state."
         break
     fi
