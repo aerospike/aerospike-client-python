6d5eae45e93d1cb791765dc7fffeac16efdcede6
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-12-02 13:34:07 -0800

    Add deprecation warning since aerospike.Scan,Query are not documented explicitly in the api docs

--- src/main/query/type.c
+++ src/main/query/type.c
@@ -172,6 +172,14 @@ static PyObject *AerospikeQuery_Type_New(PyTypeObject *type, PyObject *args,
 static int AerospikeQuery_Type_Init(AerospikeQuery *self, PyObject *args,
                                     PyObject *kwds)
 {
+    if (self->client == NULL) {
+        // TODO: experimenting with 2
+        PyErr_WarnEx(PyExc_DeprecationWarning,
+                     "aerospike.Query is deprecated. Please use "
+                     "aerospike.Client.query() instead",
+                     2);
+    }
+
     PyObject *py_namespace = NULL;
     PyObject *py_set = NULL;
 
