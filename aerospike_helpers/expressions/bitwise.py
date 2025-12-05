6bb5eefb95607fa27482749d77ab3506f6219328
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2025-12-05 09:21:32 -0800

    Revert aerospike_helpers/expressions/bitwise.py

--- aerospike_helpers/expressions/bitwise.py
+++ aerospike_helpers/expressions/bitwise.py
@@ -440,14 +440,8 @@ class BitAdd(_BaseExpr):
             bit_offset (int): Bit index of where to start operation.
             bit_size (int): Number of bits to be operated on.
             value (int): Integer value or expression for value to add.
-            action (aerospike.constant): Action taken if an overflow/underflow occurs.
-                Action must be one of the following:
-
-                - :data:`aerospike.BIT_OVERFLOW_FAIL`
-                - :data:`aerospike.BIT_OVERFLOW_SATURATE`
-                - :data:`aerospike.BIT_OVERFLOW_WRAP`
-
-                See :ref:`Bitwise Overflow <aerospike_bitwise_overflow>` for more information.
+            action (int): An aerospike bit overflow action. See
+                :ref:`Bitwise overflow actions <aerospike_bitwise_overflow>`.
             bin (TypeBinName): A :class:`~aerospike_helpers.expressions.base.BlobBin` expression.
 
         :return: resulting blob with the bits operated on.
@@ -498,14 +492,8 @@ class BitSubtract(_BaseExpr):
             bit_offset (int): Bit index of where to start operation.
             bit_size (int): Number of bits to be operated on.
             value (int): Integer value or expression for value to add.
-            action (aerospike.constant): Action taken if an overflow/underflow occurs.
-                Action must be one of the following:
-
-                - :data:`aerospike.BIT_OVERFLOW_FAIL`
-                - :data:`aerospike.BIT_OVERFLOW_SATURATE`
-                - :data:`aerospike.BIT_OVERFLOW_WRAP`
-
-                See :ref:`Bitwise Overflow <aerospike_bitwise_overflow>` for more information.
+            action (int): An aerospike bit overflow action. See
+                :ref:`Bitwise overflow actions <aerospike_bitwise_overflow>`.
             bin (TypeBinName): A :class:`~aerospike_helpers.expressions.base.BlobBin` expression.
 
         :return: resulting blob with the bits operated on.
