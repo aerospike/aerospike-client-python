963eee7b7f679dbd229f1d0ac6700fb80accee1e
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-01-08 10:23:53 -0800

    [CLIENT-2761] Client config: raise exception if a non-integer type is passed as a host port number (#770)

--- src/main/client/type.c
+++ src/main/client/type.c
@@ -752,18 +752,19 @@ static int AerospikeClient_Type_Init(AerospikeClient *self, PyObject *args,
                 if (PyUnicode_Check(py_addr)) {
                     addr = strdup((char *)PyUnicode_AsUTF8(py_addr));
                 }
+
                 py_port = PyTuple_GetItem(py_host, 1);
                 if (PyLong_Check(py_port)) {
                     port = (uint16_t)PyLong_AsLong(py_port);
                 }
                 else {
-                    PyErr_WarnEx(
-                        PyExc_FutureWarning,
-                        "In the next Python client major release, an exception "
-                        "will be raised if the port number is not an integer",
-                        2);
-                    port = 0;
+                    as_error_update(&constructor_err, AEROSPIKE_ERR_PARAM,
+                                    "The host port must be an integer");
+                    free(addr);
+                    error_code = INIT_INVALID_ADRR_ERR;
+                    goto CONSTRUCTOR_ERROR;
                 }
+
                 // Set TLS Name if provided
                 if (PyTuple_Size(py_host) == 3) {
                     py_tls_name = PyTuple_GetItem(py_host, 2);
