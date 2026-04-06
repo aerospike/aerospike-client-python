44c807587424dd7b0350b332c9e94ca8ce0dbea1
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-04-06 16:30:15 -0700

    Have sindex test case log in as user with only sindex-admin role.

--- test/new_tests/test_set_index.py
+++ test/new_tests/test_set_index.py
@@ -1,16 +1,31 @@
 import pytest
 import aerospike
 from .index_helpers import ensure_dropped_index
+from .test_base_class import TestBaseClass
 
 
 INDEX_NAME = "index_name"
 
 @pytest.mark.usefixtures("as_connection")
 class TestSetIndex:
-    @pytest.fixture(autouse=True)
-    def setup(self):
-        yield
+    @pytest.fixture
+    def client_as_sindex_admin_user(self):
+        USERNAME_AND_PASSWORD = "user_with_sindex_admin"
+        self.as_connection.admin_create_user(
+            username=USERNAME_AND_PASSWORD,
+            password=USERNAME_AND_PASSWORD,
+            roles=[
+                "sindex-admin"
+            ]
+        )
+
+        config = TestBaseClass.get_connection_config()
+        config["user"] = USERNAME_AND_PASSWORD
+        config["password"] = USERNAME_AND_PASSWORD
+
+        yield aerospike.client(config)
+
         ensure_dropped_index(self.as_connection, None, INDEX_NAME)
 
-    def test_create_set_index(self):
-        self.as_connection.index_set_create("test", "demo", INDEX_NAME)
+    def test_create_set_index(self, client_as_sindex_admin_user):
+        client_as_sindex_admin_user.index_set_create("test", "demo", INDEX_NAME)
