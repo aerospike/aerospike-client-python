204957daac4aa550f2cedf01801447390f3ae1e4
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-04-07 08:45:26 -0700

    Have test start with known environment

--- test/new_tests/test_set_index.py
+++ test/new_tests/test_set_index.py
@@ -1,7 +1,9 @@
 import pytest
 import aerospike
+from aerospike import exception as e
 from .index_helpers import ensure_dropped_index
 from .test_base_class import TestBaseClass
+import time
 
 
 INDEX_NAME = "index_name"
@@ -10,6 +12,14 @@ INDEX_NAME = "index_name"
 class TestSetIndex:
     @pytest.fixture
     def client_as_sindex_admin_user(self):
+        try:
+            self.as_connection.admin_drop_user(
+                user=USERNAME_AND_PASSWORD,
+            )
+            time.sleep(2)
+        except e.InvalidUser:
+            pass
+
         USERNAME_AND_PASSWORD = "user_with_sindex_admin"
         self.as_connection.admin_create_user(
             user=USERNAME_AND_PASSWORD,
@@ -26,9 +36,7 @@ class TestSetIndex:
         yield aerospike.client(config)
 
         ensure_dropped_index(self.as_connection, None, INDEX_NAME)
-        self.as_connection.admin_drop_user(
-            user=USERNAME_AND_PASSWORD,
-        )
+        # TODO: clean up steps should also be run here?
 
     def test_create_set_index(self, client_as_sindex_admin_user):
         client_as_sindex_admin_user.index_set_create("test", "demo", INDEX_NAME)
