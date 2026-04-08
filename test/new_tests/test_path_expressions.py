2172e61be99f25cab1fc053705379f2b4e8e78f5
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-04-08 14:25:04 -0700

    fix test case..

--- test/new_tests/test_path_expressions.py
+++ test/new_tests/test_path_expressions.py
@@ -757,8 +757,9 @@ class TestPathExprOperations:
                 flags=aerospike.EXP_PATH_SELECT_MAP_VALUE
             )
         ]
-        _, _, bins = self.as_connection.operate(self.key, ops)
-        assert set(bins[self.MAP_BIN_NAME]) == set(2, 3)
+        with self.expected_context_for_pos_tests:
+            _, _, bins = self.as_connection.operate(self.key, ops)
+            assert set(bins[self.MAP_BIN_NAME]) == set(2, 3)
 
     INT_GE_FILTER_EXPR = GE(LoopVarInt(aerospike.EXP_LOOPVAR_VALUE), 2).compile()
 
