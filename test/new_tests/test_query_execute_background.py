36927ea108c6ae9d500e4ccf3476b473d69078ab
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-04-10 11:28:09 -0700

    Fix test

--- test/new_tests/test_query_execute_background.py
+++ test/new_tests/test_query_execute_background.py
@@ -62,7 +62,7 @@ class TestQueryApply(object):
         res = query.execute_background()
         assert isinstance(res, (int, long))
 
-    @pytest.mark.xfail("This started failing when adding support for bin projection due to query.ttl not being applied")
+    @pytest.mark.xfail(reason="This started failing when adding support for bin projection due to query.ttl not being applied")
     def test_background_with_ttl(self, clean_test_background):
         """
         Ensure that ttl is set for the record found with background query
