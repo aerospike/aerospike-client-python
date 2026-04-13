0490c2ffa981b6adb290178ae5407af908505744
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-04-13 08:51:03 -0700

    Follow the sphinx autodoc docs to prevent sphinx from expanding this type alias

--- doc/conf.py
+++ doc/conf.py
@@ -120,7 +120,7 @@ autodoc_type_aliases = {
     "TypeIndex": "TypeIndex",
     "TypeChild": "TypeChild",
     "TypeCompiledOp": "TypeCompiledOp",
-    "TypeExpression": "TypeExpression",
+    "TypeExpression": "aerospike_helpers.expressions.resources.TypeExpression",
     "TypeGeo": "TypeGeo",
     "TypeKey": "TypeKey",
     "TypeKeyList": "TypeKeyList",
