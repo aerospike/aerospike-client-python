0ca32cbe7032ac3905425e9b30a19026fa9acb64
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-04-13 17:26:10 -0700

    Add test case

---
+++ test/new_tests/test_invalid_map_keys.py
@@ -0,0 +1,16 @@
+import pytest
+from .conftest import KEYS
+from aerospike import exception as e
+
+
+@pytest.mark.usefixtures("as_connection")
+class TestInvalidMapKeys:
+    def test_passing_invalid_map_keys_raises_exc(self):
+        KEY = KEYS[0]
+        invalid_map_in_server = {
+            4.0: 1
+        }
+        # TODO: should raise ParamError
+        # Python client can check if valid key, since C client doesn't raise a specific enough error in as_map_set
+        with pytest.raises(e.ClientError):
+            self.as_connection.put(KEY, bins={"map": invalid_map_in_server})
