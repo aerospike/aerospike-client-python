c03f0b9dd41b8e2b69c7c58d16e59fe3d23f6d42
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-04-16 15:03:31 -0700

    Read that Python 'eagerly' evaluates type annotations, so maybe we have to defer that

--- aerospike_helpers/batch/records.py
+++ aerospike_helpers/batch/records.py
@@ -14,7 +14,9 @@
 # limitations under the License.
 ##########################################################################
 
+from __future__ import annotations
 from typing import TYPE_CHECKING
+
 
 if TYPE_CHECKING:
     from typing import Optional, Any
