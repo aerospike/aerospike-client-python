0564dbad8127f284d8ad30a1efc9a6653d4fd295
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-04-16 13:30:11 -0700

    Add back missing type aliases. This won't expand out and will create a cross ref to TypeExpression

--- aerospike_helpers/cdt_ctx.py
+++ aerospike_helpers/cdt_ctx.py
@@ -307,7 +307,7 @@ def cdt_ctx_all_children() -> _cdt_ctx:
     """
     return _cdt_ctx(id=aerospike._AS_CDT_CTX_EXP)
 
-def cdt_ctx_all_children_with_filter(expression) -> _cdt_ctx:
+def cdt_ctx_all_children_with_filter(expression: "TypeExpression") -> _cdt_ctx:
     """
     All children of the current level will be selected, and then the filter expression
     is applied to each item in turn.  Items that cause the expression to evaluate to true will be added to the
@@ -320,7 +320,7 @@ def cdt_ctx_all_children_with_filter(expression) -> _cdt_ctx:
     """
     return _cdt_ctx(id=aerospike._AS_CDT_CTX_EXP, extra_args={aerospike._CDT_CTX_FILTER_EXPR_KEY: expression})
 
-def cdt_ctx_and_filter(expression) -> _cdt_ctx:
+def cdt_ctx_and_filter(expression: "TypeExpression") -> _cdt_ctx:
     """
     Add a boolean expression filter AND-combined with a previous :meth:`cdt_ctx_map_keys_in`.
 
