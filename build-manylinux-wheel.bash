8037098590b6d308c9daad6e693c6da6ac012be6
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-04-20 16:55:59 -0700

    Make sure absolute path of running script is used

--- build-manylinux-wheel.bash
+++ build-manylinux-wheel.bash
@@ -8,7 +8,7 @@ image_name=$1
 # Use UUID to minimize chance of colliding with another container
 CONTAINER_NAME=manylinux-$(uuidgen)
 
-this_files_dir=$(dirname "${BASH_SOURCE[0]}")
+this_files_dir=$(realpath "${BASH_SOURCE[0]}" | dirname)
 MOUNTED_REPO_DIR=/repo
 docker run -d -v "${this_files_dir}:${MOUNTED_REPO_DIR}/" --rm --name "$CONTAINER_NAME" "$image_name" tail -f /dev/null
 docker exec "$CONTAINER_NAME" $MOUNTED_REPO_DIR/build-prod-wheel.bash
