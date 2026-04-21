b40ad27354e5db542505c72f8087d5cd7cb8eedb
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-04-21 16:21:57 -0700

    Fix shared workflow failing because script is called using absolute path.

--- build-prod-wheel.bash
+++ build-prod-wheel.bash
@@ -7,17 +7,18 @@ manylinux_image_name=$1
 python_version=$2
 keep_container=$3
 
+this_file_abs_path=$(realpath "${BASH_SOURCE[0]}")
+dir_containing_this_file=$(dirname "$this_file_abs_path")
+
 if [[ -z "$manylinux_image_name" ]]; then
     # Build wheel natively
-    ./_build-prod-wheel.bash
+    "$dir_containing_this_file"/_build-prod-wheel.bash
     exit 0
 fi
 
 # Use UUID to minimize chance of colliding with another container
 CONTAINER_NAME=manylinux-$(uuidgen)
 
-this_file_abs_path=$(realpath "${BASH_SOURCE[0]}")
-dir_containing_this_file=$(dirname "$this_file_abs_path")
 MOUNTED_REPO_DIR=/repo
 docker run -d -v "${dir_containing_this_file}:${MOUNTED_REPO_DIR}/" --rm --name "$CONTAINER_NAME" "$manylinux_image_name" tail -f /dev/null
 
