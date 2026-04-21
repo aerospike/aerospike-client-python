b9f452ae95c2bc68737bf7bfc86b70b840d31f67
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-04-21 11:15:23 -0700

    By default, clean up the container. Otherwise for debugging, allow container to stay. Also clean up the container on error

--- build-manylinux-wheel.bash
+++ build-manylinux-wheel.bash
@@ -5,6 +5,7 @@ set -x
 
 image_name=$1
 python_version=$2
+keep_container=$3
 
 # Use UUID to minimize chance of colliding with another container
 CONTAINER_NAME=manylinux-$(uuidgen)
@@ -13,8 +14,16 @@ this_file_abs_path=$(realpath "${BASH_SOURCE[0]}")
 dir_containing_this_file=$(dirname "$this_file_abs_path")
 MOUNTED_REPO_DIR=/repo
 docker run -d -v "${dir_containing_this_file}:${MOUNTED_REPO_DIR}/" --rm --name "$CONTAINER_NAME" "$image_name" tail -f /dev/null
+
+cleanup() {
+    if [[ -z $keep_container ]]; then
+        docker stop "$CONTAINER_NAME"
+    fi
+}
+# TODO: Also stop container when sigkill/sigint is received
+trap 'on_failure' ERR
+
 docker exec "$CONTAINER_NAME" git config --global --add safe.directory "$MOUNTED_REPO_DIR"
 docker exec -w "$MOUNTED_REPO_DIR" "$CONTAINER_NAME" ./build-prod-wheel.bash "$python_version"
 
-# TODO: Also stop container when sigkill/sigint is received
-docker stop "$CONTAINER_NAME"
+cleanup
