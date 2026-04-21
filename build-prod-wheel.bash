444f1d52239a85d31e130d7ba9911db2e6ab1652
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-04-21 16:57:52 -0700

    As workaround for having no way to pass args to build script, use env vars instead. TODO - need to pass matrix-specific values

--- build-prod-wheel.bash
+++ build-prod-wheel.bash
@@ -3,16 +3,14 @@
 set -e
 set -x
 
-manylinux_image_name=$1
-python_version=$2
-keep_container=$3
+KEEP_CONTAINER=$1
 
 this_file_abs_path=$(realpath "${BASH_SOURCE[0]}")
 dir_containing_this_file=$(dirname "$this_file_abs_path")
 
 helper_script=_build-prod-wheel-natively.bash
 
-if [[ -z "$manylinux_image_name" ]]; then
+if [[ -z "$MANYLINUX_IMAGE_NAME" ]]; then
     # Build wheel natively
     "$dir_containing_this_file/${helper_script}"
     exit 0
@@ -22,14 +20,14 @@ fi
 CONTAINER_NAME=manylinux-$(uuidgen)
 
 MOUNTED_REPO_DIR=/repo
-docker run -d -v "${dir_containing_this_file}:${MOUNTED_REPO_DIR}/" --rm --name "$CONTAINER_NAME" "$manylinux_image_name" tail -f /dev/null
+docker run -d -v "${dir_containing_this_file}:${MOUNTED_REPO_DIR}/" --rm --name "$CONTAINER_NAME" "$MANYLINUX_IMAGE_NAME" tail -f /dev/null
 
 cleanup() {
-    if [[ -z $keep_container ]]; then
+    if [[ -z $KEEP_CONTAINER ]]; then
         docker stop "$CONTAINER_NAME"
     fi
 }
 trap 'cleanup' EXIT SIGINT SIGTERM ERR
 
 docker exec "$CONTAINER_NAME" git config --global --add safe.directory "$MOUNTED_REPO_DIR"
-docker exec -w "$MOUNTED_REPO_DIR" "$CONTAINER_NAME" ./${helper_script} "$python_version"
+docker exec -w "$MOUNTED_REPO_DIR" "$CONTAINER_NAME" ./${helper_script} "$PYTHON_VERSION"
