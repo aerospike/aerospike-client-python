b9676ba3ddfeda2f9de47ba118aad5066c8d27d6
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-04-26 11:17:18 -0700

    Add doctest to test code examples in aerospike module rst docs. TODO - this may make cicd check redundant in doc tests.

--- doc/conf.py
+++ doc/conf.py
@@ -37,6 +37,7 @@ sys.modules.update({"aerospike": Mock()})
 extensions = [
     "sphinx.ext.todo",
     "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
     "sphinx.ext.intersphinx",
     "sphinx.ext.napoleon",
     "sphinxcontrib.spelling"
