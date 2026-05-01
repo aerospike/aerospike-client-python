cfc645bd19a285a396bb2417c156e90092ab8cdb
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-05-01 15:20:05 -0700

    Currently getting server code 400 response with jf docker pull. Try pulling with the actual docker client to address that error

--- build-prod-wheel.bash
+++ build-prod-wheel.bash
@@ -35,7 +35,7 @@ MANYLINUX_IMAGE_NAME="$MANYLINUX_REGISTRY_NAME/manylinux_2_28_${arch}@sha256:$MA
 CONTAINER_NAME=manylinux-$(uuidgen)
 
 MOUNTED_REPO_DIR=/repo
-jf docker pull "$MANYLINUX_IMAGE_NAME" --build-name "$JF_BUILD_NAME" --build-number "$JF_BUILD_NUMBER"
+jf docker login artifact.aerospike.io/database-docker-dev-local
 docker run -d -v "${dir_containing_this_file}:${MOUNTED_REPO_DIR}/" --rm --name "$CONTAINER_NAME" "$MANYLINUX_IMAGE_NAME" tail -f /dev/null
 
 cleanup() {
