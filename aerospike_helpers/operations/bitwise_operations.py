2e0c039ad878430caabd06f29d1f27fc9ec16c7a
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-05-07 11:09:38 -0700

    Just ignore todo for this pr

--- aerospike_helpers/operations/bitwise_operations.py
+++ aerospike_helpers/operations/bitwise_operations.py
@@ -167,11 +167,9 @@ def bit_resize(bin_name: str, byte_size, policy=None, resize_flags: int = 0):
         bin_name (str): The name of the bin containing the map.
         byte_size (int): The new size of the bytes.
         policy (dict): The :ref:`bit_policy <aerospike_bit_policies>` dictionary. default: None.
-        # TODO: this is the only method referencing these flags, but should document explicitly that
-        # the flags can be bitwise OR'd together.
         resize_flags (int): :ref:`aerospike_bitwise_resize_flag` modifying the resize behavior
-            (default ``aerospike.BIT_RESIZE_DEFAULT``), such as ``aerospike.BIT_RESIZE_GROW_ONLY |
-            aerospike.BIT_RESIZE_FROM_FRONT``.
+            (default :py:data:`aerospike.BIT_RESIZE_DEFAULT`), such as py:data:`aerospike.BIT_RESIZE_GROW_ONLY` |
+            :py:data:`aerospike.BIT_RESIZE_FROM_FRONT`.
 
     Returns:
         A dictionary usable in operate or operate_ordered. The format of the dictionary
