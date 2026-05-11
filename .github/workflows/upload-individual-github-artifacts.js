fb97ccb69089e15daffb67b09fefd3b6fb441f9c
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-05-11 14:10:37 -0700

    Get the python version and platform tag from the wheel file name instead of using the whole file name as part of the artifact name. This makes it easier to pull artifacts using the exact python version + platform tag without accidentally pulling the sanitizer artifact

--- .github/workflows/upload-individual-github-artifacts.js
+++ .github/workflows/upload-individual-github-artifacts.js
@@ -1,5 +1,6 @@
 const { DefaultArtifactClient } = require('@actions/artifact');
 const fs = require('fs');
+const { exec } = require('child_process');
 
 const jfrog_artifacts_folder_path=process.argv[1]
 const gh_artifact_name_prefix=process.argv[2]
@@ -11,8 +12,19 @@ async function upload() {
   const files = fs.readdirSync(process.cwd());
 
   for (const file of files) {
+    // Get platform tag of wheel
+    // Python tag - ABI tag - platform tag
+    const matches = file.match(/.*([a-z0-9]+)-[A-Z0-9.]+-(.+)\.whl$/);
+
+    const python_tag = matches[0];
+    // Ignore cp- prefix of python tag
+    // Concats major version with the minor version using a dot
+    const python_version = python_tag.slice(2, 3) + '.' + python_tag.slice(3);
+
+    const platform_tag = matches[1];
+
     await client.uploadArtifact(
-      `${gh_artifact_name_prefix}-${file}`, // Unique name that will be selected downstream by a glob pattern
+      `${gh_artifact_name_prefix}-${python_version}-${platform_tag}`, // Unique name that will be selected downstream by a glob pattern
       [`${file}`], // File path
       './' // Root directory
     );
