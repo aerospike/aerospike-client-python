86612d653f0b35f0bc5039092ae57435ed357cb4
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-05-14 13:13:07 -0700

    Use esm syntax instead of cjs to fix import errors from actions/artifact.

--- .github/workflows/upload-individual-github-artifacts.js
+++ .github/workflows/upload-individual-github-artifacts.js
@@ -1,6 +1,6 @@
-const { DefaultArtifactClient } = require('@actions/artifact');
-const fs = require('fs');
-const { exec } = require('child_process');
+import { DefaultArtifactClient } from '@actions/artifact';
+import * as fs from 'fs';
+import { exec } from 'child_process';
 
 const jfrog_artifacts_folder_path=process.argv[1]
 const gh_artifact_name_prefix=process.argv[2]
