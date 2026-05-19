eb6a5c095511116924ea9c535230b366619b335b
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-05-19 10:28:38 -0700

    Add 2nd test case where multiple clients share the same shared memory

--- test/new_tests/test_shared_memory.py
+++ test/new_tests/test_shared_memory.py
@@ -11,3 +11,15 @@ class TestSharedMemory:
         assert client.is_connected()
 
         client.close()
+
+    def test_multiple_clients(self):
+        self.connection_config["use_shared_connection"] = True
+
+        client1 = aerospike.client(self.connection_config)
+        assert client1.is_connected()
+
+        client2 = aerospike.client(self.connection_config)
+        assert client2.is_connected()
+
+        client1.close()
+        client2.close()
