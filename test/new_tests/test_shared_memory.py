e7252e1c5147c97423a38aae1deabcb655637e8c
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-05-19 10:17:30 -0700

    Address test syntax failing

--- test/new_tests/test_shared_memory.py
+++ test/new_tests/test_shared_memory.py
@@ -1,6 +1,8 @@
 import aerospike
+import pytest
 
 
+@pytest.mark.usefixtures("connection_config")
 class TestSharedMemory:
     def test_one_client(self):
         self.connection_config["use_shared_connection"] = True
