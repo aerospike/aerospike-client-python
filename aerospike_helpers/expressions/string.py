d21568aa3afa8a43acd3c610eeb4afb80d9ff400
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-05-22 11:00:27 -0700

    Finish all string expression stubs. TODO now need to implement

--- aerospike_helpers/expressions/string.py
+++ aerospike_helpers/expressions/string.py
@@ -1,4 +1,5 @@
 from aerospike_helpers.expressions.resources import _BaseExpr
+from aerospike_helpers.operations.string_operations import RegexFlags, StringPolicy
 
 
 class StrLen(_BaseExpr):
@@ -65,4 +66,113 @@ class ToLower(_BaseExpr):
     def __init__(self, src: str):
         pass
 
-# TODO
+
+class ToBlob(_BaseExpr):
+    def __init__(self, src: str):
+        pass
+
+
+class Split(_BaseExpr):
+    def __init__(self, separator: str, src: str):
+        pass
+
+
+class Base64Decode(_BaseExpr):
+    def __init__(self, src: str):
+        pass
+
+
+class RegexCompare(_BaseExpr):
+    # TODO: set default flags
+    def __init__(self, pattern: str, regex_flags: RegexFlags):
+        pass
+
+
+class Insert(_BaseExpr):
+    def __init__(self, policy: StringPolicy, index: int, value: str, src: str):
+        pass
+
+
+class Overwrite(_BaseExpr):
+    def __init__(self, policy: StringPolicy, index: int, value: str, src: str):
+        pass
+
+
+class Concat(_BaseExpr):
+    def __init__(self, policy: StringPolicy, values: list[str], src: str):
+        pass
+
+
+class Snip(_BaseExpr):
+    def __init__(self, policy: StringPolicy, start: int, end: int | None, src: str):
+        pass
+
+
+class Replace(_BaseExpr):
+    def __init__(self, policy: StringPolicy, needle: str, replacement: str, src: str):
+        pass
+
+
+class ReplaceAll(_BaseExpr):
+    def __init__(self, policy: StringPolicy, needle: str, replacement: str, src: str):
+        pass
+
+
+class Upper(_BaseExpr):
+    def __init__(self, policy: StringPolicy, src: str):
+        pass
+
+
+class Lower(_BaseExpr):
+    def __init__(self, policy: StringPolicy, src: str):
+        pass
+
+
+class CaseFold(_BaseExpr):
+    def __init__(self, policy: StringPolicy, src: str):
+        pass
+
+
+class NormalizeNFC(_BaseExpr):
+    def __init__(self, policy: StringPolicy, src: str):
+        pass
+
+
+class TrimStart(_BaseExpr):
+    def __init__(self, policy: StringPolicy, src: str):
+        pass
+
+
+class TrimEnd(_BaseExpr):
+    def __init__(self, policy: StringPolicy, src: str):
+        pass
+
+
+class Trim(_BaseExpr):
+    def __init__(self, policy: StringPolicy, src: str):
+        pass
+
+
+class PadStart(_BaseExpr):
+    def __init__(self, policy: StringPolicy, target_length: int, pad_string: str, src: str):
+        pass
+
+
+class PadEnd(_BaseExpr):
+    def __init__(self, policy: StringPolicy, target_length: int, pad_string: str, src: str):
+        pass
+
+
+class Repeat(_BaseExpr):
+    def __init__(self, policy: StringPolicy, count: int, src: str):
+        pass
+
+
+class RegexReplace(_BaseExpr):
+    def __init__(self, policy: StringPolicy, pattern: str, replacement: int, regex_flags: RegexFlags, src: str):
+        pass
+
+
+class ToString(_BaseExpr):
+    def __init__(self, src: str):
+        pass
