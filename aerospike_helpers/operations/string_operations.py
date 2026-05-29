9859e1e83a7dbc4d0455af55a965d01dad7dd07e
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-05-29 14:47:13 -0700

    Use TypeCTX to be defensive; in the future, TypeCTX can also include a base64 encoded string

--- aerospike_helpers/operations/string_operations.py
+++ aerospike_helpers/operations/string_operations.py
@@ -1,73 +1,78 @@
 from enum import IntEnum
 
+from typing import TYPE_CHECKING
 
-def strlen(bin_name: str, ctx: list | None = None):
+if TYPE_CHECKING:
+    from aerospike_helpers.expressions.list import TypeCTX
+
+
+def strlen(bin_name: str, ctx: TypeCTX = None):
     pass
 
 
-def substr(bin_name: str, start: int, length: int | None = None, ctx: list | None = None):
+def substr(bin_name: str, start: int, length: int | None = None, ctx: TypeCTX = None):
     pass
 
 
-def char_at(bin_name: str, index: int, ctx: list | None = None):
+def char_at(bin_name: str, index: int, ctx: TypeCTX = None):
     pass
 
 
-def find(bin_name: str, needle: int, occurrence: int | None = None, ctx: list | None = None):
+def find(bin_name: str, needle: int, occurrence: int | None = None, ctx: TypeCTX = None):
     pass
 
 
-def contains(bin_name: str, index: int, ctx: list | None = None):
+def contains(bin_name: str, index: int, ctx: TypeCTX = None):
     pass
 
 
-def starts_with(bin_name: str, prefix: str, ctx: list | None = None):
+def starts_with(bin_name: str, prefix: str, ctx: TypeCTX = None):
     pass
 
 
-def ends_with(bin_name: str, suffix: str, ctx: list | None = None):
+def ends_with(bin_name: str, suffix: str, ctx: TypeCTX = None):
     pass
 
 
-def to_integer(bin_name: str, ctx: list | None = None):
+def to_integer(bin_name: str, ctx: TypeCTX = None):
     pass
 
 
-def to_double(bin_name: str, ctx: list | None = None):
+def to_double(bin_name: str, ctx: TypeCTX = None):
     pass
 
 
-def byte_length(bin_name: str, ctx: list | None = None):
+def byte_length(bin_name: str, ctx: TypeCTX = None):
     pass
 
 
-def is_numeric(bin_name: str, numeric_type: int | None = None, ctx: list | None = None):
+def is_numeric(bin_name: str, numeric_type: int | None = None, ctx: TypeCTX = None):
     pass
 
 
-def is_upper(bin_name: str, ctx: list | None = None):
+def is_upper(bin_name: str, ctx: TypeCTX = None):
     pass
 
 
-def is_lower(bin_name: str, ctx: list | None = None):
+def is_lower(bin_name: str, ctx: TypeCTX = None):
     pass
 
 
-def to_blob(bin_name: str, ctx: list | None = None):
+def to_blob(bin_name: str, ctx: TypeCTX = None):
     pass
 
 
-def split(bin_name: str, separator: str, ctx: list | None = None):
+def split(bin_name: str, separator: str, ctx: TypeCTX = None):
     pass
 
 
-def base64_decode(bin_name: str, ctx: list | None = None):
+def base64_decode(bin_name: str, ctx: TypeCTX = None):
     pass
 
 # TODO: use enum for regex flags
 
 
-def regex_compare(bin_name: str, pattern: str, regex_flags: int, ctx: list | None = None):
+def regex_compare(bin_name: str, pattern: str, regex_flags: int, ctx: TypeCTX = None):
     pass
 
 
@@ -96,67 +101,67 @@ class NumericType(IntEnum):
     FLOAT = 2
 
 
-def insert(policy: StringPolicy, bin_name: str, index: int, value: str, ctx: list | None = None):
+def insert(policy: StringPolicy, bin_name: str, index: int, value: str, ctx: TypeCTX = None):
     pass
 
 
-def overwrite(policy: StringPolicy, bin_name: str, index: int, value: str, ctx: list | None = None):
+def overwrite(policy: StringPolicy, bin_name: str, index: int, value: str, ctx: TypeCTX = None):
     pass
 
 
-def concat(policy: StringPolicy, bin_name: str, value: str | list[str], ctx: list | None = None):
+def concat(policy: StringPolicy, bin_name: str, value: str | list[str], ctx: TypeCTX = None):
     pass
 
 
-def snip(policy: StringPolicy, bin_name: str, start: int, end: int | None = None, ctx: list | None = None):
+def snip(policy: StringPolicy, bin_name: str, start: int, end: int | None = None, ctx: TypeCTX = None):
     pass
 
 
-def replace(policy: StringPolicy, bin_name: str, needle: str, replacement: str, ctx: list | None = None):
+def replace(policy: StringPolicy, bin_name: str, needle: str, replacement: str, ctx: TypeCTX = None):
     pass
 
 
-def replace_all(policy: StringPolicy, bin_name: str, needle: str, replacement: str, ctx: list | None = None):
+def replace_all(policy: StringPolicy, bin_name: str, needle: str, replacement: str, ctx: TypeCTX = None):
     pass
 
 
-def upper(policy: StringPolicy, bin_name: str, ctx: list | None = None):
+def upper(policy: StringPolicy, bin_name: str, ctx: TypeCTX = None):
     pass
 
 
-def lower(policy: StringPolicy, bin_name: str, ctx: list | None = None):
+def lower(policy: StringPolicy, bin_name: str, ctx: TypeCTX = None):
     pass
 
 
-def casefold(policy: StringPolicy, bin_name: str, ctx: list | None = None):
+def casefold(policy: StringPolicy, bin_name: str, ctx: TypeCTX = None):
     pass
 
 
-def normalize_nfc(policy: StringPolicy, bin_name: str, ctx: list | None = None):
+def normalize_nfc(policy: StringPolicy, bin_name: str, ctx: TypeCTX = None):
     pass
 
 
-def trim_start(policy: StringPolicy, bin_name: str, ctx: list | None = None):
+def trim_start(policy: StringPolicy, bin_name: str, ctx: TypeCTX = None):
     pass
 
 
-def trim_end(policy: StringPolicy, bin_name: str, ctx: list | None = None):
+def trim_end(policy: StringPolicy, bin_name: str, ctx: TypeCTX = None):
     pass
 
 
-def trim(policy: StringPolicy, bin_name: str, ctx: list | None = None):
+def trim(policy: StringPolicy, bin_name: str, ctx: TypeCTX = None):
     pass
 
 
-def pad_start(policy: StringPolicy, bin_name: str, target_length: int, pad_string: str, ctx: list | None = None):
+def pad_start(policy: StringPolicy, bin_name: str, target_length: int, pad_string: str, ctx: TypeCTX = None):
     pass
 
 
-def pad_end(policy: StringPolicy, bin_name: str, target_length: int, pad_string: str, ctx: list | None = None):
+def pad_end(policy: StringPolicy, bin_name: str, target_length: int, pad_string: str, ctx: TypeCTX = None):
     pass
 
 
-def repeat(policy: StringPolicy, bin_name: str, count: int, ctx: list | None = None):
+def repeat(policy: StringPolicy, bin_name: str, count: int, ctx: TypeCTX = None):
     pass
 
 # TODO: regex flags enum
@@ -168,7 +173,7 @@ def regex_replace(
         pattern: str,
         replacement: str,
         regex_flags: int,
-        ctx: list | None = None
+        ctx: TypeCTX = None
 ):
     pass
 
