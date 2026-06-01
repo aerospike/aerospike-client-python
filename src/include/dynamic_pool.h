5095bf8e9ebf821d3a4282e55fcb1a567d943196
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-05-31 19:33:06 -0700

    GET_BYTES_POOL -> dynamic_pool_init

--- src/include/dynamic_pool.h
+++ src/include/dynamic_pool.h
@@ -59,6 +59,33 @@ static inline void dynamic_pool_init(as_dynamic_pool *dynamic_pool,
 }
 
 /**
+ * Fetches the address of the next as_byte in the pool.
+ *
+ * @param map_bytes Pointer to an as_bytes.
+ * @param dynamic_pool Pointer to a dynamic pool.
+ * @param err Pointer to an as_error
+ */
+static inline as_bytes *GET_BYTES_POOL(as_dynamic_pool *dynamic_pool,
+                                       as_error *err)
+{
+    as_bytes **table = dynamic_pool->byte_group_table;
+
+    if (table == NULL) {
+        dynamic_pool_init(dynamic_pool, err);
+    }
+    else if (dynamic_pool->byte_iterator >= dynamic_pool->bytes_per_group) {
+        dynamic_pool_add_group(dynamic_pool, err);
+    }
+
+    table = dynamic_pool->byte_group_table;
+    uint16_t group_iterator = dynamic_pool->group_iterator;
+    as_bytes *group = table[group_iterator];
+    uint16_t byte_iterator = dynamic_pool->byte_iterator++;
+
+    return &group[byte_iterator];
+}
+
+/**
  * Allocates a group of as_bytes to the table.
  *
  * @param dynamic_pool Pointer to a dynamic pool.
@@ -237,33 +264,6 @@ static inline void dynamic_pool_add_group(as_dynamic_pool *dynamic_pool,
 #define BYTE_POOL_INIT_NULL(dynamic_pool)                                      \
     (dynamic_pool)->byte_group_table = NULL;                                   \
     (dynamic_pool)->allocate_buffers = false;
-
-/**
- * Fetches the address of the next as_byte in the pool.
- *
- * @param map_bytes Pointer to an as_bytes.
- * @param dynamic_pool Pointer to a dynamic pool.
- * @param err Pointer to an as_error
- */
-static inline as_bytes *GET_BYTES_POOL(as_dynamic_pool *dynamic_pool,
-                                       as_error *err)
-{
-    as_bytes **table = dynamic_pool->byte_group_table;
-
-    if (table == NULL) {
-        dynamic_pool_init(dynamic_pool, err);
-    }
-    else if (dynamic_pool->byte_iterator >= dynamic_pool->bytes_per_group) {
-        dynamic_pool_add_group(dynamic_pool, err);
-    }
-
-    table = dynamic_pool->byte_group_table;
-    uint16_t group_iterator = dynamic_pool->group_iterator;
-    as_bytes *group = table[group_iterator];
-    uint16_t byte_iterator = dynamic_pool->byte_iterator++;
-
-    return &group[byte_iterator];
-}
 
 /**
  * Destroy the dynamic pool. Must be called before the dynamic_pool loses scope.
