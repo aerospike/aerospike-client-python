91174c6d22bb16d0f7409dafd0f035e133b0cc76
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-05-31 18:04:30 -0700

    Address compiler error

--- src/main/client/batch_apply.c
+++ src/main/client/batch_apply.c
@@ -179,8 +179,8 @@ static PyObject *AerospikeClient_Batch_Apply_Invoke(
     if (py_policy_batch) {
         if (pyobject_to_policy_batch(
                 self, err, py_policy_batch, &policy_batch, &policy_batch_p,
-                &self->as->config.policies.batch, &batch_exp_list_p,
-                &dynamic_pool) != AEROSPIKE_OK) {
+                &self->as->config.policies.batch, &dynamic_pool,
+                &batch_exp_list_p) != AEROSPIKE_OK) {
             goto CLEANUP;
         }
     }
@@ -188,8 +188,8 @@ static PyObject *AerospikeClient_Batch_Apply_Invoke(
     if (py_policy_batch_apply) {
         if (pyobject_to_batch_apply_policy(
                 self, err, py_policy_batch_apply, &policy_batch_apply,
-                &policy_batch_apply_p, &batch_apply_exp_list_p,
-                &dynamic_pool) != AEROSPIKE_OK) {
+                &policy_batch_apply_p, &dynamic_pool,
+                &batch_apply_exp_list_p) != AEROSPIKE_OK) {
             goto CLEANUP;
         }
     }
