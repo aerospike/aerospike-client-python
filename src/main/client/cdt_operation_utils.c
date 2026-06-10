5aa1191012fedf64796dc17a50b8ce99095e4bda
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-06-10 12:41:12 -0700

    length argument can be None. None represents no end boundary in this case.

--- src/main/client/cdt_operation_utils.c
+++ src/main/client/cdt_operation_utils.c
@@ -167,7 +167,7 @@ as_status get_optional_uint64_t(as_error *err, const char *key,
 {
     *found = false;
     PyObject *py_val = PyDict_GetItemString(op_dict, key);
-    if (!py_val) {
+    if (!py_val || Py_IsNone(py_val)) {
         return AEROSPIKE_OK;
     }
 
