8ee462f24dbc9755e2ab78e75a20d9024eeb9681
juliannguyen4 <109386615+juliannguyen4@users.noreply.github.com>
2026-06-16 19:13:33 +0000

    Address all test failures.

--- test/new_tests/test_exception_subcode.py
+++ test/new_tests/test_exception_subcode.py
@@ -15,7 +15,8 @@ ERROR_DETAIL_VERBOSITY_SETTING = "error_detail_verbosity"
 
 class TestExceptionSubcode:
     # TODO: need to reuse fixture in conftest.py using indirect params to set num of records
-    def setup(self):
+    @pytest.fixture(autouse=True)
+    def setup(self, as_connection):
         self.as_connection.put(KEY, bins={BIN_NAME: []})
         yield
         self.as_connection.remove(KEY)
@@ -30,18 +31,19 @@ class TestExceptionSubcode:
         ]
     )
     def test_minimum_error_verbosity(self, policy: dict):
-        with pytest.raises(e.InvalidRequest) as excinfo:
+        with pytest.raises(e.OpNotApplicable) as excinfo:
             self.as_connection.operate(KEYS[0], OPS, policy=policy)
 
         # Make sure there's no regression with the parent error code
-        assert excinfo.value.code == as_errors.AEROSPIKE_ERR_REQUEST_INVALID
-        if policy[ERROR_DETAIL_VERBOSITY_SETTING] == 0:
+        assert excinfo.value.code == as_errors.AEROSPIKE_ERR_OP_NOT_APPLICABLE
+        err_verbosity_is_zero = ERROR_DETAIL_VERBOSITY_SETTING not in policy or policy[ERROR_DETAIL_VERBOSITY_SETTING] == 0
+        if err_verbosity_is_zero:
             assert excinfo.value.subcode == 0
         else:
             assert excinfo.value.subcode > 0
 
         SUBCODE_IN_MESSAGE = "subcode="
-        if policy[ERROR_DETAIL_VERBOSITY_SETTING] == 0:
+        if err_verbosity_is_zero:
             assert SUBCODE_IN_MESSAGE not in excinfo.value.msg
         elif policy[ERROR_DETAIL_VERBOSITY_SETTING] == 1:
             # Make sure there's no regression with the server error message
@@ -49,7 +51,7 @@ class TestExceptionSubcode:
             assert SUBCODE_IN_MESSAGE in excinfo.value.msg
         else:
             # There should be a message before the subcode
-            SUBCODE_IN_QUOTES = "({})".format(SUBCODE_IN_MESSAGE)
+            SUBCODE_IN_QUOTES = "({}".format(SUBCODE_IN_MESSAGE)
             assert SUBCODE_IN_QUOTES in excinfo.value.msg
 
     def test_invalid_verbosity(self):
