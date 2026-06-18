8eda0a4670019d449720d9d123c570a1b6ab2d48
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-06-18 06:38:43 -0700

    Rm outdated test cases for snip now that end is required.

--- test/new_tests/test_expressions_string.py
+++ test/new_tests/test_expressions_string.py
@@ -134,14 +134,6 @@ class TestExpressions(TestBaseClass):
                 str_expr.Concat(policy=None, values=[NEEDLE, NEEDLE], bin=STR_BIN_NAME),
                 EXAMPLE_STR + NEEDLE + NEEDLE
             ),
-#            (
-#                str_expr.Snip(policy=None, start=START_IDX, bin=STR_BIN_NAME),
-#                EXAMPLE_STR[:START_IDX]
-#            ),
-#            (
-#                str_expr.Snip(policy=None, start=START_IDX, end=None, bin=STR_BIN_NAME),
-#                EXAMPLE_STR[:START_IDX]
-#            ),
             (
                 str_expr.Snip(policy=None, start=START_IDX, end=len(EXAMPLE_STR) - 1, bin=STR_BIN_NAME),
                 EXAMPLE_STR[:START_IDX] + EXAMPLE_STR[-1]
