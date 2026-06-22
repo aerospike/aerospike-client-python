e6222fa05cd6c79a85b026263d2869a58d7a3cc6
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-06-22 08:55:34 -0700

    just revert for now - if more string ops are added after prepend, there will be ops with the same code

--- src/include/policy.h
+++ src/include/policy.h
@@ -119,8 +119,7 @@ static inline bool is_list_op(int op_code)
 }
 
 enum Aerospike_map_operations {
-    // Make sure all the op codes are unique
-    OP_MAP_SET_POLICY = OP_STRING_PREPEND + 1,
+    OP_MAP_SET_POLICY = 1101,
     OP_MAP_PUT,
     OP_MAP_PUT_ITEMS,
     OP_MAP_INCREMENT,
