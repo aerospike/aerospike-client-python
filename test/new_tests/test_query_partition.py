e8163a76137c026fa317650182e323280cca20ed
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-06-23 12:34:49 -0700

    Try using operate instead of batch_write to see how slow test_query_partition.py runs under valgrind.

--- test/new_tests/test_query_partition.py
+++ test/new_tests/test_query_partition.py
@@ -51,7 +51,7 @@ def setup(request, as_connection):
 
     as_connection.truncate(request.cls.test_ns, None, 0)
 
-    brs = br.BatchRecords(batch_records=[])
+    # brs = br.BatchRecords(batch_records=[])
     keys = []
 
     for i in range(1, 100000):
@@ -73,15 +73,15 @@ def setup(request, as_connection):
             request.cls.partition_1003_count += 1
             put = 1
         if put:
-            br_write = br.Write(key, ops=[
+            # br_write = br.Write(key, )
+            # brs.batch_records.append(br_write)
+            ops=[
                 operations.write("i", i),
                 operations.write("s", "xyz"),
                 operations.write("l", [2, 4, 8, 16, 32, None, 128, 256]),
                 operations.write("m", {"partition": rec_partition, "b": 4, "c": 8, "d": 16})
-            ])
-            brs.batch_records.append(br_write)
-
-    as_connection.batch_write(brs)
+            ]
+            as_connection.operate(key, ops)
 
     # print(f"{request.cls.partition_1000_count} records are put in partition 1000, \
     #         {request.cls.partition_1001_count} records are put in partition 1001, \
