e20283ed8a57ad8a55bda16b3c00b2cc603a2767
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-06-29 12:56:17 -0700

    Add string op quick start code example from PRD.

---
+++ examples/client/string_ops.py
@@ -0,0 +1,18 @@
+import aerospike
+from aerospike_helpers.operations import string_operations as so
+
+ops = [
+    so.strlen("mybin"),
+    so.upper("mybin"),
+    so.replace("mybin", "old", "new"),
+]
+
+config = {
+    "hosts": [
+        ("127.0.0.1", 3000)
+    ]
+}
+client = aerospike.client(config)
+
+key = ("test", "demo", 1)
+_, _, bins = client.operate(key, ops)
