a06734ea2355b4c481a15e118c3274ae89fce91e
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-06-29 14:05:23 -0700

    Address invalid usage of Upper

--- examples/client/string_expressions.py
+++ examples/client/string_expressions.py
@@ -4,5 +4,5 @@ from aerospike_helpers.operations import expression_operations as expr_ops
 expr = exp.Contains(exp.StrBin("email"), "@company.com")
 
 ops = [
-    expr_ops.expression_read("upper_name", exp.Upper(exp.StrBin("name")))
+    expr_ops.expression_read("upper_name", exp.Upper(policy=None, bin=exp.StrBin("name")))
 ]
