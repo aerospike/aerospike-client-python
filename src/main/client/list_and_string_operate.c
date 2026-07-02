aa1d1ef76c84de9a769516b4292150941214a189
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-07-02 14:59:50 -0700

    [CLIENT-4760] Refactor bit operations code (#1052)
    This makes the bit operations code easier to maintain when changes are required across all of the bit operations. This also reduces the number of forward declarations.
    
    Risks
    
    This can make editing individual operations harder because the input parsing is more spread out in one single function.

--- src/main/client/list_and_string_operate.c
+++ src/main/client/list_and_string_operate.c
@@ -30,14 +30,15 @@
 #include "cdt_list_operations.h"
 #include "cdt_operation_utils.h"
 
-typedef struct {
-    int operation_code;
-    const char *operation_name;
-} op_code_to_name;
-
+// Estimated size of array: (8 byte const char* address) * 2000 = 16000 bytes = 0.016 MB
+// not including the string constants the array points to
+// We estimate each string to be 20 bytes long.
+// 20 bytes * 2000 = 40000 bytes = 0.04 MB
+// Total memory usage is about 0.056 MB per client
 const char *op_code_to_names[] = {
 #define X(op_name) [OP_##op_name] = #op_name
-    X(LIST_APPEND), LIST_OP_NAMES_EXCEPT_LIST_APPEND, STRING_OP_NAMES
+    X(LIST_APPEND), LIST_OP_NAMES_EXCEPT_LIST_APPEND,
+    STRING_OP_NAMES X(BIT_RESIZE), BIT_OP_NAMES_EXCEPT_RESIZE
 #undef X
 };
 
