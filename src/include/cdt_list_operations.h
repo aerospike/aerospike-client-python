320ec642425bbbb7cb4e37c63c9e0ec592035aee
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-07-07 11:26:12 -0700

    Address compiler error.

--- src/include/cdt_list_operations.h
+++ src/include/cdt_list_operations.h
@@ -21,10 +21,8 @@
 #include <aerospike/as_vector.h>
 #include "types.h"
 
-as_status as_operations_add_from_pyobject(AerospikeClient *self, as_error *err,
-                                          PyObject *op_dict,
-                                          as_vector *unicodeStrVector,
-                                          as_static_pool *static_pool,
-                                          as_operations *ops,
-                                          long operation_code, long *ret_type,
-                                          int serializer_type);
+as_status as_operations_add_from_pyobject_helper(
+    AerospikeClient *self, as_error *err, PyObject *op_dict,
+    as_vector *unicodeStrVector, as_static_pool *static_pool,
+    as_operations *ops, long operation_code, long *ret_type,
+    int serializer_type);
