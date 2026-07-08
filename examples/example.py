903c51a050d9598aa495631c7c7754bcd48ee2bc
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-07-08 12:34:02 -0700

    Add Snipsync comment markers for docs.aerospike.com to selectively show parts of the code examples

--- examples/example.py
+++ examples/example.py
@@ -4,10 +4,13 @@ from aerospike_helpers.operations import string_operations
 
 class Example:
     def __init__(self):
+        # @@@SNIPSTART boilerplate
         config = {
             "hosts": [("127.0.0.1", 3000)]
         }
         client = aerospike.client(config)
+        # @@@SNIPEND
+
         self.client = client
 
     def __del__(self):
@@ -17,6 +20,7 @@ class StringOperations(Example):
     def run(self):
         client = self.client
 
+        # @@@SNIPSTART strlen
         key = ("test", "demo", "opstr_read")
         BIN_NAME = "text"
         client.put(key, {BIN_NAME: "hello world"})
@@ -26,7 +30,7 @@ class StringOperations(Example):
         ]
         _, _, bins = client.operate(key, ops)
         print(f"strlen(\"hello world\") = {bins[BIN_NAME]}")
-
+        # @@@SNIPEND
 
 if __name__ == "__main__":
     str_ops_example = StringOperations()
