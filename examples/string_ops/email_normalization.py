16a1caae53c9cbc0329d25896ee17854938e62d7
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-07-08 13:42:53 -0700

    Fix new partial extraction code example by using string expressions instead of string ops.

--- examples/string_ops/email_normalization.py
+++ examples/string_ops/email_normalization.py
@@ -23,6 +23,7 @@ client.put(key, bins={"email": ORIG_EMAIL})
 # New
 
 from aerospike_helpers.operations import string_operations as so
+from aerospike_helpers.operations import expression_operations
 from aerospike_helpers import expressions as exp
 
 ops = [so.trim("email"), so.lower("email")]
@@ -36,5 +37,26 @@ print(records)
 
 # Partial extraction
 
+from aerospike_helpers.expressions.resources import ResultType
+
 _, _, record = client.get(key)
 domain = record["email"].split("@")[1]
+print(domain)
+
+get_domain_expr = exp.ListGetByIndex(
+    ctx=None,
+    return_type=aerospike.LIST_RETURN_VALUE,
+    value_type=ResultType.STRING,
+    index=1,
+    bin=exp.SplitSeparator("@", bin="email")
+).compile()
+
+ops = [
+    expression_operations.expression_read(
+        bin_name="domain",
+        expression=get_domain_expr
+    )
+]
+
+_, _, bins = client.operate(key, ops)
+print(bins["domain"])
