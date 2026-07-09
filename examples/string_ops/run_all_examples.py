fb8fea7dc36ea59a3c9071909b0e138f6f659c57
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-07-09 13:44:56 -0700

    Divide customer experience examples into separate old and new examples. Create class to initialize boilerplate for each of those examples

--- examples/string_ops/run_all_examples.py
+++ examples/string_ops/run_all_examples.py
@@ -1,15 +1,18 @@
-from .customer_experience.email_normalization import EmailNormalization
-from .customer_experience.partial_extraction import PartialExtraction
+from .customer_experience.email_normalization_old import EmailNormalizationOld
+from .customer_experience.email_normalization_new import EmailNormalizationNew
+from .customer_experience.partial_extraction_old import PartialExtractionOld
+from .customer_experience.partial_extraction_new import PartialExtractionNew
 from .quickstart.string_expressions import StringExpressions
 from .quickstart.string_ops import StringOps
 
 example_classes = [
-    EmailNormalization,
-    PartialExtraction,
+    EmailNormalizationOld,
+    EmailNormalizationNew,
+    PartialExtractionNew,
+    PartialExtractionOld,
     StringExpressions,
     StringOps
 ]
 
 for cls in example_classes:
-    example = cls()
-    example.run()
+    example = cls().run()
