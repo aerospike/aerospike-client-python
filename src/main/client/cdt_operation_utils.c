fee05c7297576df4f74912313daa4b078cdb4028
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-07-09 08:50:45 -0700

    improve var naming in helper

--- src/main/client/cdt_operation_utils.c
+++ src/main/client/cdt_operation_utils.c
@@ -199,14 +199,14 @@ as_status get_uint64_t(as_error *err, const char *key, PyObject *op_dict,
 #define OUT_OF_BOUNDS_MESSAGE "%s must be between %d and %d."
 
 as_status get_bounded_int_from_py_dict(as_error *err, const char *key,
-                                       PyObject *op_dict, int *int_pointer,
+                                       PyObject *py_dict, int *int_pointer,
                                        int min_bound, int max_bound,
                                        bool is_optional,
                                        bool warn_if_out_of_bounds)
 {
     int64_t int64_to_return = -1;
     bool found = false;
-    if (get_optional_int64_t(err, key, op_dict, &int64_to_return, &found) !=
+    if (get_optional_int64_t(err, key, py_dict, &int64_to_return, &found) !=
         AEROSPIKE_OK) {
         return err->code;
     }
