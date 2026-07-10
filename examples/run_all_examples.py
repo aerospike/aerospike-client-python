19f97638daab48513aff8abeecec15f91ae17835
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-07-10 15:01:11 -0700

    Move run_all_examples module from string_ops folder to top level examples folder

---
+++ examples/run_all_examples.py
@@ -0,0 +1,18 @@
+from .string_ops.customer_experience.email_normalization_old import EmailNormalizationOld
+from .string_ops.customer_experience.email_normalization_new import EmailNormalizationNew
+from .string_ops.customer_experience.partial_extraction_old import PartialExtractionOld
+from .string_ops.customer_experience.partial_extraction_new import PartialExtractionNew
+from .string_ops.quickstart.string_expressions import StringExpressions
+from .string_ops.quickstart.string_ops import StringOps
+
+example_classes = [
+    EmailNormalizationOld,
+    EmailNormalizationNew,
+    PartialExtractionNew,
+    PartialExtractionOld,
+    StringExpressions,
+    StringOps
+]
+
+for cls in example_classes:
+    example = cls().run()
