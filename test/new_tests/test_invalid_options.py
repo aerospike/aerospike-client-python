8b05be69487e2d7e79a46a06b057c2a86938ca77
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-07-10 09:46:11 -0700

    TODO's have been addressed.

--- test/new_tests/test_invalid_options.py
+++ test/new_tests/test_invalid_options.py
@@ -12,9 +12,7 @@ class TestInvalidOptions:
         [
             # Use fixture with correct bins
             bitwise_operations.bit_lshift(bin_name="bitwise", bit_offset=0, bit_size=2, shift=1, policy={"bit_write_flags": -1}),
-            # TODO: aerospike.BIT_WRITE_PARTIAL <= x < aerospike.BIT_WRITE_PARTIAL * 2 may be valid
             bitwise_operations.bit_resize(bin_name="bitwise", byte_size=1, resize_flags=aerospike.BIT_RESIZE_SHRINK_ONLY * 2),
-            # TODO: same issue as above
             map_operations.map_put(bin_name="map", key=1, value=1, map_policy={"map_order": aerospike.MAP_KEY_VALUE_ORDERED + 1}),
             list_operations.list_append(bin_name="list", value=1, policy={"list_order": aerospike.LIST_ORDERED + 1}),
             list_operations.list_append(bin_name="list", value=1, policy={"write_flags": aerospike.LIST_WRITE_PARTIAL * 2}),
