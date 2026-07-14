de158109f255b365bb1001f98bcee59d08cfa9c3
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-07-14 08:29:33 -0700

    Clean up Get() example using C# client's get() example as a reference. TODO - Because of the C# client's fixture system, I'm wondering if we can just use pytest

--- examples/__init__.py
+++ examples/__init__.py
@@ -2,13 +2,25 @@ import aerospike
 
 
 class Example:
-    def __init__(self):
+    def __init__(
+        self,
+        host: str = "127.0.0.1",
+        port: int = 3000,
+        user: str = None,
+        password: str = None,
+        namespace: str = "test",
+        set_name: str = "demo"
+    ):
         config = {
-            "hosts": [("127.0.0.1", 3000)]
+            "hosts": [(host, port)],
+            "user": user,
+            "password": password
         }
         client = aerospike.client(config)
 
         self.client = client
+        self.namespace = namespace
+        self.set_name = set_name
 
     def __del__(self):
         self.client.close()
--- examples/run_all_examples.py
+++ examples/run_all_examples.py
@@ -1,3 +1,4 @@
+from .client.get import Get
 from .string_ops.customer_experience.email_normalization_old import EmailNormalizationOld
 from .string_ops.customer_experience.email_normalization_new import EmailNormalizationNew
 from .string_ops.customer_experience.partial_extraction_old import PartialExtractionOld
@@ -6,12 +7,13 @@ from .string_ops.quickstart.string_expressions import StringExpressions
 from .string_ops.quickstart.string_ops import StringOps
 
 example_classes = [
+    Get,
     EmailNormalizationOld,
     EmailNormalizationNew,
     PartialExtractionNew,
     PartialExtractionOld,
     StringExpressions,
-    StringOps
+    StringOps,
 ]
 
 for cls in example_classes:
