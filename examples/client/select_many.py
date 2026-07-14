41c15b64a7b522242dce0cebac6646ffb9dd6929
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-07-14 11:05:05 -0700

    Finish removing all the boilerplate code that is handled in the parent class.

--- examples/client/select_many.py
+++ examples/client/select_many.py
@@ -30,36 +30,6 @@ from optparse import OptionParser
 
 usage = "usage: %prog [options]"
 
-optparser = OptionParser(usage=usage, add_help_option=False)
-
-optparser.add_option(
-    "--help", dest="help", action="store_true",
-    help="Displays this message.")
-
-optparser.add_option(
-    "-U", "--username", dest="username", type="string", metavar="<USERNAME>",
-    help="Username to connect to database.")
-
-optparser.add_option(
-    "-P", "--password", dest="password", type="string", metavar="<PASSWORD>",
-    help="Password to connect to database.")
-
-optparser.add_option(
-    "-h", "--host", dest="host", type="string", default="127.0.0.1", metavar="<ADDRESS>",
-    help="Address of Aerospike server.")
-
-optparser.add_option(
-    "-p", "--port", dest="port", type="int", default=3000, metavar="<PORT>",
-    help="Port of the Aerospike server.")
-
-optparser.add_option(
-    "-n", "--namespace", dest="namespace", type="string", default="test", metavar="<NS>",
-    help="Port of the Aerospike server.")
-
-optparser.add_option(
-    "-s", "--set", dest="set", type="string", default="demo", metavar="<SET>",
-    help="Port of the Aerospike server.")
-
 optparser.add_option(
     "-k", "--keys", dest="keys", type="string", default="", metavar="<KEYS>",
     help="Keys to be accessed in the database server. Should be specified as 'name','name1','name2' etc")
--- examples/client/udf_list.py
+++ examples/client/udf_list.py
@@ -28,34 +28,6 @@ from optparse import OptionParser
 
 usage = "usage: %prog [options]"
 
-optparser = OptionParser(usage=usage, add_help_option=False)
-
-optparser.add_option(
-    "--help", dest="help", action="store_true",
-    help="Displays this message.")
-
-optparser.add_option(
-    "-U", "--username", dest="username", type="string", metavar="<USERNAME>",
-    help="Username to connect to database.")
-
-optparser.add_option(
-    "-P", "--password", dest="password", type="string", metavar="<PASSWORD>",
-    help="Password to connect to database.")
-
-optparser.add_option(
-    "-h", "--host", dest="host", type="string", default="127.0.0.1", metavar="<ADDRESS>",
-    help="Address of Aerospike server.")
-
-optparser.add_option(
-    "-p", "--port", dest="port", type="int", default=3000, metavar="<PORT>",
-    help="Port of the Aerospike server.")
-
-(options, args) = optparser.parse_args()
-
-if options.help:
-    optparser.print_help()
-    print()
-    sys.exit(1)
 
 ##########################################################################
 # Client Configuration
