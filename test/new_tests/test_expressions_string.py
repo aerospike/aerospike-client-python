0ade5c65582038c9e634990596b09a6211a441dd
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-07-20 14:29:59 -0700

    Reduce use of hardcoded strings

--- test/new_tests/test_expressions_string.py
+++ test/new_tests/test_expressions_string.py
@@ -9,8 +9,8 @@ from aerospike import exception as e
 
 from .test_base_class import TestBaseClass
 from .string_helpers import *
-from .conftest import expect_server_version_earlier_than_8_1_3_to_fail
-KEY = ("test", "demo", 1)
+from .conftest import expect_server_version_earlier_than_8_1_3_to_fail, TEST_NS, TEST_SET
+KEY = (TEST_NS, TEST_SET, 1)
 
 
 class TestExpressions:
