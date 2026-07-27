c17fe3305d1d4f0edb9e17480009645ff9b92b7f
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-07-27 16:15:51 -0700

    Run black on new tests. This makes it a lot easier to read in the PR

--- test/new_tests/test_invalid_int_arguments.py
+++ test/new_tests/test_invalid_int_arguments.py
@@ -1,7 +1,12 @@
 from .conftest import TEST_NS, TEST_SET
 import pytest
 
-from aerospike_helpers.operations import bitwise_operations, map_operations, list_operations, hll_operations
+from aerospike_helpers.operations import (
+    bitwise_operations,
+    map_operations,
+    list_operations,
+    hll_operations,
+)
 import aerospike
 from aerospike import exception as e
 
@@ -14,18 +19,43 @@ class TestInvalidOptions:
     @pytest.mark.parametrize(
         "op",
         [
-            bitwise_operations.bit_lshift(bin_name="bitwise", bit_offset=0, bit_size=2, shift=1, policy={"bit_write_flags": -1}),
-            bitwise_operations.bit_resize(bin_name="bitwise", byte_size=1, resize_flags=aerospike.BIT_RESIZE_SHRINK_ONLY * 2),
-            map_operations.map_put(bin_name="map", key=1, value=1, map_policy={"map_order": aerospike.MAP_KEY_VALUE_ORDERED + 1}),
-            list_operations.list_append(bin_name="list", value=1, policy={"list_order": aerospike.LIST_ORDERED + 1}),
-            list_operations.list_append(bin_name="list", value=1, policy={"write_flags": aerospike.LIST_WRITE_PARTIAL * 2}),
-            hll_operations.hll_add(bin_name="hll", values=[1], policy={"flags": aerospike.HLL_WRITE_ALLOW_FOLD * 2}),
-        ]
+            bitwise_operations.bit_lshift(
+                bin_name="bitwise",
+                bit_offset=0,
+                bit_size=2,
+                shift=1,
+                policy={"bit_write_flags": -1},
+            ),
+            bitwise_operations.bit_resize(
+                bin_name="bitwise",
+                byte_size=1,
+                resize_flags=aerospike.BIT_RESIZE_SHRINK_ONLY * 2,
+            ),
+            map_operations.map_put(
+                bin_name="map",
+                key=1,
+                value=1,
+                map_policy={"map_order": aerospike.MAP_KEY_VALUE_ORDERED + 1},
+            ),
+            list_operations.list_append(
+                bin_name="list",
+                value=1,
+                policy={"list_order": aerospike.LIST_ORDERED + 1},
+            ),
+            list_operations.list_append(
+                bin_name="list",
+                value=1,
+                policy={"write_flags": aerospike.LIST_WRITE_PARTIAL * 2},
+            ),
+            hll_operations.hll_add(
+                bin_name="hll",
+                values=[1],
+                policy={"flags": aerospike.HLL_WRITE_ALLOW_FOLD * 2},
+            ),
+        ],
     )
     def test_invalid_enum_values_emits_warning(self, op):
-        ops = [
-            op
-        ]
+        ops = [op]
         try:
             with pytest.warns(DeprecationWarning):
                 self.as_connection.operate(KEY, ops)
