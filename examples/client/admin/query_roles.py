2567a89f65d3d5cad5a227211d0c447a1b23eeba
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-08-11 18:20:48 -0700

    [CLIENT-5049] Convert existing code examples in examples/ folder to inherit from a base class for boilerplate setup (#1149)
    This is preparation for the aerospike.com documentation to reuse examples from examples/client.
    
    Code examples will be tweaked later on once they are included in the aerospike.com docs, so they may not be in a final state.
    
    Extra changes
    
    - CI/CD: Validate all code examples in examples/client
    - Remove the outdated async examples
    
    Risks
    
    Code examples can no longer take in parameters, so users must manually tweak the examples before running them.

--- examples/client/admin/query_roles.py
+++ examples/client/admin/query_roles.py
@@ -1,5 +1,4 @@
-
-##########################################################################
+################################################################################
 # Copyright 2013-2026 Aerospike, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,18 +12,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-##########################################################################
+################################################################################
+
+from . import AdminExample
 
 
-from .. import Example
-from aerospike import exception as e
-
-class UDFRemove(Example):
+class QueryRoles(AdminExample):
     def run(self):
-        module = "example.lua"
-        self.client.udf_remove(module)
+        policy = {}
 
-        try:
-            self.client.udf_remove(module)
-        except e.UDFNotFound:
-            print("Already removed UDF.")
+        roles = self.client.admin_query_roles(policy)
+        print(roles)
