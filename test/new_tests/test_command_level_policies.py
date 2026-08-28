25fcd25ce4414c8092747e28fe208aea8295df2b
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-08-28 11:22:42 -0700

    test: address bad indirect param syntax

--- test/new_tests/test_command_level_policies.py
+++ test/new_tests/test_command_level_policies.py
@@ -93,11 +93,11 @@ TTL = 2
 @pytest.mark.parametrize(
     "insert_records",
     [{"record_count": 500, "make_set_unique": False, "batch_write_command_policy": {"ttl": TTL}}],
-    [[
-        1,
-        True,
-        {"ttl": TTL}
-    ]],
+    [{
+        "record_count": 1,
+        "make_set_unique": True,
+        "batch_write_command_policy": {"ttl": TTL}
+    }],
     indirect=True
 )
 @pytest.mark.usefixtures("insert_records")
