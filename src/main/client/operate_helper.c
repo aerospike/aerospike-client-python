0f3191f7a0d7f997a73bf6f071d97c15d34d9b8f
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-08-31 08:03:30 -0700

    refactor: clear up why we use the same op code for the same ops that take in different arguments, then use unique codes for those equivalent expressions

--- src/main/client/operate_helper.c
+++ src/main/client/operate_helper.c
@@ -291,6 +291,8 @@ as_status as_operations_add_from_pyobject(AerospikeClient *self, as_error *err,
     switch (operation_code) {
     case OP_STRING_SUBSTR:
     case OP_STRING_SUBSTR_RANGE:
+    // OP_STRING_SNIP_START isn't used here because we don't need to worry about heap allocating
+    // memory for operations, unlike with expressions
     case OP_STRING_SNIP:
         if (get_int64_t(err, STRING_OP_START_KEY, op_dict, &start) !=
             AEROSPIKE_OK) {
@@ -388,6 +390,8 @@ as_status as_operations_add_from_pyobject(AerospikeClient *self, as_error *err,
             str_attr_key = "suffix";
             break;
         case OP_STRING_SPLIT_SEPARATOR:
+        // OP_LIST_JOIN_SEPARATOR isn't used here because we don't need to worry about heap allocating
+        // memory for operations, unlike with expressions
         case OP_LIST_JOIN:
             str_attr_key = "separator";
             break;
