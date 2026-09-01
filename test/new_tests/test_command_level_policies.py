a85bce59ccdf65376f1a0bc6fc4e26df7481cf9f
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-09-01 08:09:47 -0700

    test: make new test cases for client config use a unique set to avoid interfering with any following test cases that query the whole namespace/set to check for a user key

--- test/new_tests/test_command_level_policies.py
+++ test/new_tests/test_command_level_policies.py
@@ -92,7 +92,7 @@ TTL = 2
 
 @pytest.mark.parametrize(
     "insert_records",
-    [{"record_count": 1, "make_set_unique": False, "batch_write_command_policy": {"ttl": TTL}}],
+    [{"record_count": 1, "make_set_unique": True, "batch_write_command_policy": {"ttl": TTL}}],
     indirect=True
 )
 @pytest.mark.usefixtures("insert_records")
