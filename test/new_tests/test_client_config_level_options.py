399324e7b8c8c0a554d86c82f5c8194617bc41d6
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-09-15 13:52:38 -0700

    test: rm removed client config key

--- test/new_tests/test_client_config_level_options.py
+++ test/new_tests/test_client_config_level_options.py
@@ -483,7 +483,6 @@ def test_config_level_misc_options():
     config["policies"]["read_mode_ap"] = aerospike.POLICY_READ_MODE_AP_ALL
     config["policies"]["max_threads"] = 16
     config["policies"]["thread_pool_size"] = 16
-    config["policies"]["socket_timeout"] = 0
     config["thread_pool_size"] = 16
     config["max_threads"] = 16
     config["max_conns_per_node"] = 16
