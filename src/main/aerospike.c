c8a2fae8ecbb5b2c4d103688e6375b6128358bf3
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-09-15 13:51:05 -0700

    fix: remove client config keys from set of valid keys that were removed in this feature branch

--- src/main/aerospike.c
+++ src/main/aerospike.c
@@ -831,9 +831,9 @@ DEFINE_SET_OF_VALID_KEYS(client_config_policies, "read", "write", "apply",
                          "batch_remove", "batch_apply", "batch_write",
                          "batch_parent_write", "info", "admin", "txn_verify",
                          "txn_roll", "auth_mode",
-                         "login_timeout_ms", "key", "replica",
+                         "login_timeout_ms", "replica",
                          "metrics", "read_mode_ap",
-                         "max_threads", "thread_pool_size", "socket_timeout",
+                         "max_threads", "thread_pool_size",
                          NULL)
 // clang-format on
 
