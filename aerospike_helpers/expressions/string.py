78cd9ec6eaba6c80923fd1b5dd760bbbf390b445
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2026-09-21 08:14:09 -0700

    fix: incorrect type annotation for RegexReplace's replacement param

--- aerospike_helpers/expressions/string.py
+++ aerospike_helpers/expressions/string.py
@@ -828,7 +828,7 @@ class RegexReplace(_WriteOp):
         self,
         policy: StringPolicy,
         pattern: str,
-        replacement: int,
+        replacement: str,
         regex_flags: RegexFlags,
         bin: "TypeBinName"
     ):
